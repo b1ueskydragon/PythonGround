b039b2055db201aff6c1133992a9704d5599b3c6
ijrd1 <jang_inhwa@microad.co.jp>
2017-07-28 09:22:06 +0900

    result

--- codeit_lesson/flip.py
+++ codeit_lesson/flip.py
@@ -6,28 +6,27 @@ def flip(fst_idx, lst_idx, some_list):
         some_list[fst_idx], some_list[lst_idx] = some_list[lst_idx], some_list[fst_idx]
         fst_idx += 1
         lst_idx -= 1
-    return flip(fst_idx, lst_idx, some_list)
+        return flip(fst_idx, lst_idx, some_list)
+
 
 # Test
-some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 print(flip(0, len(some_list) - 1, some_list))
 
+# Slicing practice
+print(some_list[:1] + some_list[1:])
+print(some_list[1:] + some_list[:1])
 
 
-'''
+# case 2 (ijrd1)
 def flip(some_list):
-    if len(some_list) <= 2:  # base case
+    if len(some_list) < 2:  # base case
         return some_list
     else:  # recursive case
-        lst_idx = len(some_list) - 1
-        some_list[0], some_list[lst_idx] = some_list[lst_idx], some_list[0]
-        print(some_list)
-        some_list = some_list[1:lst_idx]
-    return flip(some_list)
+        return flip(some_list[1:]) + some_list[:1]
 
 
 # Test
-some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 some_list = flip(some_list)
 print(some_list)
-'''
