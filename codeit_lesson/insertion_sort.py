84b81788b3b623c8e166688f2e46e7b1394ca5ec
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-07-30 20:38:37 +0900

    processing...

--- codeit_lesson/insertion_sort.py
+++ codeit_lesson/insertion_sort.py
@@ -1,8 +1,11 @@
-# 삽입 정렬
+# 挿入ソート
 def insertion_sort(my_list):
     for i in range(len(my_list)):
-        my_list[i] < my_list[i + 1]
-
+        target = my_list[i]
+        cs = i + 1
+        if target < my_list[cs]:
+            my_list = my_list[cs:]
+            cs += 1
 
 some_list = [11, 3, 6, 4, 12, 1, 2]
 print(insertion_sort(some_list))
