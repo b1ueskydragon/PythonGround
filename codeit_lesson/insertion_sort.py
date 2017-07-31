65fb7b1911ec0093ad7f9348fec521fdfc1ed1f8
ijrd1 <jang_inhwa@microad.co.jp>
2017-08-01 01:08:39 +0900

    Process in... 0731

--- codeit_lesson/insertion_sort.py
+++ codeit_lesson/insertion_sort.py
@@ -1,11 +1,14 @@
 # 挿入ソート
 def insertion_sort(my_list):
-    for i in range(len(my_list)):
-        target = my_list[i]
-        cs = i + 1
-        if target < my_list[cs]:
-            my_list = my_list[cs:]
-            cs += 1
+    for i in range(len(my_list)):  # 挿入対象の探索
+        tmp = my_list[i]
+        if i > 0:
+            j = i - 1
+            target = my_list[j]
+            while j != 0 and tmp > target:
+                j -= 1
+            my_list[i], my_list[j] = my_list[j], my_list[i]
+    return my_list
 
 some_list = [11, 3, 6, 4, 12, 1, 2]
 print(insertion_sort(some_list))
