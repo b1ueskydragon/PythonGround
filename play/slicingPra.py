6814aed4eb7d32c3bca0e30cab99e5db38c758fa
jang <jang_inhwa@microad.co.jp>
2017-08-03 14:03:40 +0900

    Slicing Challenge

---
+++ play/slicingPra.py
@@ -0,0 +1,34 @@
+moji = "abcd"
+
+print(moji[1:4])
+
+print(moji[1:4:2])
+
+print(moji[1::])
+
+print(moji[::2])
+
+print(moji[::1])
+
+print(moji[::])
+
+print(moji[::-1])  # dcba
+
+print(moji[-1::-1])  # dcba
+
+print(moji[1::-1])
+
+print(moji[3::-1])  # dcba
+
+print("あ")
+print(moji[5::-1])  # dcba
+
+print("い")
+print(moji[3:0:-1])  # dcba
+
+print("う")
+print(moji[-1::1])
+
+'''start idx : stop idx : step '''
+''' -1 = 末尾'''
+''' start idx <= [//] < stop idx '''
