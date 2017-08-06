d9a8d31c3bba1f60f7260241bfc57acd8c8d99d2
jang <jang_inhwa@microad.co.jp>
2017-08-06 23:16:37 +0900

    改良中

--- codeit_lesson/insertion_sort.py
+++ codeit_lesson/insertion_sort.py
@@ -13,6 +13,8 @@ def insertion_sort(list):
                 list[i + 1] = list[i]  # shift number in slot i right to i+1
                 list[i] = value  # shift value left into slot i
 
+               # list[i], list[i+1] = list[i+1], list[i]  # にしても同じ結果ー。
+
                 i -= 1  # need decremented i
             else:  # when don't need shift value anymore (already-right-placed)
                 break
