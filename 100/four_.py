785e12dbc94cc71d6c6ced6249323a9377dc8f54
jang <jang_inhwa@microad.co.jp>
2017-08-18 12:57:54 +0900

    参考

--- 100/four_.py
+++ 100/four_.py
@@ -19,10 +19,3 @@ for i, word in enumerate(wordslist):
         symbol = word[0:2]
     result[key] = symbol
 print(result)
-
-
-b='Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
-import curses.ascii as a
-r=list(map(lambda x:len(list(filter(a.isalpha,x))), b.split()))
-
-print(r)
