dc59b1714017aff617ee88671ff63f5020f071c7
jang <jang_inhwa@microad.co.jp>
2017-08-22 15:51:32 +0900

    Refactor MedianSort

--- Sort/QuickSortMedian.py
+++ Sort/QuickSortMedian.py
@@ -1,16 +1,21 @@
 import os.path as ospath
 import time
 
-# 200000000ずつファイル読み込み
+
+DIVISION_RANGE = 200_000_000
+MY_PATH = "/Users/jang_inhwa/Desktop/"  # pathは適宜変えること
+
+
+# 200_000_000 ずつ読み込み
 def read_file(num_range):
-    open_path = path + "sort_median.txt"
+    open_path = MY_PATH + "sort_median.txt"
     num_list = []
     file = open(open_path, "r", encoding="UTF-8")
 
     for num in file:
         num = int(num)
 
-        if num_range <= num < (num_range + 200000000):
+        if num_range <= num < (num_range + DIVISION_RANGE):
             num_list += [num]
 
     file.close()
@@ -39,7 +44,7 @@ def quick_sort(list, left, right):
 
 # 書き出し
 def write_file(list):
-    write_path = path + "sort_median_result.txt"
+    write_path = MY_PATH + "sort_median_result.txt"
     file = open(write_path, 'a', encoding="UTF-8")
     for el in list:
         el = str(el) + "\n"
@@ -59,11 +64,13 @@ def isSorted(list):
         return print("index [", i, "] and [", i + 1, "] are not sorted")
 
 
-path = "/Users/jang_inhwa/Desktop/"  # pathは適宜変えること
+# 実行
 num_range = 0
 line_cnt = 1
-start = int(round(time.time()))
-while num_range < (200000000 * 10):
+
+START = int(round(time.time()))
+
+while num_range < (DIVISION_RANGE * 10):
     print(line_cnt, "行目 start")
 
     list = read_file(num_range)  # ソート前
@@ -73,12 +80,12 @@ while num_range < (200000000 * 10):
     print(line_cnt, "行目 written")
     list = []  # メモリ解放
     line_cnt += 1
-    num_range += 200000000
+    num_range += DIVISION_RANGE
 
-end = int(round(time.time()))
+END = int(round(time.time()))
 
-print((end - start))  # 処理時間
-print(ospath.getsize(path + "sort_median_result.txt") / (10**9))  # 書き出したファイルの容量
+print((END - START))  # 処理時間
+print(ospath.getsize(MY_PATH + "sort_median_result.txt") / (10 ** 9))  # 書き出したファイルの容量
 
 # TODO import resource メモリ制限
-
+# TODO ソート検査機使う
