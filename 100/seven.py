9f5b8088032448cf0d59e9497f0d2ca675c2f912
jang <jang_inhwa@microad.co.jp>
2017-08-23 13:22:23 +0900

    Quiz7 これでいいのかな

--- 100/seven.py
+++ 100/seven.py
@@ -5,3 +5,11 @@
 さらに，x=12, y="気温", z=22.4として，実行結果を確認せよ．
 '''
 
+
+def template(x, y, z):
+    x = str(x)
+    y = str(y)
+    z = str(z)
+    return x + "時の" + y + "は" + z
+
+print(template(12, "気温", 22.4))
