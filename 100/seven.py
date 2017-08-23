c7562fb1054d88a7b73e85afeedc975d9980ee48
jang <jang_inhwa@microad.co.jp>
2017-08-23 13:32:22 +0900

    Quiz7 いろいろ

--- 100/seven.py
+++ 100/seven.py
@@ -6,10 +6,26 @@
 '''
 
 
+# 方法1
 def template(x, y, z):
     x = str(x)
     y = str(y)
     z = str(z)
     return x + "時の" + y + "は" + z
 
+
 print(template(12, "気温", 22.4))
+
+
+# Thinking Time
+def make_str(i):
+    return str(i)
+
+
+# 方法2
+def __template__(x, y, z):
+    rst = list(map(lambda i: str(i), [x, y, z]))
+    return rst[0] + "時の" + rst[1] + "は" + rst[2]
+
+
+print(__template__(12, "気温", 22.4))
