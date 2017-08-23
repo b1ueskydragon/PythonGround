b9fed396f5e2558543a91ea7ce97f54449ccdc83
jang <jang_inhwa@microad.co.jp>
2017-08-23 14:07:45 +0900

    Make a Frame Tentatively

--- 100/eight.py
+++ 100/eight.py
@@ -1,7 +1,23 @@
 '''
+暗号文
+
 与えられた文字列の各文字を，以下の仕様で変換する関数cipherを実装せよ．
 
 英小文字ならば(219 - 文字コード)の文字に置換
 その他の文字はそのまま出力
 この関数を用い，英語のメッセージを暗号化・復号化せよ．
 '''
+
+
+def cipher(target_stc):
+    encrypt_stc = ""
+    for s in target_stc:
+        if not s.isalnum():
+            encrypt_stc += s.replace(s, "@")
+        else:
+            encrypt_stc += s
+    return encrypt_stc
+
+
+stc = "hello, b1ueskydragon!"
+print(cipher(stc))
