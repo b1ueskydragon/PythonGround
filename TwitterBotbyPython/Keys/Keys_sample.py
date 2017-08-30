a236cb0eb38c60885e2b4bca25124b3ffd8f2394
jang <jang_inhwa@microad.co.jp>
2017-08-30 20:20:13 +0900

    Pythonでちゃんbotはじめました

---
+++ TwitterBotbyPython/Keys/Keys_sample.py
@@ -0,0 +1,11 @@
+# サンプル
+class Keys_sample:
+    CONSUMERKEY = ""
+    CONSUMERSECRET = ""
+    ACCESSTOKEN = ""
+    ACCESSTOKENSECRET = ""
+    def __init__(self):
+        self.CONSUMERKEY = "xxxxx"
+        self.CONSUMERSECRET = "xxxxx"
+        self.ACCESSTOKEN  = "xxxxx"
+        self.ACCESSTOKENSECRET = "xxxxx"
---
+++ TwitterBotbyPython/tweet.py
@@ -0,0 +1,25 @@
+from requests_oauthlib import OAuth1Session  # Requests-OAuthlib ライブラリ
+from TwitterBotSample.Keys.Keys import Keys
+
+CK = Keys().CONSUMERKEY
+CS = Keys().CONSUMERSECRET
+AT = Keys().ACCESSTOKEN
+AS = Keys().ACCESSTOKENSECRET
+
+FROMBOT = " -- ちゃんbotより"
+
+# ツイート投稿用のURL
+url = "https://api.twitter.com/1.1/statuses/update.json"
+
+# ツイート本文
+params = {"status": "多言語でちゃんbot!" + FROMBOT}
+
+# OAuth認証で POST method で投稿
+twitter = OAuth1Session(CK, CS, AT, AS)
+req = twitter.post(url, params)
+
+# レスポンスを確認
+if req.status_code == 200:
+    print ("OK")
+else:
+    print ("Error: %d" % req.status_code)
