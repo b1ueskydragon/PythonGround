a4078bb3374a1edf20badccb2e2a03baf90e745c
b1ueskydragon <jang_inhwa@microad.co.jp>
2017-11-08 00:09:49 +0900

    Refactor first

--- CIQ/palindromic_pivot.py
+++ CIQ/palindromic_pivot.py
@@ -1,31 +1,31 @@
-RANGE = 1_000_000
-
-
 # 回文数ですか?
-def isPal(target):
+def is_pal(target):
     return int(target) == int(str(target)[::-1])
 
-# 探索だけする
-def find(c1, c2):
-    while (c1 <= c2):
-        if isPal(c1):
-            print(c1)
-        c1 += 1
 
-#真ん中だけ求める
+# 探索だけする
+def find(x, y):
+    while x <= y:
+        if is_pal(x):
+            print(x)
+        x += 1
+
+
+# 真ん中だけ求める
 def center(a, b):
     return int((a + b) / 2)
 
 
-'''
-move
-pivot
-'''
+RANGE = 1_000_000
+
+
+# move, pivot
 def calc_left(m, p):
     if center(m, p) - m > RANGE:
         calc_left(center(m, p), p)
     else:
         find(m, p)
+
 
 def calc_right(p, m):
     if m - center(p, m) > RANGE:
@@ -33,20 +33,18 @@ def calc_right(p, m):
     else:
         find(p, m)
 
+
 '''
 start
 finish
 center
 '''
-s = 28228056190
-f = 77331569708
+s = 174517247652
+f = 237309752247
 c = center(s, f)
-
-print("真ん中", c)
 
 calc_left(s, c)
 calc_right(c, f)
-
 
 
 # 28228056190, 77331569708 (expected 52779797725,52779897725)
