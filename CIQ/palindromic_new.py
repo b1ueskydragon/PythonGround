bccf6e1140ad81b0727a3a44ee2fe29e7c6716e1
b1ueskydragon <jang_inhwa@microad.co.jp>
2017-11-07 23:14:08 +0900

    171107

--- CIQ/palindromic_new.py
+++ CIQ/palindromic_new.py
@@ -1,37 +1,105 @@
-'''
-GOAL = 真ん中の回文数
+M = 100_000
 
-52779812949
-52779797725,52779897725
-'''
-
-
-mill = 10_000
 
 # 回文数ですか?
-def is_palindrom(target):
+def isPal(target):
     return int(target) == int(str(target)[::-1])
 
-def binary_search(left, right):
-    center = int((left + right) / 2)
-    if (center - left) > 10000:
-        center = binary_search(left, center)
-    return center
 
-# left から right までの回文数を求める
-def find_all(left, right):
-    result = []
-    target = left  # 出発
-    while target <= right:
-        if is_palindrom(target): result += [target]
-        target += 1
-    return result
+def find(c1, c2):
+    while (c1 <= c2):
+        if not isPal(c1):
+            c1 += 1
+        else:
+            return c1
 
-# 材料
-left = 1000000000
-right = 9000000000
-center = int((left + right) / 2)
-new_center = binary_search(left, right)
 
-print(find_all(left, new_center))
-print(find_all(new_center, new_center))
+def center(a, b):
+    if b - a <= 374631:
+        center(a, b)
+    return int((a + b) / 2)
+
+
+def recursion(l, r):
+    c = center(l, r)
+
+    find(l, c)
+    find(c, r)
+
+
+
+s = 28228056190
+f = 77331569708
+
+c = center(s, f)
+cl = center(s, c)
+cr = center(c, f)
+
+cll = center(cl, c)
+crr = center(c, cr)
+
+clll = center(cll, c)
+crrr = center(c, crr)
+
+cllll = center(clll, c)
+crrrr = center(c, crrr)
+
+clllll = center(cllll, c)
+crrrrr = center(c, crrrr)
+
+cllllll = center(clllll, c)
+crrrrrr = center(c, crrrrr)
+
+clllllll = center(cllllll, c)
+crrrrrrr = center(c, crrrrrr)
+
+cllllllll = center(clllllll, c)
+crrrrrrrr = center(c, crrrrrrr)
+
+clllllllll = center(cllllllll, c)
+crrrrrrrrr = center(c, crrrrrrrr)
+
+cllllllllll = center(clllllllll, c)
+crrrrrrrrrr = center(c, crrrrrrrrr)
+
+clllllllllll = center(cllllllllll, c)
+crrrrrrrrrrr = center(c, crrrrrrrrrr)
+
+cllllllllllll = center(clllllllllll, c)
+crrrrrrrrrrrr = center(c, crrrrrrrrrrr)
+
+clllllllllllll = center(cllllllllllll, c)
+crrrrrrrrrrrrr = center(c, crrrrrrrrrrrr)
+
+cllllllllllllll = center(clllllllllllll, c)
+crrrrrrrrrrrrrr = center(c, crrrrrrrrrrrrr)
+
+clllllllllllllll = center(cllllllllllllll, c)
+crrrrrrrrrrrrrrr = center(c, crrrrrrrrrrrrrr)
+
+cllllllllllllllll = center(clllllllllllllll, c)
+crrrrrrrrrrrrrrrr = center(c, crrrrrrrrrrrrrrr)
+
+clllllllllllllllll = center(cllllllllllllllll, c)
+crrrrrrrrrrrrrrrrr = center(c, crrrrrrrrrrrrrrrr)
+
+cllllllllllllllllll = center(clllllllllllllllll, c) # ここ
+crrrrrrrrrrrrrrrrrr = center(c, crrrrrrrrrrrrrrrrr) # ここ
+
+print("正解r", cllllllllllllllllll)
+print(c)
+print("正解l", crrrrrrrrrrrrrrrrrr)
+
+
+clllllllllllllllllll = center(cllllllllllllllllll, c)
+crrrrrrrrrrrrrrrrrrr = center(c, crrrrrrrrrrrrrrrrrr)
+
+
+print(find(cllllllllllllllllll, c))
+print(find(c, crrrrrrrrrrrrrrrrrr))
+
+print("超えたr", clllllllllllllllllll)
+print(c)
+print("超えたl", crrrrrrrrrrrrrrrrrrr)
+
+print(crrrrrrrrrrrrrrrrr - clllllllllllllllll)
