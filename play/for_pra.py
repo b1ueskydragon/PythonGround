9ec24960d9f8277585894d425b00a410665fe82f
b1ueskydragon <jang_inhwa@microad.co.jp>
2018-02-11 13:01:03 +0900

    Practice of for-loop params

---
+++ play/for_pra.py
@@ -0,0 +1,14 @@
+for i in range(10, -1, -2):  # start, stop, step
+    print(i)  # 10,8,6,4,2,0
+print('\n')
+
+for i in range(10, -2, -2):
+    print(i)  # 10,8,6,4,2,0
+print('\n')
+
+for i in range(10, -4, -2):
+    print(i)  # 10,8,6,4,2,0,-2
+print('\n')
+
+for i in range(10, 0, -2):
+    print(i)  # 10,8,6,4,2
