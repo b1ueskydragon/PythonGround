42543c7ddac60237bcb31e16c18164ae3edba128
b1ueskydragon <jang_inhwa@microad.co.jp>
2018-02-14 09:29:09 +0900

    Implementation with py

--- dailyOne/P05.py
+++ dailyOne/P05.py
@@ -1,12 +1,24 @@
+""""
+closures
+returns a new anonymous function
+"""
+
+
 # constructs a pair
 def cons(a, b):
     return lambda f: f(a, b)
 
+
 # returns the first element
-def car(cons):
-    return None
+def car(pair):
+    return pair(lambda a, b: a)
 
 
 # returns the last element
-def cdr(cons):
-    return None
+def cdr(pair):
+    return pair(lambda a, b: b)
+
+
+print(car(cons(3, 4)))
+print(cdr(cons(3, 4)))
+print(cdr(cdr(cons(3, cons(3, 4)))))
