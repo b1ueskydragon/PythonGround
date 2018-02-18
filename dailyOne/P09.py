48eedd904af7840a5278504546d7b7dccedfb8bd
b1ueskydragon <jang_inhwa@microad.co.jp>
2018-02-19 01:53:49 +0900

    WIP

--- dailyOne/P09.py
+++ dailyOne/P09.py
@@ -15,3 +15,17 @@ def largest_non_adjacent(arr):
 a = [1, 2, 3]
 print(max(a[1:]))
 print(a[0] + max(a[2:]))
+
+
+# dynamic programming to store: O(N) : Have some problems
+def use_cache(nums):
+    cache = [nums[0], nums[1]]  # first two elements
+    for i, num in enumerate(nums, 2):  # index i start as [2], num start as num[0]
+        cache.append(i)
+        x = cache[i - 2] + num
+        y = cache[i - 1]
+        cache[i] = (max(x, y))  # example: num[0] + num[2] vs num[1]
+    return cache[-1]
+
+
+print(use_cache([0, 2, 3, 4, 5, 6]))
