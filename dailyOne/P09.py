b56bd4484930de6706bddb6259c05601d348eca9
b1ueskydragon <jang_inhwa@microad.co.jp>
2018-02-18 17:50:21 +0900

    WIP: My first solution

--- dailyOne/P09.py
+++ dailyOne/P09.py
@@ -1,33 +1,40 @@
+"""
+@b1ueskydragon
+"""
+
 import math
 
 
-def large_sum_non_adjacent(nums):  # WIP : if len(nums) == 4
-    if len(nums) == 0:
-        return 0
+# リストを絞っていく
+def large_sum_non_adjacent(nums):
+    if len(nums) <= 2:
+        return max(nums)
 
-    buff_list = []
-
+    buff = []
     center = math.ceil(len(nums) / 2)
 
-    left_half = nums[:center]
-    if len(left_half) == 2:  # left half
-        if left_half[0] > left_half[1]:
-            buff_list.append(left_half[0])
-        else:
-            buff_list.append(left_half[1])
+    left_half = nums[:center]  # left half
+    find_helper(left_half, buff)
 
-    right_half = nums[center:]
-    if len(right_half) == 2:  # right half
-        if right_half[0] > right_half[1]:
-            buff_list.append(right_half[0])
-        else:
-            buff_list.append(right_half[1])
+    right_half = nums[center:]  # right half
+    find_helper(right_half, buff)
 
-    return buff_list  # WIP: リストを絞っていく (recursion)
+    return buff[0] + buff[1]
+
+
+def find_helper(nums, buff):
+    if len(nums) <= 2:
+        buff.append(max(nums))
+    else:
+        find_helper(nums[2:], buff)
 
 
 print(large_sum_non_adjacent([2, 4, 6, 8]))  # 12
 print(large_sum_non_adjacent([5, 1, 1, 5]))  # 10
+
+print(large_sum_non_adjacent([5, 1, 1, 5, 2, 4, 8, 7]))  # 5, 8
+
+print(large_sum_non_adjacent([5, 1, 1, 5, 2, 4, 6, 8, 11]))  # .. ?
 
 # 総当たり O(n(n-1)/2) == O(n^2)
 # O(n) ?
