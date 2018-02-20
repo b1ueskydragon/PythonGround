19999773890d2ba779d8cdb2dec02911a4fe2fab
jang <jang_inhwa@microad.co.jp>
2018-02-20 16:46:14 +0900

    WIP

--- dailyOne/P08.py
+++ dailyOne/P08.py
@@ -1,17 +1,17 @@
 """
- leaf nodes are always unival trees
+leaf nodes can be unival tree too
 """
 
 
 # each node of a tree
 class Node:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, value):
+        self.value = value
         self.left = None
         self.right = None
 
     def get_node(self):
-        return self.data
+        return self.value
 
 
 def a_tree():
@@ -24,21 +24,42 @@ def a_tree():
     return tree
 
 
+def b_tree():
+    tree = Node('a')  # root node
+    tree.left = Node('c')
+    tree.right = Node('b')
+    tree.right.left = Node('b')
+    tree.right.right = Node('b')
+    tree.right.right.right = Node('b')
+    return tree
+
+
+"""
 def tree_sum(tree):
-    if tree:
-        return tree.get_node() + tree_sum(tree.left) + tree_sum(tree.right)
-    else:
+    if tree is None:
         return 0
+    else:
+        return tree.get_node() + tree_sum(tree.left) + tree_sum(tree.right)
 
 
 print(tree_sum(a_tree()))
+"""
 
 
-def tree_count(tree, i):
-    if tree:
-        print(tree.get_node(), "_" * i)
-        i += 1
-        return tree_count(tree.left, i)
+def is_unival(tree):  # basic case is root
+    return tree_count(tree, tree.value)
 
 
-print(tree_count(a_tree(), 1))
+def tree_count(tree, value):
+    if tree is None:
+        return True
+    if tree.value == value:
+        return tree_count(tree.left, value) and tree_count(tree.right, value)
+    return False
+
+
+rootA = a_tree()
+print(is_unival(rootA))
+rootB = b_tree()
+print(is_unival(rootB))
+
