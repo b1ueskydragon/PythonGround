682394673b7c0fb359a2591a405e50f4472d8cfe
jang <jang_inhwa@microad.co.jp>
2018-02-21 14:29:09 +0900

    WIP P13

---
+++ dailyOne/P13/P13.py
@@ -0,0 +1,26 @@
+# should be O(N)
+def longest_substring(str, kth):
+    if kth >= len(str):
+        return str
+
+    for i, v in enumerate(str, kth):
+        candidate = str[:kth]
+        candidate += str[i]
+        s = set(candidate)
+        if len(s) < kth:
+            candidate += str[i]
+        else:
+            candidate = str[i - 1:]
+
+        return candidate
+
+
+s = "abcba"
+k = 2
+
+print(longest_substring(s, k))
+
+longs = "abcbdbdbbdcdabd"
+kk = 2
+
+print(longest_substring(longs, kk))
