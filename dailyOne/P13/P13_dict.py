36ffb0ec447d72e1f76fc8e60b39f63be87320c3
jang <jang_inhwa@microad.co.jp>
2018-02-26 12:04:42 +0900

    Add comments

--- dailyOne/P13/P13_dict.py
+++ dailyOne/P13/P13_dict.py
@@ -5,20 +5,20 @@ def longest_substring(s, k):
 
     '''init settings'''
     table = {}  # {current_look, count}
-    left = 0
-    max_len = k
+    left = 0  # start point of current candidated string
+    max_len = k  # max length of current point
 
     for i, current_look in enumerate(s):
 
         if current_look in table:
-            table[current_look] = table.get(current_look) + 1
+            table[current_look] = table.get(current_look) + 1  # increment counter if character exist
         else:
-            table[current_look] = 1
+            table[current_look] = 1  # Add a new character if not exist
 
         if len(table) > k:
-            max_len = max(max_len, i - left)
+            max_len = max(max_len, i - left)  # comparing current max VS new candidate
 
-            while len(table) > k:
+            while len(table) > k:  # reducing counter and move start point to end of candidate
                 start_point = s[left]
 
                 if table.get(start_point) is 1:
@@ -28,7 +28,7 @@ def longest_substring(s, k):
 
                 left += 1
 
-    max_len = max(max_len, len(s) - left)
+    max_len = max(max_len, len(s) - left)  # comparing current max VS new candidate
 
     return max_len
 
