79e9550c67db568dcf674ecb801061b802b785a8
b1ueskydragon <jang_inhwa@microad.co.jp>
2018-03-07 09:31:52 +0900

    Fix to logN

--- play/power_calc.py
+++ play/power_calc.py
@@ -1,11 +1,4 @@
-"""
-高速累乗計算
-x: 基数
-n: 乗
-"""
-
-
-# O(logN)
+# O(n)
 def power_calc(x, n):
     if n % 2 == 0:
         return (x ** 2) ** int(n / 2)
@@ -14,3 +7,24 @@ def power_calc(x, n):
 
 
 print(power_calc(3, 11))
+
+"""
+高速累乗計算
+最低でも2回に1回は n のビット数が減る
+
+x: 基数
+n: 乗
+"""
+
+
+# O(log n)
+def power(x, n):
+    if n is 0:  # exit case
+        return 1
+    if n % 2 is 0:  # n が偶数: 半分にして次は偶数か奇数に
+        return power(x ** 2, int(n / 2))
+    else:  # n が奇数: 1を引いて次は必ず偶数
+        return x * power(x, n - 1)
+
+
+print(power(3, 11))
