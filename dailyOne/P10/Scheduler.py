3ba2364c45d14a4b6083ace5bb1c6d7069023bce
jang <jang_inhwa@microad.co.jp>
2018-03-08 14:14:17 +0900

    WIP: add comment

--- dailyOne/P10/Scheduler.py
+++ dailyOne/P10/Scheduler.py
@@ -11,5 +11,8 @@ class Scheduler:
             time.sleep(n / 1000)
             f()
 
-        t = threading.Thread(target=call_after_sleep)
+        t = threading.Thread(
+            target=call_after_sleep)  # spin off a new thread each time we call delay -> easy to explode
         t.start()
+
+# TODO: Use only one thread and storing f. -> use list structure
