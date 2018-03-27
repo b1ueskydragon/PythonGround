3884e638c644d34b2c00770bc1ad472f2118fdcc
jang <jang_inhwa@microad.co.jp>
2018-03-27 12:00:07 +0900

    Sort?

--- play/graph/depth-first-search.py
+++ play/graph/depth-first-search.py
@@ -1,51 +1,52 @@
-# to graph
-graph = {
-    's': {'1', '6', '8'},
-    '1': {'s', '2', '3'},
-    '2': {'1', '10', '11'},
-    '3': {'1', '4', '12'},
-    '4': {'3', '5', '13'},
-    '5': {'4', '6', '9'},
-    '6': {'s', '5', '7'},
-    '7': {'6', '8', '9'},
-    '8': {'s', '7', '14'},
-    '9': {'5', '7', 't'},
-    '10': {'2'},
-    '11': {'2'},
-    '12': {'3'},
-    't': {'9'}
-}
-
-# visited (not yet is -1)
-visited = {}
-
-# previous visit point of point u
-previous_visit = {}
-
-
 def depth_first_search(u):
-    '''
-    recursive search
+    """
+    recursive search.
 
     :param u: at this point
     :return:
-    '''
+    """
     visited[u] = 'gray'
-    for v in graph:
-        if visited[v] is 'white':
-            previous_visit[v] = u
-            depth_first_search(v)
+    for gv in graph.get(u):
+        if visited[gv] is 'white':
+            pre_visit[gv] = u
+            depth_first_search(gv)
     visited[u] = 'black'
 
-    return previous_visit
+    return pre_visit
 
 
 if __name__ == "__main__":
+    # to graph (隣接接点が昇順にソーと済み)
+    graph = {
+        's': {'1', '6', '8'},
+        '1': {'s', '2', '3'},
+        '2': {'1', '10', '11'},
+        '10': {'2'},
+        '11': {'2'},
+        '3': {'1', '4', '12'},
+        '12': {'3'},
+        '4': {'3', '5', '13'},
+        '13': {'4'},
+        '5': {'4', '6', '9'},
+        '6': {'s', '5', '7'},
+        '7': {'6', '8', '9'},
+        '8': {'s', '7', '14'},
+        '14': {'8'},
+        '9': {'5', '7', 't'},
+        't': {'9'}
+    }
+
+    # `gray`  分岐の可能性あり
+    # `white` 未探索
+    # `black` 探索済み
+    visited = {}
+
+    # previous visit point of point u (not yet is -1)
+    pre_visit = {}
+
     for v in graph:
-        '''
-        initialize
-        '''
-        previous_visit[v] = -1
+        '''initialize'''
+        pre_visit[v] = -1
         visited[v] = 'white'
 
     print(depth_first_search('s'))
