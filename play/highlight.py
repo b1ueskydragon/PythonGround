e6f55db67dd4da83ccd58dff37450fa1f059bc51
jang <jang_inhwa@microad.co.jp>
2018-04-06 21:45:28 +0900

    WIP

---
+++ play/highlight.py
@@ -0,0 +1,33 @@
+def input_sys():
+    first_line_list = input().split(' ')
+    break_point = first_line_list[1]
+
+    result = []
+    for line in range(int(break_point)):
+        result.append(input().split(' '))
+
+    return [first_line_list] + result
+
+
+def highlight(target=input_sys()):
+    # n回目ブロック数
+    tail = target[1:]
+    prev_s = int(tail[0][0])
+    prev_e = int(tail[0][1])
+
+    for n, lst in enumerate(tail):
+        curr_s = int(tail[n][0])
+        curr_e = int(tail[n][1])
+
+        if prev_s <= curr_s and prev_e <= curr_e:
+            curr_length = curr_e - prev_s + 1
+            # スタートそのまま
+            prev_e = curr_e
+
+        elif prev_s <= curr_s and prev_e >= curr_e:
+            curr_length = curr_length - (curr_e - curr_s + 1)
+
+    return curr_length
+
+
+print(highlight())
