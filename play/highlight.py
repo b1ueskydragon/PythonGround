4bdd29dc7439d028f6fa087860d29ab74dbf3eb9
b1ueskydragon <jang_inhwa@microad.co.jp>
2018-04-07 09:06:04 +0900

    Use list comprehension

--- play/highlight.py
+++ play/highlight.py
@@ -1,39 +1,29 @@
 def input_sys():
     first_line_list = input().split(' ')
-    break_point = first_line_list[1]
+    break_point = int(first_line_list[1])
 
-    result = []
-    for line in range(int(break_point)):
-        result.append(input().split(' '))
-
-    return [first_line_list] + result
+    return [first_line_list] + [(input().split(' ')) for ln in range(break_point)]
 
 
 def highlight(target=input_sys()):
     # n回目ブロック数
     tail = target[1:]
     table = set()
-    prev_s = int(tail[0][0])
-    prev_e = int(tail[0][1])
+    prev_s, prev_e = int(tail[0][0]), int(tail[0][1])
     for n, lst in enumerate(tail):
-        curr_s = int(tail[n][0])
-        curr_e = int(tail[n][1])
+        curr_s, curr_e = int(tail[n][0]), int(tail[n][1])
+        contain_s, contain_e = curr_s in table, curr_e in table
 
-        contain_s = curr_s in table
-        contain_e = curr_e in table
-
-        if contain_s and contain_e and curr_e < prev_e and prev_s < curr_s \
+        if contain_s and contain_e and \
+                curr_e < prev_e and prev_s < curr_s \
                 or curr_e < prev_e and prev_s == curr_s \
                 or curr_e == prev_e and prev_s < curr_s:
-            for i in range(curr_s, curr_e + 1):
-                table.remove(i)
+            [table.remove(i) for i in range(curr_s, curr_e + 1)]
         else:
-            for i in range(curr_s, curr_e + 1):
-                table.add(i)
+            [table.add(i) for i in range(curr_s, curr_e + 1)]
 
         if len(table) > 0:
-            prev_s = min(table)
-            prev_e = max(table)
+            prev_s, prev_e = min(table), max(table)
 
     return len(table)
 
