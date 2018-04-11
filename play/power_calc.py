25eaa94e1943fb869755df51da939a234efe252a
jang <jang_inhwa@microad.co.jp>
2018-04-11 12:11:41 +0900

    docstring refactor

--- play/power_calc.py
+++ play/power_calc.py
@@ -1,24 +1,12 @@
-# O(n)
-def power_calc(x, n):
-    if n % 2 == 0:
-        return (x ** 2) ** int(n / 2)
-    else:
-        return x * (x ** (n - 1))
-
-
-print(power_calc(3, 11))
-
-"""
-高速累乗計算
-最低でも2回に1回は n のビット数が減る
-
-x: 基数
-n: 乗
-"""
-
-
-# O(log n)
 def power(x, n):
+    """
+    高速累乗計算 O(log n)
+    最低でも2回に1回は n のビット数が減る
+
+    :param x: 基数
+    :param n: 乗
+    :return:
+    """
     if n is 0:  # exit case
         return 1
     if n % 2 is 0:  # n が偶数: 半分にして次は偶数か奇数に
@@ -28,3 +16,14 @@ def power(x, n):
 
 
 print(power(3, 11))
+
+
+# O(n)
+def power_calc(x, n):
+    if n % 2 == 0:
+        return (x ** 2) ** int(n / 2)
+    else:
+        return x * (x ** (n - 1))
+
+
+print(power_calc(3, 11))
