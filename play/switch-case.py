a2edbbd374dc562de2ee2eed7f3e5868b30de73b
b1ueskydragon <jang_inhwa@microad.co.jp>
2018-04-11 22:12:52 +0900

    Switch case like function

---
+++ play/switch-case.py
@@ -0,0 +1,13 @@
+import re
+
+my_tuple = (
+    ("t1", "path1"),
+    ("t2", "path2"),
+    ("t3", "path3"),
+    ("t4", "path4")
+)
+
+my_func = lambda s: next(v for k, v in my_tuple if re.match(k, s))
+
+print(my_func("t1"))
+print(my_func("t3"))
