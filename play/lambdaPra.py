30ab153ebff2727a76bff6d33ed1ffa9c77dadfd
b1ueskydragon <jang_inhwa@microad.co.jp>
2018-04-12 00:25:05 +0900

    Old one

--- play/lambdaPra.py
+++ play/lambdaPra.py
@@ -1,22 +1,23 @@
 # これが
-msg = "いはっるてからさっんしーゃ、い"
+msg = "さたしべすにせいそこすうし"
 msg = msg[1::2] + msg[::2]
 print(msg)
 
 # こうなる
-msg = (lambda msg: msg[1::2] + msg[::2])("いはっるてからさっんしーゃ、い")
-print(msg)
+msg = (lambda msg: msg[1::2] + msg[::2])
+print(msg("さたしべすにせいそこすうし"))
 
 print((lambda txt: txt[::-1][::2] + txt[1::2])(" -G_M-O"))
 
 kbd = "新しいキーボードが届いたので試します。文字数は偶数なのかな?"
 print((lambda kbd: len(kbd) % 2 == 0)(kbd))
 
-
 '''
 以下、参考しましたー
 http://www.sejuku.net/blog/23677
 '''
+
+
 # lambda式を使わずに通常の関数を用いる場合
 def func(price, tax):
     return price + (price * tax)
