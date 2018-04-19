285bf033ba3646d86385c2050c00a17164c1c105
jang <jang_inhwa@microad.co.jp>
2018-04-19 20:43:37 +0900

    WIP add comment

--- dailyOne/P37/power_set.py
+++ dailyOne/P37/power_set.py
@@ -1,19 +1,27 @@
-def power_set(lstset):
+def power_set(origin):
     """
     [ [], [], ... [] ] の形で出力する.
 
-    :param lstset: set 想定の list
+    :param origin: set 想定の list
     :return: list の list
     """
-    if not lstset:  # base case: is empty
+    if not origin:  # exit case: origin(==tail) is empty
         return [[]]
     else:
-        tail = power_set(lstset[1:])
-        tmp = []
-        for el in tail:
-            head = [lstset[0]]
-            tmp += [head + el]
-        return tail + tmp
+        tail = power_set(origin[1:])  # result
+        tmp = []  # 再帰の度に初期化 (for 文のため溜めている)
+        print("tail: ", tail)
+        for subtail in tail:  # subtail is a list
+            head = [origin[0]]  # 再帰の際に引数で渡した元origin[1:]のhead
+            print("subtail: ", subtail, "  head: ", head)
+            tmp += [subtail + head]
+            print("tmp: ", tmp)
+
+        current = tail + tmp
+        print("current: ", current, "\n")
+        return current  # kept the same result
 
 
-print(power_set(['a', 'b', 'c']))
+print(power_set(['a', 'b']))
+
+# TODO Replace to list comprehension
