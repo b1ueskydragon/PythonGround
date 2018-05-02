35b843671408f135615e3b341a7239d6d9aaeaf1
jang <jang_inhwa@microad.co.jp>
2018-05-02 14:52:42 +0900

    Simplify: change type list to void

--- 99py/P26.py
+++ 99py/P26.py
@@ -1,23 +1,20 @@
+outer = []
+
+
 def combinations(n, origin):
-    rst = []
-    c_helper(outer=rst, each_stk=[], stk_len=n, current=origin)
-
-    return rst
+    c_helper([], n, origin)
+    return outer
 
 
-def c_helper(outer, each_stk, stk_len, current):
+def c_helper(each_stk, stk_len, current):
     if len(each_stk) is stk_len:
         outer.append(each_stk)
         return outer
 
     generatable = len(current) + len(each_stk) >= stk_len
-
-    if not generatable:
-        return outer
-
     if generatable:
-        c_helper(outer, [current[0]] + each_stk, stk_len, current[1:])
-        return c_helper(outer, each_stk, stk_len, current[1:])
+        c_helper([current[0]] + each_stk, stk_len, current[1:])
+        c_helper(each_stk, stk_len, current[1:])
 
 
 print(combinations(2, ['a', 'b', 'c']))
