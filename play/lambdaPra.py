768a6d1f5ff574e6e71416a55e861acca5c4b309
jang <jang_inhwa@microad.co.jp>
2018-05-16 10:58:17 +0900

    lambda with map

--- play/lambdaPra.py
+++ play/lambdaPra.py
@@ -1,24 +1,16 @@
-# これが
 msg = "さたしべすにせいそこすうし"
 msg = msg[1::2] + msg[::2]
 print(msg)
 
-# こうなる
 msg = (lambda msg: msg[1::2] + msg[::2])
 print(msg("さたしべすにせいそこすうし"))
 
-print((lambda txt: txt[::-1][::2] + txt[1::2])(" -G_M-O"))
+print((lambda txt: txt[::-1][::2] + txt[1::2])(" -v_v-v"))
 
 kbd = "新しいキーボードが届いたので試します。文字数は偶数なのかな?"
 print((lambda kbd: len(kbd) % 2 == 0)(kbd))
 
-'''
-以下、参考しましたー
-http://www.sejuku.net/blog/23677
-'''
 
-
-# lambda式を使わずに通常の関数を用いる場合
 def func(price, tax):
     return price + (price * tax)
 
@@ -26,6 +18,9 @@ def func(price, tax):
 payment1 = func(100, 0.08)
 print(payment1)
 
-# lambda式を用いる場合
 payment2 = (lambda price, tax: price + (price * tax))(100, 0.08)
 print(payment2)
+
+xy = [[3, 5, 1], [4, 2, 9]]
+ans = list(map(lambda x, y: x ** 2 + y, *xy))
+print(ans)
