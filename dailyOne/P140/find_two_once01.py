0028f65b1700dd39c5f12aef2c8a7a1296516d6b
b1ueskydragon <dragoalie@gmail.com>
2018-06-30 09:39:05 +0900

    Refactor and separate version

--- dailyOne/P140/find_two_once01.py
+++ dailyOne/P140/find_two_once01.py
@@ -1,8 +1,18 @@
-def find_two_once(ary):
-    """
-    use constant space edition.
-    """
+# { k : cnt } mapping
+# { cnt : (k1, k2 ..) } mapping ?
+# table = {2: set(), 1: set()}
+def find_two_once_01(ary):
+    table = {}
+    for k in ary:
+        cnt = 1
+        if table.get(k) is 1:
+            cnt += 1
+        table.update({k: cnt})
+
+    rst = []
+    [rst.append(kc) for kc in table if table.get(kc) is 1]
+    return rst
 
 
 given = [2, 4, 6, 8, 10, 2, 6, 10]
-print(find_two_once(given))
+print(find_two_once_01(given))
---
+++ dailyOne/P140/find_two_once02.py
@@ -0,0 +1,9 @@
+def find_two_once(ary):
+    """
+    use constant space edition.
+    """
+
+
+given = [2, 4, 6, 8, 10, 2, 6, 10]
+print(find_two_once(given))
+
