23e31c233d676673d63deed9b31f1b70c90c03a7
jang <jang_inhwa@microad.co.jp>
2018-07-03 17:34:47 +0900

    TODO Debug more, Use for-comprehension

--- dailyOne/P138/coins_dp.py
+++ dailyOne/P138/coins_dp.py
@@ -1,2 +1,35 @@
-cents = [1, 5, 10, 25]
-# TODO with dp
+units = [1, 5, 10, 25]
+
+
+def count_min_dp(n):
+    """
+     run O(n) time and space.
+
+    :param n: make n ¢
+    :return:
+    """
+    cache = [0 for _ in range(n + 1)]
+    for u in units:
+        if u < len(cache):
+            cache[u] = 1
+
+    for i in range(1, n + 1):
+        '''
+        e.g.
+        f is result. f(n¢).
+
+        8¢ = 1¢ + f(7¢)
+        8¢ = 5¢ + f(3¢)
+        '''
+        # TODO migrate to for-comprehension
+        var = []
+        for u in units:
+            if i >= u:
+                var.append(1 + cache[i - u])
+
+        cache[i] = min(var)
+
+    return cache[n]
+
+
+print(count_min_dp(n=int(input())))
