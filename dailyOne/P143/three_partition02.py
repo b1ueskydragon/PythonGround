fddea19deda4d3949ab42c1da3a36dd26afca1b2
b1ueskydragon <dragoalie@gmail.com>
2018-07-10 00:04:47 +0900

    I found bug

--- dailyOne/P143/three_partition02.py
+++ dailyOne/P143/three_partition02.py
@@ -6,14 +6,26 @@ def three_partition(x, lst):
     :return:
     """
     left, right = 0, 0
-    center = len(lst) - 1
+    last = len(lst) - 1
 
-    while left < right:
+    while right < last:
         if lst[right] == x:
-            pass
+            right += 1
         elif lst[right] < x:
-            pass
+            swap_value(lst, left, right)
+            left += 1
+            right += 1
         else:
-            pass
+            swap_value(lst, last, right)
+            last -= 1
 
     return lst
+
+
+def swap_value(lst, i1, i2):
+    lst[i1], lst[i2] = lst[i2], lst[i1]
+
+
+# x, lst = 10, [9, 12, 3, 5, 14, 10, 10]
+x, lst = 2, [4, 1, 1, 2, 1, 1, 3]  # TODO Bug found
+print(three_partition(x, lst))
