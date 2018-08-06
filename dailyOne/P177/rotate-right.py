dbb36967469fe65b17187a1ad44333cd0a2a1ca3
Ina <dragoalie@gmail.com>
2018-08-06 12:46:56 +0900

    Add prev pointer

--- dailyOne/P177/rotate-right.py
+++ dailyOne/P177/rotate-right.py
@@ -13,18 +13,38 @@ class LinkedList:
 
     def add_last(self, el):
         tmp_lst = self.last
-        node = Node(el)
+        node = Node(el)  # new Node
         self.last = node
         if not tmp_lst:
             self.head = node
         else:
+            self.last.prev = tmp_lst
             tmp_lst.next = node
         self.size += 1
 
+    def remove_and_get_last(self):
+        if not self.last:
+            raise NotImplementedError
+        else:
+            tmp = self.last
+            self.last = None
+            self.size -= 1
+        return tmp
 
-given = LinkedList()
-given.add_last(5)
-given.add_last(4)
-given.add_last(3)
 
-print(given.head.next.next.value)
+if __name__ == '__main__':
+    given = LinkedList()
+
+    # TODO impl with input
+    given.add_last(1)
+    given.add_last(2)
+    given.add_last(3)
+    given.add_last(4)
+    given.add_last(5)
+    k = 3
+    # 1,2,3,4,5 (ori)
+
+    # 5,1,2,3,4
+    # 4,5,1,2,3
+    # 3,4,5,1,2 (res)
+    print(given.head.next.prev.next.next.prev.prev is given.head)
