c4528a35ac955aa87a2bbc8ba17ff437c5412451
b1ueskydragon <dragoalie@gmail.com>
2018-08-08 01:19:30 +0900

    TODO: Using queue edition

---
+++ dailyOne/P180/a-stack-a-queue01.py
@@ -0,0 +1,19 @@
+from queue import Queue
+import math
+
+
+def interleave_half(stack):
+    """
+    -1th
+    S [3,2,1]
+    Q [5,4]
+
+    S [1,5,2,4,3]
+    """
+    q = Queue
+    size = len(stack)
+    pivot = size // 2
+
+    # TODO use a `Queue`
+
+    return stack
