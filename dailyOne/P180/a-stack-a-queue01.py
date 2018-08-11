7f620312f2b55950bd81d7b82d88772d2e953db6
jang <dragoalie@gmail.com>
2018-08-11 23:06:53 +0900

    TODO: debug more

--- dailyOne/P180/a-stack-a-queue01.py
+++ dailyOne/P180/a-stack-a-queue01.py
@@ -8,16 +8,33 @@ def interleave_half(stack):
     S [3,2,1]
     Q (5,4)
 
+    Q (1,5,2,4,3)
     S [1,5,2,4,3]
     """
     qu = Queue()
     size = len(stack)
 
+    pivot = size // 2
+    move = int(math.ceil(size / 2))  # `pivot + 1` if is odd
+
     while stack:
         qu.put(stack.pop())
 
-    for _ in range(int(size / 2)):
+    for _ in range(pivot):
         qu.put(qu.get())
+
+    for _ in range(move):
+        stack.append(qu.get())
+
+    for _ in range(pivot):
+        qu.put(stack.pop())
+        qu.put(qu.get())
+
+    if stack:
+        qu.put(stack.pop())
+
+    while not qu.empty():
+        stack.append(qu.get())
 
     return stack
 
@@ -26,3 +43,4 @@ given_odd = [1, 2, 3, 4, 5]
 given_even = [1, 2, 3, 4]
 
 print(interleave_half(given_odd))
+print(interleave_half(given_even))
