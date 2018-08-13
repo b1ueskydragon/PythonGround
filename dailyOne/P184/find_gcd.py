941eb4617c2ee636bc313514b4b4e6c1b36019ed
b1ueskydragon <dragoalie@gmail.com>
2018-08-13 10:11:16 +0900

    Add quiz

---
+++ dailyOne/P184/find_gcd.py
@@ -0,0 +1,16 @@
+"""
+Given n numbers, find the greatest common denominator between them.
+"""
+
+
+def find_gcd(given):
+    pass
+
+def is_prime(num):
+    pass
+
+# isPrime ? (return 1 if, all of elements are prime)
+# primes ? (else, division with primes continuously)
+
+given = [42, 56, 14]
+print(find_gcd(given))
