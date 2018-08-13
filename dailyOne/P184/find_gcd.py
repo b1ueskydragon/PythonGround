96ee561187d5768713cd70b84afa30fbdda2bf0f
b1ueskydragon <dragoalie@gmail.com>
2018-08-14 00:00:18 +0900

    first thought

--- dailyOne/P184/find_gcd.py
+++ dailyOne/P184/find_gcd.py
@@ -4,10 +4,30 @@ Given n numbers, find the greatest common denominator between them.
 
 
 def find_gcd(given):
-    pass
+    """
+    O(k*N)
+    k = a size of head element of given list
+    N = an length of given list
+    """
+
+    return 1
+
 
 def is_prime(num):
-    pass
+    if num < 2 or (num is not 2 and num % 2 is 0):
+        return False
+    for _ in range(3, num, 2):
+        if num % _ is 0:
+            return False
+        else:
+            continue
+    return True
+
+
+def primes(num):
+    return [i for i in reversed(range(num + 1)) if is_prime(i)]
+
+print(primes(42))
 
 # isPrime ? (return 1 if, all of elements are prime)
 # primes ? (else, division with primes continuously)
