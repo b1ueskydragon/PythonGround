6ebc6192a980da991886e2797b304f71cf687ad2
jang <dragoalie@gmail.com>
2018-08-30 14:25:07 +0900

    [WIP] with count digits

--- dailyOne/P202/palindrome.py
+++ dailyOne/P202/palindrome.py
@@ -1,7 +1,25 @@
+import math
+
+
 def is_palindrome(given):
     # TODO Pythagoras
     # String(x) List(o) ?
     return False
 
 
-print(is_palindrome(int(input())))
+# print(is_palindrome(int(input())))
+
+# Positive integers only
+digit = lambda k: int(math.log10(k))
+
+given = 12321
+
+print(given)
+print(10 ** digit(given))
+
+given = given - (10 ** digit(given))
+print(given)
+print(10 ** digit(given))
+given = given - (10 ** digit(given))
+
+print(given)
