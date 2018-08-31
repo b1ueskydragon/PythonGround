81fa4b70908c3dabeb6c3145dc19cb53b68f4cec
jang <dragoalie@gmail.com>
2018-08-31 17:36:38 +0900

    [WIP] TODO fix bug

--- dailyOne/P202/palindrome.py
+++ dailyOne/P202/palindrome.py
@@ -1,23 +1,36 @@
 import math
 
 # Positive integers only
-digit = lambda k: int(math.log10(k))
+digit_len = lambda k: int(math.log10(k))
 
 
-def is_palindrome(given):
-    pivot = math.ceil(digit(given) / 2)
+def digit_to_list(given):
+    ori_len = digit_len(given)
     acc = []
-
-    for i, _ in enumerate(reversed(range(digit(given) + 1))):
-        if i == pivot:
-            break  # TODO reversed half
-
-        d = (10 ** digit(given))
-        k = given // (10 ** digit(given))
+    for _ in reversed(range(ori_len + 1)):
+        d = 10 ** digit_len(given)  # TODO math domain error when d is 0
+        k = given // d
         given -= d * k
         acc.append(k)
 
     return acc
 
 
-print(is_palindrome(int(input())))
+def is_palindrome(dist):
+    ori_len = len(dist)
+    pivot = math.ceil(ori_len / 2) - 1
+    acc, rcc = [], []
+
+    for i, d in enumerate(dist):
+        if i > pivot:
+            rcc.append(d)
+        else:
+            acc.append(d)
+
+    if ori_len % 2 != 0:
+        acc.pop(int(pivot))
+
+    return acc == list(reversed(rcc))
+
+
+print(is_palindrome(digit_to_list(int(input()))))
