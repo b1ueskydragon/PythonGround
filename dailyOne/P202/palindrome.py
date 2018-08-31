1e30ce6a6228303032d5b409fa89d7195e8e7e4d
jang <dragoalie@gmail.com>
2018-08-31 11:38:40 +0900

    [WIP] to list ?

--- dailyOne/P202/palindrome.py
+++ dailyOne/P202/palindrome.py
@@ -1,25 +1,20 @@
 import math
 
-
-def is_palindrome(given):
-    # TODO Pythagoras
-    # String(x) List(o) ?
-    return False
-
-
-# print(is_palindrome(int(input())))
-
 # Positive integers only
 digit = lambda k: int(math.log10(k))
 
-given = 12321
 
-print(given)
-print(10 ** digit(given))
+def is_palindrome(given):
+    backup = given # required ?
+    acc = []
 
-given = given - (10 ** digit(given))
-print(given)
-print(10 ** digit(given))
-given = given - (10 ** digit(given))
+    for _ in reversed(range(digit(given) + 1)):
+        d = (10 ** digit(given))
+        k = given // (10 ** digit(given))
+        given -= d * k
+        acc.append(k)
 
-print(given)
+    return acc
+
+
+print(is_palindrome(int(input())))
