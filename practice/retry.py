48bc3f9a0aef197401fc5d23ef624c186289e917
jang <dragoalie@gmail.com>
2018-09-04 12:48:54 +0900

    Add using enumerate edition

--- practice/retry.py
+++ practice/retry.py
@@ -1,5 +1,9 @@
 import time
 
+given = 5
+
+print('[test00]')
+
 
 def retry_test(given):
     flag = given  # Try input bound integers (given ± 1)
@@ -17,7 +21,31 @@ def retry_test(given):
             time.sleep(1)
 
         if cnt is given:
+            print(cnt)
             print(f"Retry {given} all times.")
 
 
-retry_test(given=5)
+retry_test(given)
+
+print('[test01]')
+
+
+def retry_test_idx(given):
+    flag = given  # Try input bound integers (given ± 1)
+    for i, _ in enumerate(range(given), start=1):
+        try:
+            if i is flag:
+                print('success')
+                break
+            else:
+                3 // 0  # Error excepted.
+        except:
+            print('error')
+            time.sleep(1)
+
+        if i is given:
+            print(i)
+            print(f"Retry {given} all times.")
+
+
+retry_test_idx(given)
