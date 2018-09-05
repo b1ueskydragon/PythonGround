8a0b1617e9ec508681b518e44aca8c214b09e3eb
jang <dragoalie@gmail.com>
2018-09-05 13:25:00 +0900

    [WIP]

--- dailyOne/P202/palindrome02.py
+++ dailyOne/P202/palindrome02.py
@@ -2,15 +2,31 @@ import math
 
 
 def is_palindrome(num):
+    if num < 0:
+        return False
+
     rem = num % 10
     num //= 10
 
+    if num is 0:
+        return True
+    if rem is 0:
+        return False
+
     digit = int(math.log10(num))
     rem = rem * (10 ** digit)
+    num -= rem
 
-    num = num - rem
+    if num is 0:
+        return True
 
-    return rem, num
+    if num < 0:
+        return False
+
+    if int(math.log10(num)) >= digit:
+        return False
+
+    return True
 
 
-print(is_palindrome(52325))
+print(is_palindrome(19000001))
