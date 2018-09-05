797a6b6cbfa7b6dfda1d8e9c192780fc9a1159b5
jang <dragoalie@gmail.com>
2018-09-05 13:42:20 +0900

    [WIP] non use log10

--- dailyOne/P202/palindrome02.py
+++ dailyOne/P202/palindrome02.py
@@ -1,34 +1,10 @@
-import math
-
-digit = lambda k: int(math.log10(k)) if k > 0 else 0
-
-
-# TODO Do not use log10 to find digit
 def is_palindrome(num):
-    while num:
-        if num < 0:
-            return False
-
-        rem = num % 10
+    digit = 1
+    while num > 10:
+        digit *= 10
         num //= 10
 
-        if num is 0:
-            return True
-        if rem is 0:
-            return False
-
-        bef = digit(num)
-        rem = rem * (10 ** bef)
-        num -= rem
-        aft = digit(num)
-
-        if num < 0:
-            return False
-
-        if aft is bef:
-            return False
-
-    return True
+    return num, digit
 
 
-print(is_palindrome(1010550101))
+print(is_palindrome(9010550109))
