8311d7fae6fca30c0499ef6a400b3a5723f22358
jang <dragoalie@gmail.com>
2018-09-05 16:13:53 +0900

    [WIP] Another approach without subtraction

--- dailyOne/P202/palindrome02.py
+++ dailyOne/P202/palindrome02.py
@@ -1,10 +1,24 @@
+"""
+Using subtraction should be avoided.
+ - if most significant digit is 0, loss of significant digits occurs.
+ - e.g) 909909
+        909909 % 10 = 9
+        909909 // 10 = 90990
+        90990 - 90000 = (0)990
+"""
+
+
 def is_palindrome(num):
     digit = 1
-    while num > 10:
+    while (num // digit) >= 10:
         digit *= 10
-        num //= 10
 
-    return num, digit
+    most = num // digit
+    least = num % 10
+    if most != least:
+        return False
+
+    return True
 
 
-print(is_palindrome(9010550109))
+print(is_palindrome(909909))
