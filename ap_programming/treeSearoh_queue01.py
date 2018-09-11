e3d46885d703cf16ec4c716bd560af2acee3ac17
jang <dragoalie@gmail.com>
2018-09-11 20:12:45 +0900

    [WIP] put queue res instead of curr🤔

--- ap_programming/treeSearoh_queue01.py
+++ ap_programming/treeSearoh_queue01.py
@@ -5,21 +5,29 @@ from queue import Queue
 
 
 def treeSearoh(given, target):
+    """
+    O(2^n)
+    """
     queue = Queue()
     i = 0
-    curr_sum = given[i]  # sum til current
+    curr_sum = 0  # sum til current
     res_sum = curr_sum  # candidate
-    last = len(given) - 1
+    size = len(given)
 
     queue.put(curr_sum)
     while not queue.empty():
         curr_sum = queue.get()
-        if abs(target - res_sum) > abs(target - curr_sum):
+
+        # variables for debug
+        res_error = abs(target - res_sum)
+        curr_error = abs(target - curr_sum)
+
+        if res_error > curr_error:
             res_sum = curr_sum
-        elif curr_sum < target and i is not last:
-            i += 1  # TODO 一周しかしない (2^n 探索必要)
-            queue.put(curr_sum + given[i])
-            queue.put(curr_sum)
+        elif curr_sum < target and i < size:
+            queue.put(res_sum + given[i])
+            queue.put(res_sum)
+            i += 1
 
     return max(curr_sum, res_sum)
 
