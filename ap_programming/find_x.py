fe9a261c519a13b9f4faa5cea108f08beeb395c5
b1ueskydragon <dragoalie@gmail.com>
2018-09-12 00:59:36 +0900

    Done - TODO improvement speed

---
+++ ap_programming/find_x.py
@@ -0,0 +1,32 @@
+"""
+Arranged edition of `treeSearoh`
+"""
+
+
+def find_x(nums, x):
+    cache = 0  # candidate of sum result on overall
+    for i in range(len(nums)):
+        curr = 0  # candidate of sum result in this list
+        for num in nums[i:]:
+            inc_next = curr + num  # select next
+            exc_next = curr  # not select next
+
+            # select exc_next vs inc_next
+            inc_error = abs(x - inc_next)
+            exc_error = abs(x - exc_next)
+            if inc_error > exc_error:
+                curr = exc_next
+            else:
+                curr = inc_next
+
+        # select cache vs res_sum
+        cache_error = abs(x - cache)
+        res_error = abs(x - curr)
+        if cache_error > res_error:
+            cache = curr
+
+    return cache
+
+
+given, target = list(reversed(sorted([10, 34, 55, 77]))), 100  # [55 + 34 + 10]
+print(find_x(given, target))
