8e265749a30b66f58cc32f5e9fba89047ce44103
b1ueskydragon <dragoalie@gmail.com>
2018-09-25 21:50:37 +0900

    [WIP] Add possible cases

--- dailyOne/P203/find_min_in_rotated_list.py
+++ dailyOne/P203/find_min_in_rotated_list.py
@@ -1,7 +1,15 @@
 def find_min(nums):
     # O(log N) -> half, half, half ...
+    # half-division logic ?
     return
 
 
-given = [5, 7, 10, 3, 4]
-print(find_min(nums=given))  # 3
+given = [2, 3, 4, 5, 1]
+print(find_min(given))  # 1
+
+# given = [5, 7, 10, 3, 4]
+# print(find_min(given))  # 3
+# given = [5, 7, 3, 4]
+# print(find_min(given))  # 3
+# given = [4, 5, 1, 2, 3]
+# print(find_min(given))  # 1
