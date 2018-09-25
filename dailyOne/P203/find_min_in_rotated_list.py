c14da787e21f0369d98d2c8cecb902d18dfc1266
b1ueskydragon <dragoalie@gmail.com>
2018-09-26 00:00:51 +0900

    [WIP] zzz...🌛

--- dailyOne/P203/find_min_in_rotated_list.py
+++ dailyOne/P203/find_min_in_rotated_list.py
@@ -1,25 +1,35 @@
 def find_min(nums):
-    # O(log N) -> half, half, half ...
-    # half-division logic ?
+    size = len(nums)
+    head = nums[0]
+    last = nums[size - 1]
+    pivot = size // 2  # TODO pivot ?
 
-    # find max at first
+    if head < last:
+        nums = nums[:pivot]
+    else:
+        nums = nums[pivot:]
 
-    pivot = len(nums) // 2
+    if size <= 1:
+        return head
+    else:
+        return find_min(nums)
 
-    lh = min(nums[:pivot])
-    rh = min(nums[pivot:])
-
-    # ↑ min, max is O(N)
-
-    return min(lh, rh)
+    # TODO find `max`
 
 
-given = [2, 3, 4, 5, 1]
-print(find_min(given))  # 1
-
+# given = [2, 3, 4, 5, 1]
+# print(find_min(given) == min(given))  # 1
+# given = [3, 4, 5, 6, 1, 2]
+# print(find_min(given) == min(given))  # 1
+# given = [0, 2, 4, 6, 8, 10]
+# print(find_min(given) == min(given))  # 0
 # given = [5, 7, 10, 3, 4]
-# print(find_min(given))  # 3
+# print(find_min(given) == min(given))  # 3
 # given = [5, 7, 3, 4]
-# print(find_min(given))  # 3
+# print(find_min(given) == min(given))  # 3
 # given = [4, 5, 1, 2, 3]
-# print(find_min(given))  # 1
+# print(find_min(given) == min(given))  # 1
+
+# TODO
+given = [_ for _ in range(100, 200)] + [_ for _ in range(50, 100)]
+print(find_min(given) == min(given))  # 50
