0c243e6ee9b309a0fd6e1b64c3fd2db4132523db
jang <dragoalie@gmail.com>
2018-09-26 16:55:16 +0900

    🤔

--- dailyOne/P203/find_min_in_rotated_list.py
+++ dailyOne/P203/find_min_in_rotated_list.py
@@ -1,22 +1,14 @@
 def find_min(nums):
     size = len(nums)
-    pivot = size // 2
-    lhc = 0
-    rhc = pivot
 
-    # candidate
-    res = nums[lhc]
-
-    # left half
     for i, num in enumerate(nums, start=1):
-        if num > nums[i]:
-            print(num, nums[i])
-            break
-
-    # right half
+        if i == size:
+            return nums[0]
+        elif num > nums[i]:
+            return nums[i]
 
 
 given = [_ for _ in range(100, 200)] + [_ for _ in range(50, 100)]
-
 print(find_min(given))
-# print(min(given))
+given = [_ for _ in range(50, 200)]
+print(find_min(given))
