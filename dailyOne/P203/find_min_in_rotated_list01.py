1a3576a98583df2dd8a16f6e17a0dc9abc30eb1b
jang <dragoalie@gmail.com>
2018-09-27 17:26:23 +0900

    [TODO] Another edition

---
+++ dailyOne/P203/find_min_in_rotated_list01.py
@@ -0,0 +1,17 @@
+def find_min(nums):
+    """
+    Another edition.
+    O(log N)
+    """
+    # TODO call binary
+
+
+def binary():
+    return
+
+
+# TODO set more bigger range ...
+given = [_ for _ in range(1000, 2000)] + [_ for _ in range(400, 402)] + [_ for _ in range(500, 900)]
+print(find_min(given))
+given = [_ for _ in range(50, 200)]
+print(find_min(given))
