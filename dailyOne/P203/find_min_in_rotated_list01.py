213a384babcd2854ac3a299152d88e27d5962a75
jang <dragoalie@gmail.com>
2018-09-28 12:04:36 +0900

    [WIP]

--- dailyOne/P203/find_min_in_rotated_list01.py
+++ dailyOne/P203/find_min_in_rotated_list01.py
@@ -3,15 +3,22 @@ def find_min(nums):
     Another edition.
     O(log N)
     """
-    # TODO call binary
+    # TODO call binary_search
 
 
-def binary():
-    return
+def binary_search(arr, low, high):
+    if low == high:
+        return arr[low]
 
+    pivot = (low + high) // 2
+    if arr[pivot] < arr[high]:
+        high = pivot
+    else:
+        low = pivot + 1
 
-# TODO set more bigger range ...
-given = [_ for _ in range(1000, 2000)] + [_ for _ in range(400, 402)] + [_ for _ in range(500, 900)]
-print(find_min(given))
-given = [_ for _ in range(50, 200)]
-print(find_min(given))
+    return binary_search(arr, low, high)
+
+# given = [_ for _ in range(1000, 2000)] + [_ for _ in range(400, 402)] + [_ for _ in range(500, 900)]
+# print(find_min(given))
+# given = [_ for _ in range(50, 200)]
+# print(find_min(given))
