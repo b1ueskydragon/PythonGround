170d62be3a27b715a9ea6bd46072352268c17b94
b1ueskydragon <dragoalie@gmail.com>
2018-09-29 12:26:54 +0900

    Add quiz🌝

---
+++ dailyOne/P232/prefix_mapsum.py
@@ -0,0 +1,11 @@
+class PrefixMapSum:
+    def __init__(self):
+        self.stack = []
+
+    def insert(self, key, value):
+        # TODO Set or overwrite a given key's value in the map
+        return
+
+    def sum(self, prefix):
+        # TODO Return the sum of all values of keys that begin with a given prefix
+        return
