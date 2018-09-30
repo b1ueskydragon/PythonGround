416c065345306d1d054910f1e797416f6f3d8f3b
b1ueskydragon <dragoalie@gmail.com>
2018-09-30 19:55:55 +0900

    Done with basic approach

--- dailyOne/P232/prefix_mapsum.py
+++ dailyOne/P232/prefix_mapsum.py
@@ -1,11 +1,33 @@
 class PrefixMapSum:
     def __init__(self):
-        self.stack = []
+        self.map = {}
+        # self.enroll = {} # TODO memoing existing search result
 
     def insert(self, key, value):
-        # TODO Set or overwrite a given key's value in the map
-        return
+        self.map[key] = value
 
     def sum(self, prefix):
-        # TODO Return the sum of all values of keys that begin with a given prefix
-        return
+        # should be local variables (Do not setting with params)
+        local_res, local_cache = set(), 0
+
+        for key in self.map.keys():
+            if key.startswith(prefix):
+                local_res.add(key)
+
+        for hit in local_res:
+            local_cache += self.map[hit]
+
+        return local_cache
+
+
+mapsum = PrefixMapSum()
+mapsum.insert("columnar", 3)
+print(mapsum.sum("col"))  # 3
+mapsum.insert("column", 2)
+mapsum.insert("cool", 4)
+print(mapsum.sum("co"))  # 9
+print(mapsum.sum("col"))  # 5
+print(mapsum.sum("A"))  # 0
+mapsum.insert("Alex", 1)
+print(mapsum.sum("A"))  # 1
+print(mapsum.sum("col"))  # 5
