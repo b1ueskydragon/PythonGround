054e765e06a20fb30de41f7c0cb2cb08862badc6
b1ueskydragon <dragoalie@gmail.com>
2018-09-30 22:53:58 +0900

    [WIP] Outline

---
+++ math/linear/associative.py
@@ -0,0 +1,19 @@
+"""
+Q: p.08 (10)
+
+(AB)C == AB(C)
+
+A(n,m) idx of elem (i,j)
+B(m,l) idx of elem (j,k)
+C(l,p) idx of elem (k,h)
+"""
+
+
+# TODO with numpy
+def sum(i, h, m, l):
+    for j in range(m + 1):
+        for k in range(l + 1):
+            print(i, j, k, h)
+
+
+sum('i', 'h', 3, 5)
