fa15e02db367ad681917353faa0e8d10ffdf3a44
jang <dragoalie@gmail.com>
2018-10-01 19:35:51 +0900

    Easy one

--- math/linear/associative.py
+++ math/linear/associative.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 Q: p.08 (10)
 
@@ -8,12 +10,38 @@ B(m,l) idx of elem (j,k)
 C(l,p) idx of elem (k,h)
 """
 
+A = np.array([
+    [1, 2, 3],
+    [4, 5, 6]
+])
 
-# TODO with numpy
-def sum(i, h, m, l):
-    for j in range(1, m + 1):
-        for k in range(1, l + 1):
-            print(i, j, k, h)
+B = np.array([
+    [1, 2],
+    [3, 4],
+    [5, 6]
+])
+
+C = np.array([
+    [1, 2, 3]
+])
 
 
-sum('i', 'h', 3, 5)
+def res(matrix1, matrix2):
+    sum1, sum2, sum3, sum4 = [], [], [], []
+
+    for i, a in enumerate(matrix1):
+        for j, b in enumerate(matrix2):
+            if len(sum1) < 3:
+                sum1.append(a[j] * b[0])
+                sum2.append(a[j] * b[1])
+            else:
+                sum3.append(a[j] * b[0])
+                sum4.append(a[j] * b[1])
+
+    return np.array([
+        [sum(sum1), sum(sum2)],
+        [sum(sum3), sum(sum4)]
+    ])
+
+
+print(res(A, B))
