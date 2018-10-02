ed7a7bf3a7c782a48b19b5c5699747bfbb0d2303
jang <dragoalie@gmail.com>
2018-10-02 13:31:04 +0900

    [WIP] Reset condition

--- math/linear/associative.py
+++ math/linear/associative.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 Q: p.08 (10)
 
@@ -8,40 +10,59 @@ B(m,l) idx of elem (j,k)
 C(l,p) idx of elem (k,h)
 """
 
+# (2 x 3)
 A = [
     [1, 2, 3],
     [4, 5, 6]
 ]
 
+# (3 x 2)
 B = [
     [1, 2],
     [3, 4],
     [5, 6]
 ]
 
+# (2 x 1)
 C = [
-    [1, 2, 3]
+    [10],
+    [11]
 ]
 
 
-def calc(matrix1, matrix2):
-    res = [0 for _ in range(len(matrix1) * len(matrix2[0]))]
-    pivot = len(res) // 2
+def calc1(matrix1, matrix2):
+    res = []
+    inner = [0 for _ in range(len(matrix1) * len(matrix2[0]))]
     cnt = 0
 
     for i, a in enumerate(matrix1):
         for j, b in enumerate(matrix2):
             cnt += 1
             if cnt <= len(matrix2):
-                # TODO loop
-                res[0] += a[j] * b[0]
-                res[1] += a[j] * b[1]
+                # TODO Generalization with loop
+                inner[0] += a[j] * b[0]
+                inner[1] += a[j] * b[1]
             else:
-                res[2] += a[j] * b[0]
-                res[3] += a[j] * b[1]
+                inner[2] += a[j] * b[0]
+                inner[3] += a[j] * b[1]
 
-    return res  # TODO make not flatted
+    res.append([inner[0], inner[1]])
+    res.append([inner[2], inner[3]])
+
+    return res
 
 
-print(calc(A, B))
+def calc2(matrix1, matrix2):
+    res = []
+    inner = [0 for _ in range(len(matrix1) * len(matrix2[0]))]
 
+    for i, a in enumerate(matrix1):
+        for j, b in enumerate(matrix2):
+            inner[i] += a[j] * b[0]
+        res.append([inner[i]])
+
+    return res
+
+
+print(calc1(A, B))
+print(calc2(B, C))
