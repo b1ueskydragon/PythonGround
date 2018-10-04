7f0a848b9ccbba2cfe4e7fc28d18e105a75f5e42
jang <dragoalie@gmail.com>
2018-10-04 18:04:37 +0900

    [WIP]🤔

--- dailyOne/P232/prefix_mapsum_trie.py
+++ dailyOne/P232/prefix_mapsum_trie.py
@@ -4,28 +4,43 @@ Using prefix tree (trie) edition
 """
 
 
+class Trie:
+    def __init__(self):
+        self.chars = {}
+        self.total = 0
+
+
 class PrefixMapSum:
     def __init__(self):
         self.map = {}
+        self.root = Trie()
 
-    def insert(self, key, value):
+    def insert(self, key: str, value: int):
         """
         associate each letter with a dictionary
         O(k)
         """
-        return
+        value -= self.map.get(key, 0)  # if key does not exist, return value 0
+        self.map[key] = value
 
-    def sum(self, prefix):
+        curr = self.root
+        for char in key:
+            curr = curr.chars.setdefault(char, Trie())  # if char does not exist, return Trie() to default
+            curr.total += value
+
+    def sum(self, prefix: str):
         """
         O(k)
         """
         return
 
 
-# test
-from collections import defaultdict
+mapsum = PrefixMapSum()
+mapsum.insert("columnar", 3)
+mapsum.insert("column", 2)
+mapsum.insert("col", 9)
+mapsum.insert("cool", 4)
+mapsum.insert("Alex", 1)
+mapsum.insert("columnar", 4)
 
-some = defaultdict(dict)
-some[0] = 'a'
-some[1] = 'b'
-print(some)
+print(mapsum.map)
