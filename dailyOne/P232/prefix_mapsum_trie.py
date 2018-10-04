399e8b727a69c0ee552778e4196093f4cfc66fab
jang <dragoalie@gmail.com>
2018-10-04 11:34:04 +0900

    Make outline for trie approach

---
+++ dailyOne/P232/prefix_mapsum_trie.py
@@ -0,0 +1,31 @@
+"""
+Using prefix tree (trie) edition
+  - k is the length of the prefix.
+"""
+
+
+class PrefixMapSum:
+    def __init__(self):
+        self.map = {}
+
+    def insert(self, key, value):
+        """
+        associate each letter with a dictionary
+        O(k)
+        """
+        return
+
+    def sum(self, prefix):
+        """
+        O(k)
+        """
+        return
+
+
+# test
+from collections import defaultdict
+
+some = defaultdict(dict)
+some[0] = 'a'
+some[1] = 'b'
+print(some)
