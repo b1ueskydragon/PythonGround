ce54d70616e2fcc61fb92f89c450f654a6efe4f7
b1ueskydragon <dragoalie@gmail.com>
2018-10-09 14:08:04 +0900

    [WIP] memo memo

--- dailyOne/P148/gray_code.py
+++ dailyOne/P148/gray_code.py
@@ -1,5 +1,7 @@
-def gray_code(bit: int) -> list:
-    return
+def gray_code(bit: int, codes=[]) -> list:
+    # TODO bit invert. convert binary num to gray code.
+    # 後に隣接する符号間のハミング距離が必ず1になるような順番
+    return codes
 
 
 print(gray_code(bit=2))
