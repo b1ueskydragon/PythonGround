e2fd05cf18d101aaf4f44d52f8516453f20ea9bc
jang <dragoalie@gmail.com>
2018-10-10 12:46:23 +0900

    Solving method - shift op and make xor

--- dailyOne/P148/gray_code.py
+++ dailyOne/P148/gray_code.py
@@ -1,28 +1,22 @@
 """
 Reflecting the Hamming distance of 1 between adjacent codes.
 
+In n bit, Hamming distance could be taken 2^0 ~ 2^(n-1).
 e.g) 2 bit
     - Result of XOR is 01 or 10 between adjacent codes.
 """
 
 
-def gray_code(bit: int, res=[]) -> list:
-    # Hamming distance could be taken. n bit, 2^0 ~ 2^(n-1).
-    distance = {2 ** n for n in range(bit)}
-
+def gray_code(bit: int) -> list:
     # Maximum digit could be taken
     max_n = 2 ** bit
 
-    # before convert to gray
-    ori = [_ for _ in range(max_n)]
+    # make an XOR between k and 1bit right shifted k
+    res = [k ^ (k >> 1) for k in range(max_n)]
 
     return res
 
 
 print(gray_code(bit=2))
-
-# excepted
-print(0 ^ 1)  # 01 2^0
-print(1 ^ 3)  # 10 2^1
-print(3 ^ 2)  # 01 2^0
-print(2 ^ 0)  # 10 2^1
+print(gray_code(bit=3))
+print(gray_code(bit=4))
