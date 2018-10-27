7d3d2f0d19c24eea70cc44d013744ac93ee33e98
b1ueskydragon <dragoalie@gmail.com>
2018-10-27 10:35:35 +0900

    [WIP] testing..

--- ap_programming/print_bst.py
+++ ap_programming/print_bst.py
@@ -4,6 +4,21 @@ Visualization exist BST
 
 
 # TODO
-def print_tree(root):
-    if root:
-        print(root.value)
+def print_tree(node):
+    if node:
+        print(node.value)
+        if node.left:
+            print_tree(node.left)
+        if node.right:
+            print_tree(node.right)
+
+# def print_tree(node: Node):
+#     if node:
+#         print(' ' + revert(node.codes))
+#     if node.left:
+#         blank = ''.join([' ' for _ in range(len(revert(node.codes)) - 1)])
+#         print('/' + blank + '\\')
+#         blank = ''.join([' ' for _ in range(len(revert(node.codes)) // 3)])
+#         print(revert(node.left.codes) + blank + revert(node.right.codes))
+#     if node.right:
+#         print()
