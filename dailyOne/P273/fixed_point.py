8e5bc39114a1e0b304e8808c5bba526931663d89
b1ueskydragon <dragoalie@gmail.com>
2018-11-09 10:00:02 +0900

    [WIP] fix bug in binary search, improve time

--- dailyOne/P273/fixed_point.py
+++ dailyOne/P273/fixed_point.py
@@ -1,4 +1,9 @@
-def has_fixed_point_l(ary):
+"""
+Condition: given a sorted array
+"""
+
+
+def linear_search(ary):
     """
     linear foreach.
     O(N) - n is an length of array.
@@ -9,27 +14,46 @@ def has_fixed_point_l(ary):
     return False
 
 
-def has_fixed_point_s(ary):
+def has_fixed_point_b(ary):
     """
-    TODO:
-    use Set.
-    binary search (pointer left and right).
+    O(logN)
 
-    TODO:
     use `sorted` array (Pruning).
+    TODO: binary search recursion (split, split ...)
+
+    TODO: imagine differential graph
+
+    TODO: use set edition to find missing part?
     """
-    size = len(ary)  # Make a set with max len? - not sorted one
-    pivot = size // 2 if size % 2 != 0 else size // 2 - 1
+    pivot = len(ary) // 2  # odd or even does not matter.
+    p_val = ary[pivot]
 
-    return
+    print(pivot, p_val)
+
+    if p_val < pivot:
+        ary = ary[:pivot]
+    else:
+        ary = ary[pivot:]
+
+    linear_search(ary)
+
+    return pivot if p_val == pivot else False
 
 
-# given a sorted array
-given_t = [-6, 0, 2, 40]
-given_f = [1, 5, 7, 8]
+# given_t = [-6, 0, 2, 40]
+# given_f = [1, 5, 7, 8]
+#
+# print(linear_search(given_t))
+# print(linear_search(given_f))
 
-print(has_fixed_point_l(given_t))
-print(has_fixed_point_l(given_f))
+# print(has_fixed_point_b(given_t))
+# print(has_fixed_point_b(given_f))
 
-print(has_fixed_point_s(given_t))
-print(has_fixed_point_s(given_f))
+# import random
+# target = list(sorted(random.sample(range(100), 30)))
+
+target = [-3, -2, -1, 0, 1, 2, 3, 5, 6, 7, 10, 37, 38, 40, 41, 43,
+          50, 56, 60, 61, 62, 68, 80, 83, 84, 85, 87, 92, 96, 99]
+
+print(linear_search(target))
+print(has_fixed_point_b(target))  # TODO fix bug
