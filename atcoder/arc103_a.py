ec39ca749805cd85a64e7a1f0c3e84c239957e61
b1ueskydragon <dragoalie@gmail.com>
2018-11-09 21:10:46 +0900

    Improve with collections import and sentinel

--- atcoder/arc103_a.py
+++ atcoder/arc103_a.py
@@ -1,47 +1,24 @@
 # https://beta.atcoder.jp/contests/arc103/tasks/arc103_a
 
+"""
+improvement space - more less variables
+            time  - use collections, avoid using list length or for-loop
+"""
 
-def given(_dummy=int(input())): return [int(_) for _ in input().split()]
-
-
-def solve(ary):
-    if len(set(ary)) < 2:
-        return len(ary) // 2
-
-    even, odd = [], []
-    for i, _ in enumerate(ary):
-        even.append(_) if i % 2 == 0 else odd.append(_)
-
-    even_key, odd_key = list(set(even)), list(set(odd))
-
-    res = len(ary)
-    most = 0
-
-    if len(even_key) != 1:
-        l_cache = 0
-        for n in even:
-            curr = max(l_cache, even.count(n))
-            if l_cache != curr:
-                most = n
-            l_cache = curr
-
-        res -= l_cache
-    else:
-        res -= len(even)
-
-    if len(odd_key) != 1:
-        r_cache = 0
-        for n in odd:
-            if n == most:
-                continue
-            curr = max(r_cache, odd.count(n))
-            r_cache = curr
-
-        res -= r_cache
-    else:
-        res -= len(odd)
-
-    return res
+from collections import Counter
 
 
-print(solve(given()))
+def solve(size=int(input()), ary=list(map(int, input().split()))):
+    even, odd = Counter(ary[::2]), Counter(ary[1::2])
+    """ 
+    put a sentinel in counter - make elements more than one always.
+    avoid error when given was only one element - e. g.) [1, 1, 1, 1]
+    given value should be bigger than 1 - so we put 0 count to 0.
+    """
+    even[0], odd[0] = 0, 0
+    (e, em), (_, es) = even.most_common(2)
+    (o, om), (_, os) = odd.most_common(2)
+    return size - max(em + os, es + om) if e == o else size - em - om
+
+
+print(solve())
