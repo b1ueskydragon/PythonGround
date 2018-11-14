995509027d769af89c19740e3d65e2f3210c30d3
b1ueskydragon <dragoalie@gmail.com>
2018-11-15 00:10:15 +0900

    Refactor

--- euler/q010/n_prime_sum01.py
+++ euler/q010/n_prime_sum01.py
@@ -1,24 +1,32 @@
-def solve(n=2_000_000, cache=0):
-    # if n < 2:
-    #     return 0
-    # if n == 2:
-    #     return 2
-    # if n % 2 == 0:
-    #     n += 1
+"""
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.
+"""
+
+below = 2_000_000
+
+
+def solve(n=below, cache=0):
+    if n < 2:
+        return 0
+    if n == 2:
+        return 2
+
+    n = n + 1 if n % 2 == 0 else n + 2
 
     primes = [True] * n
-    primes[0], primes[1] = [None] * 2  # sentinels
+    primes[0], primes[1] = None, None  # sentinels
 
     for i, prime in enumerate(primes):
         if not prime:
             continue  # skip
-
         elif prime and i <= n ** 0.5 + 1:
+            """ eratosthenes' sieve """
             primes[i * 2::i] = [False] * (((n - 1) // i) - 1)
 
         cache += i
 
-    print(cache)
+    return cache
 
 
-solve()
+print(solve())
