d05f4fe520773ef508aa22c112adf80eff0d9a54
jang <dragoalie@gmail.com>
2018-11-26 14:04:55 +0900

    p283 re-understand ques, wip

--- dailyOne/P283/regular_number.py
+++ dailyOne/P283/regular_number.py
@@ -1,9 +1,19 @@
 def solve(n):
-    return n
+    regular_nums = [1] * n  # pre-set
 
+    count = 1
+    while count < n:
+        count += 1
+
+    return regular_nums
+
+
+n = 59
+print(solve(n))
 
 """
-find n,
-60 % n == 0
-n == 2^i * 3^j * 5^k
+x == 2^i * 3^j * 5^k
+[x1, x2, ... xN]
+
+len(solve(N)) == N
 """
