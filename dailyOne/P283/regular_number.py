49a421d922d307ddca0843d5be0674cdb07ad06a
b1ueskydragon <dragoalie@gmail.com>
2018-12-02 13:58:46 +0900

    With a hamming number

--- dailyOne/P283/regular_number.py
+++ dailyOne/P283/regular_number.py
@@ -1,11 +1,25 @@
 def solve(n):
-    regular_nums = [1] * n  # pre-set
+    """
+    Dijkstra's Hamming numbers
+     - 1 に 2, 3, 5 を k回 (k>=0) かけ算して得られる数
 
-    count = 1
-    while count < n:
-        count += 1
+    O(n)
+    """
+    nums = [1] * n  # pre-set
+    i2, i3, i5 = 0, 0, 0  # smallest indices of the array - multiplying by each prime factors
 
-    return regular_nums
+    for i in range(1, n):
+        num = min(2 * nums[i2], 3 * nums[i3], 5 * nums[i5])
+        nums[i] = num
+
+        if num % 2 == 0:
+            i2 += 1
+        if num % 3 == 0:
+            i3 += 1
+        if num % 5 == 0:
+            i5 += 1
+
+    return nums
 
 
 n = 59
