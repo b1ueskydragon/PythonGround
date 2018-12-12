06be9df7a43a08fc7a3da36268ff9fe6faa879e8
jang <dragoalie@gmail.com>
2018-12-12 13:33:30 +0900

    wip: only one time before rec(2)

--- dailyOne/P305/rm_sum_zero.py
+++ dailyOne/P305/rm_sum_zero.py
@@ -1,13 +1,17 @@
-def solve(lst, head=0):
+def solve(lst):
     # jump to next head. not necessary remove elements.
     acc = 0
-    for i, n in enumerate(lst):
-        acc += n
-        if acc == 0:
-            lst = lst[i + 1:]
+    head = 0
+    i = head + 1
 
-        elif acc == lst[head]:
-            lst = [lst[head]] + lst[i + 1:]
+    if lst[head] == 0:
+        return lst[head + 1:]
+
+    while i < len(lst):
+        acc += lst[i]
+        i += 1
+        if acc == 0 or acc == lst[head]:
+            return [lst[head]] + lst[i:]
 
     return lst
 
@@ -15,10 +19,12 @@ def solve(lst, head=0):
 # linked list
 given = [3, 4, -7, 5, -6, 6]
 given00 = [5, -6, 6, 7]
-given01 = [0, 4, -7, 7, 3, 8]
-given02 = [4, -7, 7, 3, 8, -8, -8]
 
 # print(solve(given))  # 5
-print(solve(given00))  # 5
-# print(solve(given01))  # 4, 3, 8
-print(solve(given02))  # 4, 3, -8
+# print(solve(given00))  # 5, 7
+
+given01 = [0, 4, -7, 7, 3, 8]
+print(solve(given01))  # 4, 3, 8
+
+given02 = [4, -7, 7, 3, -3, -8, -10]
+print(solve(given02))  # 4, -8, -10
