8f5d2e2b2998f255498409509e174a73bb32c162
jang <dragoalie@gmail.com>
2018-12-12 13:53:30 +0900

    Done? TODO: bug fix, replace to a linked node

--- dailyOne/P305/rm_sum_zero.py
+++ dailyOne/P305/rm_sum_zero.py
@@ -7,6 +7,10 @@ TODO:
 
 def solve(lst):
     # jump to next head. not necessary remove elements.
+
+    if not lst:
+        return []
+
     acc = 0
     head = 0
     i = head + 1
@@ -17,8 +21,10 @@ def solve(lst):
     while i < len(lst):
         acc += lst[i]
         i += 1
-        if acc == 0 or acc == lst[head]:
+        if acc == 0:
             return solve([lst[head]] + lst[i:])
+        if acc + lst[head] == 0:
+            return solve(lst[i:])
 
     return lst
 
@@ -35,3 +41,12 @@ print(solve(given01))  # 4, 3, 8
 
 given02 = [4, -7, 7, 3, -3, -8, -10]
 print(solve(given02))  # 4, -8, -10
+
+given03 = [1, 1, -1, -1, 2, 2, 3, 3]
+print(solve(given03))  # 2, 2, 3, 3
+
+given04 = [0, 1, -1]
+print(solve(given04))  # []
+
+given05 = [1, 2, 3, 4, -3, -1]
+print(solve(given05))  # bug
