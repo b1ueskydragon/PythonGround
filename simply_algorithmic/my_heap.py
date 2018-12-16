e839fab577a55f03f722732f8cc6c9c51433faa3
b1ueskydragon <dragoalie@gmail.com>
2018-12-16 10:59:36 +0900

    Fix heapify be able to set endpos outside, for heapsort

--- simply_algorithmic/my_heap.py
+++ simply_algorithmic/my_heap.py
@@ -1,3 +1,28 @@
+import random
+
+
+def heapsort(ary):
+    """
+    O(N) time to build a max heap,
+
+    O(logN) time with (N - 1) calls
+    to heapify all of the items in the heap
+    except for the root node.
+
+    :param ary: an array before heapify
+    :return: sorted array
+    """
+    build_max_heap(ary)
+    last = len(ary) - 1
+
+    while last > 0:
+        ary[0], ary[last] = ary[last], ary[0]
+        heapify(ary, 0, last)
+        last -= 1
+
+    return ary
+
+
 def heappop_max(heap):
     """
     Pop the largest item off the heap and re-heapify.
@@ -6,7 +31,7 @@ def heappop_max(heap):
     if heap:
         head = heap[0]
         heap[0] = last
-        heapify(heap, 0)  # start from root-position
+        heapify(heap, 0, len(heap))  # start from root-position
         return head
     return last
 
@@ -18,34 +43,35 @@ def build_max_heap(ary):
     O(N) time
     """
     for i in reversed(range(len(ary) // 2)):
-        heapify(ary, i)
+        heapify(ary, i, len(ary))
 
 
-def heapify(ary, i):
+def heapify(ary, i, endpos):
     """
     A node that has a child(ren), is same or larger than it's child(ren).
       - Add a child, left to right.
       - recursive and in-place.
 
     :param ary: array
-    :param i: index
+    :param i: an index of current pos
+    :param endpos: a maximum index of heap array
     """
 
-    largest = i  # assume that parent's index is an largest index.
-    left = 2 * i + 1  # leftmost child position
-    right = left + 1
+    parent = i  # assume that current pos is a parent pos
+    leftchild = 2 * i + 1  # leftmost child position
+    rightchild = leftchild + 1
 
-    if left < len(ary) and ary[left] > ary[i]:
+    if leftchild < endpos and ary[leftchild] > ary[i]:
         ''' left child is bigger than parent '''
-        largest = left
+        parent = leftchild
 
-    if right < len(ary) and ary[right] > ary[largest]:
+    if rightchild < endpos and ary[rightchild] > ary[parent]:
         ''' right child is bigger than left child, also parent '''
-        largest = right
+        parent = rightchild
 
-    if largest != i:
-        ary[i], ary[largest] = ary[largest], ary[i]
-        heapify(ary, largest)
+    if parent != i:
+        ary[i], ary[parent] = ary[parent], ary[i]
+        heapify(ary, parent, endpos)
 
 
 """
@@ -59,3 +85,7 @@ build_max_heap(ary)
   2  10     8
 
 """
+
+ary = list(range(200_000))
+random.shuffle(ary)
+assert sorted(ary) == heapsort(ary)
