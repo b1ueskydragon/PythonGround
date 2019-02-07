0991926a2991d84bd10b4973ac0605a260e6ebf3
jang <dragoalie@gmail.com>
2019-02-07 17:32:15 +0900

    Add foldleft

--- dailyOne/P342/my_fold.py
+++ dailyOne/P342/my_fold.py
@@ -1,3 +1,27 @@
+"""
+O(N * f)
+
+N is length of a list,
+f is cost of call function.
+"""
+
+
+def foldl(xs, f, acc):
+    """
+    fold left
+    xs is immutable, acc is mutable.
+
+    :param xs: a list to fold
+    :param f:  a binary function
+    :param acc: init value of an accumulator (starting value)
+    :return: folded result
+    """
+    for x in xs:
+        acc = f(acc, x)
+
+    return acc
+
+
 def foldr(xs, f, acc):
     """
     fold right.
@@ -24,5 +48,9 @@ def add(a, b):
 
 # test
 l = [2, 5, 3, 1, 4]
+
+print(foldl(l, add, 0))
+print(foldl(l, min, l[0]))
+
 print(foldr(l, add, 0))
 print(foldr(l, min, l[0]))
