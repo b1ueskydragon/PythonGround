61f4dde7b085b8744e9e1a966447d9c418072764
b1ueskydragon <dragoalie@gmail.com>
2019-02-23 01:07:33 +0900

    Add a quiz

---
+++ dailyOne/P265/payment.py
@@ -0,0 +1,15 @@
+import heapq
+
+
+def solve(achieves):
+    """
+    compare with their neighbor
+
+    :param achieves:
+    :return: how much each one should get paid
+    """
+    
+    return
+
+
+given = [10, 40, 200, 1000, 60, 30]  # [1, 2, 3, 4, 2, 1]
