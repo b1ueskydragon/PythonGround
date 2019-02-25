ff25fa71bdb30beff8478de82cf4aa0b171d440f
jang <dragoalie@gmail.com>
2019-02-25 12:23:57 +0900

    Tower of Hanoi with recursion, exponential of 2

--- codeit_kr/tower-of-hanoi.py
+++ codeit_kr/tower-of-hanoi.py
@@ -1,5 +1,10 @@
+"""
+move all disks at least (2^n − 1) times
+"""
+
+
 def move_disk(disks, left, right):
-    print("move disk %d, from %d to %d" % (disks, left, right))
+    print(f"move disk {disks}, from {left} to {right}")
 
 
 def hanoi(num_discs, start_peg, end_peg):
@@ -15,18 +20,23 @@ def hanoi(num_discs, start_peg, end_peg):
     if num_discs < 1:
         return
 
-    if num_discs == 1:
-        move_disk(num_discs, 1, 3)
-
-    hanoi(num_discs - 1, start_peg, end_peg)
     inter_peg = 6 - start_peg - end_peg
-    move_disk(num_discs, start_peg, inter_peg)
+
+    # move all disks except biggest one (standard pattern)
+    hanoi(num_discs - 1, start_peg, inter_peg)
+
+    # move biggest one to goal first
+    move_disk(num_discs, start_peg, end_peg)
+
+    # move remain disks to goal
+    hanoi(num_discs - 1, inter_peg, end_peg)
 
 
 # test
-# hanoi(1, 1, 3)
-# hanoi(2, 1, 3)
-
+hanoi(1, 1, 3)
+print()
+hanoi(2, 1, 3)
+print()
 hanoi(3, 1, 3)
 
 # TODO: gray code
