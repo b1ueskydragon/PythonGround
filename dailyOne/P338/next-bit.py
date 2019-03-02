d9ba55192d4684a475bbc929986e29f772c4c150
b1ueskydragon <dragoalie@gmail.com>
2019-03-02 19:34:24 +0900

    adding right most set bit to the bit pattern

--- dailyOne/P338/next-bit.py
+++ dailyOne/P338/next-bit.py
@@ -1,44 +1,57 @@
 """
 Find the next higher number with same number of set bits.
+
+About bit invert,
+for example,
+
+    0 - (+5) = -5
+
+    it can be represented at binary
+
+    10000
+    - 101
+     1011
+     ↑
+    (-)
+
+
+About mask,
+mask should be 1 or 10...0
+and then, for example,
+    ~ 100 = 1011
 """
 
 
-def _digit(n, d=0):
-    while n > 1:
-        n //= 2
-        d += 1
-    return d
-
-
-def solve(num):
+def snoob(num):
     """
-    with shift-op
-
-    0) 10011100
-    1) 10111000
-    2) 10100011
-
-    0) 1000
-    1) 10000
+    O(N)
+    N is size of integer
     """
-    if num <= 0:
-        return
+    if not num:
+        return 0
 
-    head = 1 << _digit(n=num)
-    body = (num - head) << 1
+    one = 0  # the number of 1 to clear
+    mask = 1
 
-    if body != 0:
-        body = 1 << _digit(n=body)
+    # iterate LSB to MSB until get a 10...0
+    while not ((num & mask) > 0 and (num & (mask << 1)) == 0):
+        if num & mask > 0:
+            one += 1
+            num &= ~mask  # set the bit to 0
 
-    print(bin(head + body)[2:])
+        mask <<= 1
+
+    num &= ~mask  # set the bit to 0
+    num |= mask << 1  # set the next highest bit to 1
+
+    for i in range(one):
+        num |= 1 << i
+
+    print(num, bin(num)[2:])
 
 
-solve(6)  # 110 (6) -> 1001 (9)
-solve(12)  # 1100 (12) -> 10001 (17)
-solve(8)  # 1000 (8) -> 10000 (16)
-solve(31)  # 11111 (31) -> 101111
-solve(156)  # 10011100 (156) -> 10100011 (163)
-
-# 10000000
-# 10111000
-# 10100011
+snoob(6)  # 110 (6) -> 1001 (9)
+snoob(12)  # 1100 (12) -> 10001 (17)
+snoob(8)  # 1000 (8) -> 10000 (16)
+snoob(31)  # 11111 (31) -> 101111
+snoob(156)  # 10011100 (156) -> 10100011 (163)
