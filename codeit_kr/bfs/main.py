4b29cf348a9446f04b2773ce43f19b931ec536b4
b1ueskydragon <dragoalie@gmail.com>
2019-03-03 00:01:23 +0900

    Split and Connect stations, put in dict

--- codeit_kr/bfs/main.py
+++ codeit_kr/bfs/main.py
@@ -1,19 +1,36 @@
+from codeit_kr.bfs import station as new
+
+"""
+파일의 데이터를 정리하여 각 역에 해당하는 Station 인스턴스를 만들고,
+add_connection 메소드로 이웃 역들을 연결시킨다.
+후에 각 역을 쉽게 찾아서 쓸 수 있도록 stations 사전의
+key 로 역 이름을 넣고, value 로 해당 Station 인스턴스를 넣는다.
+"""
 stations = {}
 in_file = open('stations.txt')
 
-# file to instance Station
-# add_connection
-# stations = {station_name : Station}
+for ln in in_file:
+    line = [_ for _ in ln.split() if _ != '-']
+    l, r = -1, 1
+    for st in line:
+        ins = new.Station(st)
+        if l > -1 and line[l]:
+            ins.add_connection(new.Station(line[l]))
+        if r < len(line) and line[r]:
+            ins.add_connection(new.Station(line[r]))
+        l += 1
+        r += 1
+        stations[st] = ins
 
 in_file.close()
 
-# Test
-start_name = "이태원"
-goal_name = "잠원"
-
-start = stations[start_name]
-goal = stations[goal_name]
-
-path = bfs(start, goal)
-for station in path:
-    print(station.name)
+# # Test
+# start_name = "이태원"
+# goal_name = "잠원"
+#
+# start = stations[start_name]
+# goal = stations[goal_name]
+#
+# path = bfs(start, goal)
+# for station in path:
+#     print(station.name)
--- codeit_kr/bfs/station.py
+++ codeit_kr/bfs/station.py
@@ -1,8 +1,8 @@
 class Station:
     def __init__(self, name):
-        name = ""
+        self.name = name
         self.neighbors = []
 
     def add_connection(self, another_station):
-        self.neighbors.append(another_station)
-        another_station.neighbors.append(self)
+        self.neighbors.append(another_station)  # debug another_station.name
+        another_station.neighbors.append(self)  # debug self.name
