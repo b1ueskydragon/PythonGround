a958cef6164bfbd6c4f45d3c02500330c4371e31
b1ueskydragon <dragoalie@gmail.com>
2019-03-04 00:45:52 +0900

    wip and todo - draw a whole process at first

--- codeit_kr/bfs/bfs.py
+++ codeit_kr/bfs/bfs.py
@@ -1,36 +1,22 @@
 from collections import deque
 
-from codeit_kr.bfs import station as a
 
-
-def bfs(start: a.Station, goal: a.Station, stations: dict):
-    previous_stations = stations
-    queue = deque
+def bfs(start, goal):
+    previous = {}
+    found = deque()
     current = None
 
-    start.previous = None
-    queue.append(start)
+    previous[start] = None
+    found.append(start)
 
-    """
-    # 변수 정의
-    previous 사전 생성
-    queue 생성
-    current = None
+    while found and current != goal:
+        current = found.pop()
+        for st in current:  # TODO
+            if st not in found:
+                found.append(st)
+                previous[st.name] = st
 
-    # 초기 설정
-    start의 previous를 None로 설정
-    queue에 start 추가
-
-    아직 보지 않은 역이 있고, 도착역을 찾지 않았을 경우 반복:
-        queue의 앞에 있는 역 빼서 current에 지정
-
-        current의 neighbor를 순서대로 본다:
-            neighbor가 아직 안 본 역이면:
-                queue에 추가한다
-                previous 사전에 추가한다
-
-    만약 current가 goal이면:
-        경로를 만들어서 리턴
-
-    current가 goal이 아니면 None을 리턴
-    """
+    if current == goal:
+        return previous
+    else:
+        return None
