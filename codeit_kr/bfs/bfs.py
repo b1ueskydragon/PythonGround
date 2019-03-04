d17868b6cfecdde1c20a409e9b2d200fceb76e7d
b1ueskydragon <dragoalie@gmail.com>
2019-03-05 01:01:52 +0900

    Bfs done

--- codeit_kr/bfs/bfs.py
+++ codeit_kr/bfs/bfs.py
@@ -2,21 +2,25 @@ from collections import deque
 
 
 def bfs(start, goal):
-    previous = {}
-    found = deque()
+    memo = {}  # mapping station -> prev (どこから来たか)
+    lookup = deque()  # lookup 予定の待ち¬列
     current = None
 
-    previous[start] = None
-    found.append(start)
+    memo[start] = current
+    lookup.append(start)
 
-    while found and current != goal:
-        current = found.pop()
-        for st in current:  # TODO
-            if st not in found:
-                found.append(st)
-                previous[st.name] = st
+    while lookup and current != goal:
+        current = lookup.popleft()
+        for st in current.neighbors:
+            if st not in memo.keys():
+                lookup.append(st)
+                memo[st] = current
 
-    if current == goal:
-        return previous
-    else:
-        return None
+    path = [goal]
+    curr = goal
+
+    while memo[curr]:
+        curr = memo[curr]
+        path.append(memo[curr])
+
+    return path
