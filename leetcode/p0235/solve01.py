be6011ad971c9d147b99614a8fd1c130372e399b
b1ueskydragon <dragoalie@gmail.com>
2019-03-12 11:30:26 +0900

    LCA in BST with iteration

---
+++ leetcode/p0235/solve01.py
@@ -0,0 +1,45 @@
+"""
+Binary Search Tree
+
+with iterate
+
+O(N) time in the worst case (visiting all the nodes)
+O(1) space since we don't use a stack
+"""
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
+        else:
+            return root
+
+
+if __name__ == '__main__':
+    root = TreeNode(6)
+    root.left = TreeNode(2)
+    root.right = TreeNode(8)
+    root.left.left = TreeNode(0)
+    root.left.right = TreeNode(4)
+    root.right.left = TreeNode(7)
+    root.right.right = TreeNode(9)
+    root.left.right.left = TreeNode(3)
+    root.left.right.right = TreeNode(5)
+
+    s = Solution()
+
+    # p=3, q=5
+    print(s.lowestCommonAncestor(root, p=root.left.right.left, q=root.left.right.right).val)
