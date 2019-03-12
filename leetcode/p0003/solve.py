d0d6ba9a06ce65571bf38d3a13774ceb00a640a3
b1ueskydragon <dragoalie@gmail.com>
2019-03-13 00:25:09 +0900

    Solve with dp and recursion (Todo: speed up and reduce memory usage)

--- leetcode/p0003/solve.py
+++ leetcode/p0003/solve.py
@@ -1,9 +1,25 @@
 class Solution:
+    """
+    Dynamic programming (memoize)
+
+    we want to get an length .. not a substring itself
+    """
+
     def lengthOfLongestSubstring(self, s: str) -> int:
         prev, curr = 0, 0
-        # TODO Dynamic programming (memoize)
-        # we want to get an length .. not a substring itself
-        return max(prev, curr)
+        lookup = set()
+
+        if not s:  # exit case
+            return curr
+        else:  # recursive case
+            for c in s:
+                lookup.add(c)
+                curr = len(lookup)
+                if prev == curr:  # stop traversal (c is existed in lookup)
+                    break
+                prev = curr
+
+            return max(curr, self.lengthOfLongestSubstring(s[1:]))
 
 
 if __name__ == '__main__':
