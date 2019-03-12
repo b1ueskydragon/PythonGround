a5e5028a9574bbeaa80ed9089207c1fbdb33ad05
b1ueskydragon <dragoalie@gmail.com>
2019-03-12 10:39:39 +0900

    LCA in BST with recursion

--- leetcode/p0235/solve.py
+++ leetcode/p0235/solve.py
@@ -1,5 +1,8 @@
 """
 Binary `Search` Tree
+
+O(N) time at worse,
+O(N) space by the using recursion stack.
 """
 
 
@@ -15,9 +18,26 @@ class Solution:
         if root.val == p.val or root.val == q.val:
             return root
 
-        if root.val > p.val and root.val > q.val:
+        if root.val > p.val and root.val > q.val:  # lies in left (standard case)
             return self.lowestCommonAncestor(root.left, p, q)
-        if root.val < p.val and root.val < q.val:
+        if root.val < p.val and root.val < q.val:  # lies in right (standard case)
             return self.lowestCommonAncestor(root.right, p, q)
 
         return root
+
+
+if __name__ == '__main__':
+    root = TreeNode(6)
+    root.left = TreeNode(2)
+    root.right = TreeNode(8)
+    root.left.left = TreeNode(0)
+    root.left.right = TreeNode(4)
+    root.right.left = TreeNode(7)
+    root.right.right = TreeNode(9)
+    root.left.right.left = TreeNode(3)
+    root.left.right.right = TreeNode(5)
+
+    s = Solution()
+
+    # p=7, q=8
+    print(s.lowestCommonAncestor(root, p=root.right.left, q=root.right).val)
