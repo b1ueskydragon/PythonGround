0e37c45d67fe98c675054518355fbda77f43457a
b1ueskydragon <dragoalie@gmail.com>
2019-03-16 00:16:22 +0900

    TODO - fix bug

--- simply_algorithmic/codility.py
+++ simply_algorithmic/codility.py
@@ -14,11 +14,17 @@ def solution(A):
     pairs.sort(key=lambda p: p[1])
     dis = 0  # distance
     curr = 0
+    # print(pairs)
 
     i, j = 0, 1
     while i < N and j < N:
-        dis = max(dis, pairs[j][1] - pairs[i][1])
-        curr = max(curr, abs(pairs[j][0] - pairs[i][0]))
+        curr_dis = pairs[j][1] - pairs[i][1]
+        if curr_dis >= dis:
+            dis = curr_dis
+            curr = max(curr, abs(pairs[j][0] - pairs[i][0]))
+        elif curr_dis == 0:  # TODO: fix here
+            curr = curr - pairs[i][0] + pairs[j][0]
+
         i += 1
         j += 1
 
@@ -26,5 +32,5 @@ def solution(A):
 
 
 if __name__ == '__main__':
-    print(solution(A=[1, 4, 7, 3, 3, 5]))
-    print(solution(A=[0, 3, 3, 7, 5, 3, 11, 1]))
+    print(solution(A=[1, 4, 7, 3, 3, 5]))  # 4
+    print(solution(A=[0, 3, 3, 7, 5, 3, 11, 1]))  # 7
