9b03b37a5572fcc592eaa81f0c8f7156ade28d9e
jang <dragoalie@gmail.com>
2019-03-15 17:59:10 +0900

    Make list object only once, use var for sum

--- simply_algorithmic/equilibrium_index.py
+++ simply_algorithmic/equilibrium_index.py
@@ -1,9 +1,22 @@
 def solve(nums, n):
-    i = 1
-    while sum(nums[:i]) != sum(nums[i + 1:]):
-        i += 1
-    else:
-        return i
+    if not nums:
+        return -1
+    if n > 2:
+        i = 1
+        left = sum(nums[:i])
+        right = sum(nums[i + 1:])
+
+        while n > i and left != right:
+            i += 1
+            left += nums[i - 1]
+            if n > i:
+                right -= nums[i]
+        else:
+            if n > i:
+                return i
+    return -1
 
 
-print(solve(nums=[-1, 3, -4, 5, 1, -6, 2, 1], n=8))
+print(solve(nums=[2, 1, 2, -3, 4, -2, -3, 4], n=8))
+print(solve(nums=[0, -2147483648, 0, -2147483648], n=4))
+print(solve(nums=[0, -2147483648, 0, 2147483648], n=4))
