22cf238b6800331ceb08ebab6b2c00f148bd152c
b1ueskydragon <dragoalie@gmail.com>
2019-03-20 01:34:24 +0900

    next approach to divide sevral cases

--- leetcode/p0921/solve.py
+++ leetcode/p0921/solve.py
@@ -1,12 +1,25 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        l, r = 0, 0
-        for c in s:
-            if c == "(":
+        n = len(s)
+        if n < 1:
+            return 0
+
+        l, r = 0, 0  # need to add. l is "(" r is ")"
+        prev = ""
+
+        closed = s[0] == "(" and s[-1] == ")"
+
+        # not valid pattern ((, ))
+        # () is a valid pattern
+        # )( is ..?
+        for i, c in enumerate(s, start=1):
+            if i < n and c == "(" and s[i] == "(":
                 r += 1
-            elif c == ")":
+            elif prev == ")" and c == ")":
                 l += 1
-        return abs(r - l)
+            prev = c
+
+        return (r + l + 1, r + l)[closed]
 
 
 if __name__ == '__main__':
@@ -15,3 +28,7 @@ if __name__ == '__main__':
     print(s.minAddToMakeValid(s="((("))  # 3
     print(s.minAddToMakeValid(s="))))))))))))))("))  # 15
     print(s.minAddToMakeValid(s="(((((((((((((()"))  # 13
+    print(s.minAddToMakeValid(s="()))(("))  # 4
+    print(s.minAddToMakeValid(s="("))  # 1
+    print(s.minAddToMakeValid(s=")"))  # 1
+    print(s.minAddToMakeValid(s="()()()()"))  # 0
