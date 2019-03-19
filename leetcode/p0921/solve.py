926c07637d76d67ce54c405fc9c4b2aaf679ae5d
b1ueskydragon <dragoalie@gmail.com>
2019-03-20 02:07:49 +0900

    entire len minus valid len

--- leetcode/p0921/solve.py
+++ leetcode/p0921/solve.py
@@ -1,34 +1,16 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         n = len(s)
-        if n < 1:
-            return 0
-
-        l, r = 0, 0  # need to add. l is "(" r is ")"
-        prev = ""
-
-        closed = s[0] == "(" and s[-1] == ")"
-
-        # not valid pattern ((, ))
-        # () is a valid pattern
-        # )( is ..?
+        valid = 0
         for i, c in enumerate(s, start=1):
-            if i < n and c == "(" and s[i] == "(":
-                r += 1
-            elif prev == ")" and c == ")":
-                l += 1
-            prev = c
+            if i < n and c == "(" and s[i] == ")":
+                valid += 2
 
-        return (r + l + 1, r + l)[closed]
+        return n - valid
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.minAddToMakeValid(s="())"))  # 1
-    print(s.minAddToMakeValid(s="((("))  # 3
-    print(s.minAddToMakeValid(s="))))))))))))))("))  # 15
-    print(s.minAddToMakeValid(s="(((((((((((((()"))  # 13
-    print(s.minAddToMakeValid(s="()))(("))  # 4
-    print(s.minAddToMakeValid(s="("))  # 1
-    print(s.minAddToMakeValid(s=")"))  # 1
-    print(s.minAddToMakeValid(s="()()()()"))  # 0
+    print(s.minAddToMakeValid(s="((())"))  # 1
+    print(s.minAddToMakeValid(s="()()"))  # 0
+    print(s.minAddToMakeValid(s="()((()))"))  # 0
