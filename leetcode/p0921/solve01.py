7aa68009728b58882fa4b440139689d9f671ddd7
b1ueskydragon <dragoalie@gmail.com>
2019-03-21 08:55:03 +0900

    Count after append or pop an last element

--- leetcode/p0921/solve01.py
+++ leetcode/p0921/solve01.py
@@ -1,2 +1,22 @@
-# TODO stack
-# TODO another count and decrease
+"""
+append or pop an last (most lately) element
+"""
+
+
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        stack = []  # need to append
+        for c in s:
+            if c == "(":
+                stack.append(")")
+            elif stack:  # c is ")" and stack is not empty
+                stack.pop()
+
+        return len(stack)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.minAddToMakeValid(s="((())"))  # 1
+    print(s.minAddToMakeValid(s="()()"))  # 0
+    print(s.minAddToMakeValid(s="()((()))"))  # 0
