5d5a9a3fd73c61cd078084f56cc580d3aec8734d
b1ueskydragon <dragoalie@gmail.com>
2019-03-22 08:37:07 +0900

    Add buff count when stack is empty but need to add

--- leetcode/p0921/solve01.py
+++ leetcode/p0921/solve01.py
@@ -5,14 +5,17 @@ append or pop an last (most lately) element
 
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        stack = []  # need to append
+        stack = []  # need to append ")"
+        buff = 0
         for c in s:
             if c == "(":
                 stack.append(")")
             elif stack:  # c is ")" and stack is not empty
                 stack.pop()
+            else:  # c is ")" but stack is empty (need to add "(")
+                buff += 1
 
-        return len(stack)
+        return len(stack) + buff
 
 
 if __name__ == '__main__':
@@ -20,3 +23,5 @@ if __name__ == '__main__':
     print(s.minAddToMakeValid(s="((())"))  # 1
     print(s.minAddToMakeValid(s="()()"))  # 0
     print(s.minAddToMakeValid(s="()((()))"))  # 0
+    print(s.minAddToMakeValid(s=")))"))  # 3
+    print(s.minAddToMakeValid(s="(((())"))  # 2
