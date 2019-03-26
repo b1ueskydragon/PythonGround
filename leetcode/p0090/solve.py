bdc176f595f0982f3745c03dab26ff8aab7107da
b1ueskydragon <dragoalie@gmail.com>
2019-03-27 01:49:08 +0900

    wip - divide first

--- leetcode/p0090/solve.py
+++ leetcode/p0090/solve.py
@@ -1,8 +1,24 @@
+"""
+base case      [] -> [] (itself)
+standard case [1] -> [1], [] ([] is a previous subset)
+"""
+
+
 class Solution:
+    cache = []  # TODO: to set
+
     def subsetsWithDup(self, nums):
-        return
+        if not nums:  # base case. add itself.
+            self.cache.append(nums)
+
+        else:  # standard case. divide
+            self.cache.append(nums)
+            self.cache.append(nums[:1])
+            self.subsetsWithDup(nums[1:])
+
+        return self.cache
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.subsetsWithDup([1, 2, 2]))
+    print(s.subsetsWithDup([1, 2]))
