5b2e462437dff156bbc09ebe2a5c09f5ffeeed8d
b1ueskydragon <dragoalie@gmail.com>
2019-03-28 10:26:16 +0900

    debug concat process

--- leetcode/p0090/solve.py
+++ leetcode/p0090/solve.py
@@ -5,20 +5,23 @@ standard case [1] -> [1], [] ([] is a previous subset)
 
 
 class Solution:
-    cache = []  # TODO: to set
+    cache = []
 
     def subsetsWithDup(self, nums):
         if not nums:  # base case. add itself.
             self.cache.append(nums)
 
         else:  # standard case. divide
-            self.cache.append(nums)
-            self.cache.append(nums[:1])
             self.subsetsWithDup(nums[1:])
+
+            for l in self.cache:
+                print(l + nums[:1])
+
+            self.cache.append(nums[:1])
 
         return self.cache
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.subsetsWithDup([1, 2]))
+    print(s.subsetsWithDup([1, 2, 3]))
