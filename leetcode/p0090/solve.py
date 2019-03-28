b9d71cb82ab0e6258c9b150cb67a6c98d387a4f0
b1ueskydragon <dragoalie@gmail.com>
2019-03-28 10:57:49 +0900

    wip

--- leetcode/p0090/solve.py
+++ leetcode/p0090/solve.py
@@ -5,21 +5,25 @@ standard case [1] -> [1], [] ([] is a previous subset)
 
 
 class Solution:
-    cache = []
+    cache, res = [], []
 
     def subsetsWithDup(self, nums):
         if not nums:  # base case. add itself.
             self.cache.append(nums)
+            self.res.append(nums)
+            return []
 
         else:  # standard case. divide
             self.subsetsWithDup(nums[1:])
 
             for l in self.cache:
-                print(l + nums[:1])
+                curr = l + nums[:1]
+                if curr not in self.res:
+                    self.res.append(curr)
 
             self.cache.append(nums[:1])
 
-        return self.cache
+        return self.res if nums in self.res else self.res + [nums]
 
 
 if __name__ == '__main__':
