56e60efa8c7de757ab660f8182247fc11ca14ca3
b1ueskydragon <dragoalie@gmail.com>
2019-04-11 00:47:21 +0900

    compare equal bw char-count hash table

--- leetcode/p0242/solve01.py
+++ leetcode/p0242/solve01.py
@@ -1,2 +1,20 @@
-# wip: eturn len(s) == len(t) and set(s) == set(t)
-# TODO: hash table mapping {char : count}
+class Solution:
+    def hashtable(self, s: str):
+        pair = {}  # {char : count}
+        for c in s:
+            if c not in pair:
+                pair[c] = 1
+            else:
+                pair[c] += 1
+
+        return pair
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        return self.hashtable(s) == self.hashtable(t)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    # only lowercase, may contain unicode characters
+    print(s.isAnagram("anagram", "mngrama"))  # false
+    print(s.isAnagram("anagra?m!", "n!aga?ram"))  # true
