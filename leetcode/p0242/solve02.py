8c1bce740e73f7b3c8073536ca0f273ce1e20546
b1ueskydragon <dragoalie@gmail.com>
2019-04-15 00:04:11 +0900

    a to z, fixed length array

--- leetcode/p0242/solve02.py
+++ leetcode/p0242/solve02.py
@@ -1,1 +1,27 @@
-# TODO ascii table
+"""
+only for lowercase alpha, a to z (26 length Fixed array)
+"""
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        alphas = [0] * 26  # a-z
+
+        for c in s:
+            alphas[ord(c) - 97] += 1
+
+        for c in t:
+            i = ord(c) - 97
+            alphas[i] -= 1
+            if alphas[i] < 0:
+                return False
+
+        return True
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.isAnagram("anagram", "mngrama"))  # false
