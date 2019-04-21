8cb7d624df8658a6915ad4f392532723f5bc7e97
b1ueskydragon <dragoalie@gmail.com>
2019-04-21 15:54:16 +0900

    Solve; O(N) computate dynamically

--- leetcode/p0997/solve01.py
+++ leetcode/p0997/solve01.py
@@ -1,27 +1,32 @@
 class Solution:
     """
-    judge x が存在するとき,
+    if judge x exists,
 
-    [1...n], 任意の n に対して [n, x] が存在,
-                            [x, n] が存在しない.
+    [1...n], For any n [n, x] exists,
+                       [x, n] not exists.
     """
 
     def findJudge(self, N: int, trust: [[int]]) -> int:
-        citz = [0] * N  # create indices
-        trustee = set()
+        trusted = [0] * N  # trusted[i] = sum of who trust i+1
+        _sum = N * (N + 1) // 2
 
-        judge = N * (N + 1) // 2
+        for c, j in trust:
+            trusted[j - 1] += c
+            trusted[c - 1] = -1  # judge trusts nobody
 
-        for (c, j) in trust:
-            citz[c - 1] = c
-            trustee.add(j)
+        for i, t in enumerate(trusted, start=1):
+            if t == _sum - i:
+                return i
 
         return -1
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.findJudge(N=1, trust=[]))
-    print(s.findJudge(N=3, trust=[[1, 3], [2, 3], [3, 1]]))
-    print(s.findJudge(N=3, trust=[[1, 2], [2, 3]]))  # 1->3 not exists, return -1
-    print(s.findJudge(N=4, trust=[[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]]))
+    print(s.findJudge(N=1, trust=[]))  # 1
+    print(s.findJudge(N=2, trust=[[1, 2]]))  # 2
+    print(s.findJudge(N=3, trust=[[1, 3], [2, 3], [3, 1]]))  # judge trusts nobody, -1
+    print(s.findJudge(N=3, trust=[[1, 3], [2, 3], [2, 1]]))  # 3
+    print(s.findJudge(N=3, trust=[[1, 2], [2, 3]]))  # 1->3 not exists, -1
+    print(s.findJudge(N=3, trust=[[1, 2], [2, 3], [1, 3]]))  # 3
+    print(s.findJudge(N=4, trust=[[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]]))  # 3
