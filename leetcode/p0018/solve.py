fefebedac7b972c1c9aa237471ae059861c8d2da
b1ueskydragon <dragoalie@gmail.com>
2019-04-29 00:25:24 +0900

    loop based by two-sum and narrow remaining range

--- leetcode/p0018/solve.py
+++ leetcode/p0018/solve.py
@@ -1,16 +1,47 @@
+"""
+O(N^3) retrieval time
+pruning with skip and break.
+"""
+
+
 class Solution:
     def fourSum(self, nums: [int], target: int) -> [[int]]:
-        # I/O
-        # input; 同一要素を含んだ not sorted ints
-        # output; 最大で nC4 通り
+        res = []
+        nums.sort()
 
-        # Plan
-        # sort first (同一要素を飛ばすため)
-        # depth first search (線形で末尾まで探索, 構成できなかったら一つ戻る)
+        for i in range(len(nums) - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            if nums[i] * 4 > target:  # since nums are sorted
+                break
 
-        # TODO divide
+            for j in range(i + 1, len(nums) - 2):
+                if j > i and nums[j] == nums[j - 1]:
+                    continue
+                if nums[j] * 3 > target - nums[i]:
+                    break
 
-        return
+                low, high = j + 1, len(nums) - 1
+
+                while low < high:
+                    _sum = nums[i] + nums[j] + nums[low] + nums[high]
+
+                    if _sum > target:
+                        high -= 1
+                    elif _sum < target:
+                        low += 1
+                    else:
+                        res.append([nums[i], nums[j], nums[low], nums[high]])
+
+                        while low < high and nums[low] == nums[low + 1]:
+                            low += 1
+                        while low < high and nums[high] == nums[high - 1]:
+                            high -= 1
+
+                        low += 1
+                        high -= 1
+
+        return res
 
 
 if __name__ == '__main__':
