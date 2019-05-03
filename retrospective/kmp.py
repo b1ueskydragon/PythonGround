a8206707d5c07f8f8bc6a31840263b6d3eaa60b0
b1ueskydragon <dragoalie@gmail.com>
2019-05-03 10:30:56 +0900

    Refactor skip table

--- retrospective/kmp.py
+++ retrospective/kmp.py
@@ -8,24 +8,24 @@ Times : O(k)
 def partial_match_table(word):
     table = [0] * (len(word) + 1)
     table[0] = -1
-    w, t = 0, 1
+    i, j = 0, 1
 
-    while t < len(word):
-        if word[w] == word[t]:  # sub-pattern found
-            w += 1
-            t += 1
-            table[t] = w
-        elif w > 0:  # no sub-pattern, fallback
-            w = table[w]
-        else:  # w == 0
-            t += 1
-            table[t] = 0
+    while j < len(word):
+        matched = word[i] == word[j]  # is sub pattern found
+
+        if not matched and i > 0:
+            i = table[i]
+        else:
+            if matched:
+                i += 1
+            j += 1
+            table[j] = i
 
     return table
 
 
 def kmp_search(text, word):
-    table = partial_match_table(word)
+    skip_table = partial_match_table(word)
 
     t, w = 0, 0  # cursor of text, word
     while t != len(text) and w != len(word):
@@ -35,7 +35,7 @@ def kmp_search(text, word):
         elif w == 0:  # not matched and there is no skip
             t += 1
         else:  # set restart position
-            w = table[w]
+            w = skip_table[w]
 
     if w == len(word):  # cursor is on end
         return t - w  # end pos - start pos
