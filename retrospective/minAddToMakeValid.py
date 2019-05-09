ecc910684246b8a1eeacd8d439f35737d59c5264
b1ueskydragon <dragoalie@gmail.com>
2019-05-09 09:52:47 +0900

    Complete a code with intention of Braces

--- retrospective/minAddToMakeValid.py
+++ retrospective/minAddToMakeValid.py
@@ -1,2 +1,14 @@
-def min_add_to_make_valid(self, s: str) -> int:
-    return
+def min_add_to_make_valid(braces: str):
+    add_close = 0
+    add_open = 0
+    _open, _close = "{", "}"
+
+    for c in braces:
+        if c == _open:
+            add_close += 1
+        elif c == _close and add_close != 0:
+            add_close -= 1
+        else:  # c == _close but add_close == 0
+            add_open += 1
+
+    return add_close + add_open
