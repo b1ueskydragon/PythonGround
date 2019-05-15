bc11b65840ae6692d987d11969fa73bf1eb0832a
b1ueskydragon <dragoalie@gmail.com>
2019-05-15 10:29:26 +0900

    todo to retrospective

---
+++ retrospective/maximumSumOfNonAdjacentElements.py
@@ -0,0 +1,2 @@
+def maximum_sum_of_non_adjacent_elements(nums):
+    return 0
---
+++ retrospective/maximumSumOfNonAdjacentElements_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_maximum_sum_of_non_adjacent_elements(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
