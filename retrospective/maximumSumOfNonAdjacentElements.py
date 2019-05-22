2d6e98cec1288bfb9787f9f15453d61d5b5181b0
b1ueskydragon <dragoalie@gmail.com>
2019-05-22 23:39:26 +0900

    solve with four caches

--- retrospective/maximumSumOfNonAdjacentElements.py
+++ retrospective/maximumSumOfNonAdjacentElements.py
@@ -1,2 +1,12 @@
 def maximum_sum_of_non_adjacent_elements(nums):
-    return 0
+    incl, excl = 0, 0  # include current num or not
+    cache_in, cache_ex = 0, 0  # cache that include previous adjacent num or not
+
+    for curr in nums:
+        incl = cache_ex + curr
+        excl = max(cache_in, cache_ex)  # since both are not adjacent elements of next curr
+
+        cache_in = incl
+        cache_ex = excl
+
+    return max(incl, excl)
