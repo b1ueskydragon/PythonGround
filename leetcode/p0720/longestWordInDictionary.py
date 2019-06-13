35cf1856484d20f93206dce69dcadec1f7dabbcc
b1ueskydragon <dragoalie@gmail.com>
2019-06-14 00:01:26 +0900

    Prune and brute force

--- leetcode/p0720/longestWordInDictionary.py
+++ leetcode/p0720/longestWordInDictionary.py
@@ -1,3 +1,14 @@
 class Solution:
     def longestWord(self, words: [str]) -> str:
-        return ""
+        max_word = ""
+        prefixes = set(words)
+
+        for word in words:
+            longer = len(word) > len(max_word)
+            smaller = len(word) == len(max_word) and word < max_word
+
+            if longer or smaller:
+                if all(word[:i] in prefixes for i in range(1, len(word))):
+                    max_word = word
+
+        return max_word
