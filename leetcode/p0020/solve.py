309d1b59ef8516d4a0cb237c90bec98f0af717d6
b1ueskydragon <dragoalie@gmail.com>
2019-06-27 17:51:00 +0900

    Slow but simple removing pattern solution

---
+++ leetcode/p0020/solve.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isValid(self, s: str):
+        if len(s) % 2 != 0:
+            return False
+        while "[]" in s or "()" in s or "{}" in s:
+            s = s.replace("[]", "").replace("()", "").replace("{}", "")
+        return s == ""
