8d7d304c16092d3f2ccd89eb5ffb6d9a922369ad
b1ueskydragon <dragoalie@gmail.com>
2019-06-27 17:54:50 +0900

    Add a case

--- leetcode/p0020/test_isValid.py
+++ leetcode/p0020/test_isValid.py
@@ -1,38 +1,65 @@
 import unittest
 
 from leetcode.p0020.solve import Solution
+from leetcode.p0020.solve01 import Solution as Solution01
 
 
 class IsValidTest(unittest.TestCase):
     def test_empty(self):
         s = Solution()
+        s01 = Solution01()
+
         brackets = ""
         self.assertEqual(True, s.isValid(brackets))
+        self.assertEqual(True, s01.isValid(brackets))
 
     def test_case01(self):
         s = Solution()
+        s01 = Solution01()
+
         brackets = "()"
         self.assertEqual(True, s.isValid(brackets))
+        self.assertEqual(True, s01.isValid(brackets))
 
     def test_case02(self):
         s = Solution()
+        s01 = Solution01()
+
         brackets = "()[]{}"
         self.assertEqual(True, s.isValid(brackets))
+        self.assertEqual(True, s01.isValid(brackets))
 
     def test_case03(self):
         s = Solution()
+        s01 = Solution01()
+
         brackets = "(]"
         self.assertEqual(False, s.isValid(brackets))
+        self.assertEqual(False, s01.isValid(brackets))
 
     def test_case04(self):
         s = Solution()
+        s01 = Solution01()
+
         brackets = "([)]"
         self.assertEqual(False, s.isValid(brackets))
+        self.assertEqual(False, s01.isValid(brackets))
 
     def test_case05(self):
         s = Solution()
+        s01 = Solution01()
+
         brackets = "{[]}"
         self.assertEqual(True, s.isValid(brackets))
+        self.assertEqual(True, s01.isValid(brackets))
+
+    def test_case06(self):
+        s = Solution()
+        s01 = Solution01()
+
+        brackets = "(("
+        self.assertEqual(False, s.isValid(brackets))
+        self.assertEqual(False, s01.isValid(brackets))
 
 
 if __name__ == '__main__':
