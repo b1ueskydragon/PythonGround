7dae000782b11aa20c11f7d0b8b1c6816a499dfa
ina <dragoalie@gmail.com>
2019-07-12 10:41:28 +0900

    Solve with Perfect squares, Fix comments

--- leetcode/p0441/solve.py
+++ leetcode/p0441/solve.py
@@ -3,8 +3,27 @@ class Solution:
         """
         n = (1 + k) * k / 2
         k^2 + k - 2n = 0
-        k :　(-1 + (1 + 8n ) ^ 1/2) / 2
+
+        apply Quadratic formula to find k
+        k =　(-1 + (1 + 8n ) ^ 1/2) / 2
 
         find natural number k ( ~ 2^31 -1)
         """
         return int(((1 + 8 * n) ** 0.5 - 1) * 0.5)
+
+    def arrangeCoins_(self, n: int) -> int:
+        """
+        n = (1 + k) * k / 2
+        2n = k + k^2
+
+        make to Perfect squares
+        2n + 1/4 = k^2 + k + 1/4
+        2n + 1/4 = (k + 1/2)^2
+
+        (2n + 1/4) ^ 1/2 = k + 1/2  (k >= 0)
+        k = (2n + 1/4) ^ 1/2 - 1/2
+
+        * Quadratic formula could be proved by Perfect squares,
+          so this is identically same as above actually.
+        """
+        return int((n * 2 + 0.25) ** 0.5 - 0.5)
--- leetcode/p0441/test_arrangeCoins.py
+++ leetcode/p0441/test_arrangeCoins.py
@@ -6,14 +6,17 @@ class ArrangeCoinsTest(unittest.TestCase):
     def test_5(self):
         a = A()
         self.assertEqual(2, a.arrangeCoins(5))
+        self.assertEqual(2, a.arrangeCoins_(5))
 
     def test_6(self):
         a = A()
         self.assertEqual(3, a.arrangeCoins(6))
+        self.assertEqual(3, a.arrangeCoins_(6))
 
     def test_8(self):
         a = A()
         self.assertEqual(3, a.arrangeCoins(8))
+        self.assertEqual(3, a.arrangeCoins_(8))
 
 
 if __name__ == '__main__':
