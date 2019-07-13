ad3a53c3708d8107d9645dcce045194eaa4416ed
b1ueskydragon <dragoalie@gmail.com>
2019-07-13 11:42:46 +0900

    both add and substract version is ok

--- leetcode/p0441/solve01.py
+++ leetcode/p0441/solve01.py
@@ -5,3 +5,10 @@ class Solution:
             k += 1
             acc += k
         return k - 1
+
+    def arrangeCoins_(self, n: int) -> int:
+        k = 0
+        while n > 0:
+            k += 1
+            n -= k
+        return k if n == 0 else k - 1
--- leetcode/p0441/test_arrangeCoins.py
+++ leetcode/p0441/test_arrangeCoins.py
@@ -9,18 +9,21 @@ class ArrangeCoinsTest(unittest.TestCase):
         self.assertEqual(2, a.arrangeCoins(5))
         self.assertEqual(2, a.arrangeCoins_(5))
         self.assertEqual(2, b.arrangeCoins(5))
+        self.assertEqual(2, b.arrangeCoins_(5))
 
     def test_6(self):
         a, b = A(), B()
         self.assertEqual(3, a.arrangeCoins(6))
         self.assertEqual(3, a.arrangeCoins_(6))
         self.assertEqual(3, b.arrangeCoins(6))
+        self.assertEqual(3, b.arrangeCoins_(6))
 
     def test_8(self):
         a, b = A(), B()
         self.assertEqual(3, a.arrangeCoins(8))
         self.assertEqual(3, a.arrangeCoins_(8))
         self.assertEqual(3, b.arrangeCoins(8))
+        self.assertEqual(3, b.arrangeCoins_(8))
 
 
 if __name__ == '__main__':
