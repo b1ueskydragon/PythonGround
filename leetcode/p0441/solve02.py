1ec41dffe3adca80798ebde5c5d3596f43e6b9e0
ina <dragoalie@gmail.com>
2019-07-15 20:44:24 +0900

    binary search (recursion)

--- leetcode/p0441/solve02.py
+++ leetcode/p0441/solve02.py
@@ -12,3 +12,14 @@ class Solution:
             else:
                 high = mid - 1
         return low - 1
+
+    def arrangeCoins_(self, n: int) -> int:
+        """ Binary search (recursion) """
+
+        def rec(low, high):
+            if low > high: return low - 1
+            mid = low + (high - low) // 2
+            acc = (1 + mid) * mid // 2
+            return mid if acc == n else rec(mid + 1, high) if acc < n else rec(low, mid - 1)
+
+        return rec(0, n - 1)
--- leetcode/p0441/test_arrangeCoins.py
+++ leetcode/p0441/test_arrangeCoins.py
@@ -13,6 +13,7 @@ class ArrangeCoinsTest(unittest.TestCase):
         self.assertEqual(2, b.arrangeCoins(5))
         self.assertEqual(2, b.arrangeCoins_(5))
         self.assertEqual(2, c.arrangeCoins(5))
+        self.assertEqual(2, c.arrangeCoins_(5))
 
     def test_6(self):
         a, b, c = A(), B(), C()
@@ -21,6 +22,7 @@ class ArrangeCoinsTest(unittest.TestCase):
         self.assertEqual(3, b.arrangeCoins(6))
         self.assertEqual(3, b.arrangeCoins_(6))
         self.assertEqual(3, c.arrangeCoins(6))
+        self.assertEqual(3, c.arrangeCoins_(6))
 
     def test_8(self):
         a, b, c = A(), B(), C()
@@ -29,6 +31,7 @@ class ArrangeCoinsTest(unittest.TestCase):
         self.assertEqual(3, b.arrangeCoins(8))
         self.assertEqual(3, b.arrangeCoins_(8))
         self.assertEqual(3, c.arrangeCoins(8))
+        self.assertEqual(3, c.arrangeCoins_(8))
 
 
 if __name__ == '__main__':
