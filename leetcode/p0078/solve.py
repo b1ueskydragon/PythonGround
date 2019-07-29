2eafe18960b6221493ba71597423502f09601edb
b1ueskydragon <dragoalie@gmail.com>
2019-07-30 01:35:56 +0900

    wip - order, performance

--- leetcode/p0078/solve.py
+++ leetcode/p0078/solve.py
@@ -1,3 +1,24 @@
 class Solution:
     def subsets(self, nums: [int]) -> [int]:
-        return [[]]
+        res = {}
+
+        def _dfs(xs, pos, node):
+            n, k = len(xs), len(node)
+            if pos == n:
+                if k in res:
+                    if node not in res[k]:
+                        res[k] += [node]
+                else:
+                    res[k] = [node]
+                return
+
+            _dfs(xs, pos + 1, [xs[pos]] + node)
+            _dfs(xs, pos + 1, node)
+
+        _dfs(nums, 0, [])
+        
+        flatten = []
+        for ls in res.values():
+            for l in ls:
+                flatten.append(l)
+        return flatten
--- leetcode/p0078/test_subsets.py
+++ leetcode/p0078/test_subsets.py
@@ -3,7 +3,7 @@ from leetcode.p0078.solve import Solution as A
 
 
 class SubSetsCase(unittest.TestCase):
-    def test_case01(self):
+    def test_all_distinct(self):
         a = A()
         nums = [1, 2, 3]
         expected = [
@@ -14,6 +14,19 @@ class SubSetsCase(unittest.TestCase):
             [1, 3],
             [2, 3],
             [1, 2],
+            []
+        ]
+        self.assertEqual(expected, a.subsets(nums))
+
+    def test_has_duplication(self):
+        a = A()
+        nums = [1, 2, 2]
+        expected = [
+            [2],
+            [1],
+            [1, 2, 2],
+            [1, 2],
+            [2, 2],
             []
         ]
         self.assertEqual(expected, a.subsets(nums))
