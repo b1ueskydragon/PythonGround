5086b3e907a23c415f513ae5e1460de3e40d462c
b1ueskydragon <dragoalie@gmail.com>
2019-07-30 21:21:07 +0900

    sovle - given is distinct nums, orders not matter

--- leetcode/p0078/solve.py
+++ leetcode/p0078/solve.py
@@ -1,24 +1,19 @@
 class Solution:
+    """
+    Given a set of distinct integers.
+    """
+
     def subsets(self, nums: [int]) -> [int]:
-        res = {}
+        res = []
 
         def _dfs(xs, pos, node):
-            n, k = len(xs), len(node)
-            if pos == n:
-                if k in res:
-                    if node not in res[k]:
-                        res[k] += [node]
-                else:
-                    res[k] = [node]
+            if pos == len(xs):
+                res.append(node)
                 return
 
             _dfs(xs, pos + 1, [xs[pos]] + node)
             _dfs(xs, pos + 1, node)
 
         _dfs(nums, 0, [])
-        
-        flatten = []
-        for ls in res.values():
-            for l in ls:
-                flatten.append(l)
-        return flatten
+
+        return res
--- leetcode/p0078/test_subsets.py
+++ leetcode/p0078/test_subsets.py
@@ -3,30 +3,17 @@ from leetcode.p0078.solve import Solution as A
 
 
 class SubSetsCase(unittest.TestCase):
-    def test_all_distinct(self):
+    def test_sample(self):
         a = A()
         nums = [1, 2, 3]
         expected = [
+            [3, 2, 1],
+            [2, 1],
+            [3, 1],
+            [1],
+            [3, 2],
+            [2],
             [3],
-            [1],
-            [2],
-            [1, 2, 3],
-            [1, 3],
-            [2, 3],
-            [1, 2],
-            []
-        ]
-        self.assertEqual(expected, a.subsets(nums))
-
-    def test_has_duplication(self):
-        a = A()
-        nums = [1, 2, 2]
-        expected = [
-            [2],
-            [1],
-            [1, 2, 2],
-            [1, 2],
-            [2, 2],
             []
         ]
         self.assertEqual(expected, a.subsets(nums))
