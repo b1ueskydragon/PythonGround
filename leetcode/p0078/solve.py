df42f2659f5c6ba822b1b7e9714bc2c3df6d37cf
b1ueskydragon <dragoalie@gmail.com>
2019-08-01 22:16:16 +0900

    Use outer param

--- leetcode/p0078/solve.py
+++ leetcode/p0078/solve.py
@@ -6,14 +6,14 @@ class Solution:
     def subsets(self, nums: [int]) -> [int]:
         res = []
 
-        def _dfs(xs, pos, node):
-            if pos == len(xs):
+        def _dfs(pos, node):
+            if pos == len(nums):
                 res.append(node)
                 return
 
-            _dfs(xs, pos + 1, [xs[pos]] + node)
-            _dfs(xs, pos + 1, node)
+            _dfs(pos + 1, [nums[pos]] + node)
+            _dfs(pos + 1, node)
 
-        _dfs(nums, 0, [])
+        _dfs(0, [])
 
         return res
