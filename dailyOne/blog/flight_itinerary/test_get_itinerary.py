2b606d74b9bf44223c95135b2ada790bd330aba1
ina <dragoalie@gmail.com>
2019-08-05 09:15:30 +0900

    Make a test case

---
+++ dailyOne/blog/flight_itinerary/test_get_itinerary.py
@@ -0,0 +1,15 @@
+import unittest
+
+from dailyOne.blog.flight_itinerary.solve import get_itinerary
+
+
+class GetItineraryTest(unittest.TestCase):
+    def test_get_itinerary(self):
+        flights = [("HNL", "AKL"), ("YUL", "ORD"), ("ORD", "SFO"), ("SFO", "HNL")]
+        expected = ["YUL", "ORD", "SFO", "HNL", "AKL"]
+        current_itinerary = ["YUL"]  # departure
+        self.assertEqual(expected, get_itinerary(flights, get_itinerary(flights, current_itinerary)))
+
+
+if __name__ == '__main__':
+    unittest.main()
