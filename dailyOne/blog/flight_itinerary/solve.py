b50b590bb4e4033a6f786dbc66c668a02fb5a2bd
ina <dragoalie@gmail.com>
2019-08-06 00:08:00 +0900

    Memo and wip to solve

--- dailyOne/blog/flight_itinerary/solve.py
+++ dailyOne/blog/flight_itinerary/solve.py
@@ -1,2 +1,19 @@
-def get_itinerary(flights, current_itinerary):
-    return None
+def get_itinerary(flights: [(str, str)], current_itinerary: [str]) -> [str]:
+    """
+    Backtracking (pruning before dive into recursion).
+
+    :param flights: pair of departure and destination
+    :param current_itinerary: result (destinations)
+    :return: result (destinations)
+    """
+    if not flights:  # base case
+        return current_itinerary
+
+    last_stop = current_itinerary[-1]  # goal
+
+    for i, flight in enumerate(flights):
+        # TODO pruning
+        # TODO recursion in for loop
+        return get_itinerary(flights, current_itinerary)
+
+    return current_itinerary
