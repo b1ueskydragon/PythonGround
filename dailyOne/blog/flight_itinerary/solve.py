f4c3a99e22b7717944e239845bb82e08781b1054
ina <dragoalie@gmail.com>
2019-08-10 11:06:43 +0900

    Fix fatal bugs

--- dailyOne/blog/flight_itinerary/solve.py
+++ dailyOne/blog/flight_itinerary/solve.py
@@ -1,22 +1,22 @@
-def get_itinerary(flights: [(str, str)], current_itinerary: [str]) -> [str]:
+def get_itinerary(flights: [(str, str)], itinerary: [str]) -> [str]:
     """
     Backtracking (pruning before dive into recursion).
 
     :param flights: pair of departure and destination
-    :param current_itinerary: result (destinations)
+    :param itinerary: result (destinations)
     :return: result (destinations)
     """
     if not flights:  # base case
-        return current_itinerary
+        return itinerary
 
-    last_stop = current_itinerary[-1]  # goal
+    last_stop = itinerary[-1]  # goal
 
     for i, flight in enumerate(flights):
         start, stop = flight
-        current_itinerary.append(stop)
+        itinerary.append(stop)
         flights_not_used = flights[:i] + flights[i + 1:]
         if last_stop == start:
-            return get_itinerary(flights_not_used, current_itinerary)
-        current_itinerary.pop()
+            return get_itinerary(flights_not_used, itinerary)
+        itinerary.pop()
 
-    return current_itinerary
+    return None
--- dailyOne/blog/flight_itinerary/test_get_itinerary.py
+++ dailyOne/blog/flight_itinerary/test_get_itinerary.py
@@ -6,15 +6,17 @@ from dailyOne.blog.flight_itinerary.solve import get_itinerary
 class GetItineraryTest(unittest.TestCase):
     def test_get_itinerary(self):
         flights = [("HNL", "AKL"), ("YUL", "ORD"), ("ORD", "SFO"), ("SFO", "HNL")]
-        expected = ["YUL", "ORD", "SFO", "HNL", "AKL"]
         current_itinerary = ["YUL"]  # departure
-        self.assertEqual(expected, get_itinerary(flights, get_itinerary(flights, current_itinerary)))
+
+        expected = ["YUL", "ORD", "SFO", "HNL", "AKL"]
+        self.assertEqual(expected, get_itinerary(flights, current_itinerary))
 
     def test_return_none(self):
         flights = [("HNL", "AKL"), ("YUL", "ORD"), ("ORD", "SFO"), ("SFO", "HNL")]
-        expected = []
         current_itinerary = ["AKL"]  # departure
-        self.assertEqual(expected, get_itinerary(flights, get_itinerary(flights, current_itinerary)))
+
+        expected = None
+        self.assertEqual(expected, get_itinerary(flights, current_itinerary))
 
 
 if __name__ == '__main__':
