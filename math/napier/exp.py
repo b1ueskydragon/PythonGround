2bcedcda345224ea32e318495778566d5ae3fac3
ina <dragoalie@gmail.com>
2019-08-22 15:42:26 +0900

    Add a graph

--- math/napier/exp.py
+++ math/napier/exp.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 def exp_f(x):
-    base = 2  # 2.718
+    base = 2
     return np.power(base, x)
 
 
@@ -15,16 +15,24 @@ def numerical_diff(f, x):
     return (f(x + h) - f(x - h)) / (2 * h)  # mean-value theorem (to minimize error)
 
 
-def graph():
-    x_values = np.arange(-1, 1, 0.1)
+def draw_2(x_values):
     exp_values = exp_f(x_values)
     diff_values = numerical_diff(exp_f, x_values)
 
     plt.plot(x_values, exp_values, color="blue")
     plt.plot(x_values, diff_values, color="red")
-    plt.grid()
-    plt.show()
+
+
+def draw_e(x_values):
+    # an approximation of Napier's constant
+    plt.plot(x_values, np.power(2.718, x_values), color="green")
+    plt.plot(x_values, numerical_diff(lambda x: np.power(2.718, x), x_values), color="yellow")
 
 
 if __name__ == '__main__':
-    graph()
+    xs = np.arange(-1, 1, 0.1)
+    draw_2(xs)
+    draw_e(xs)
+
+    plt.grid()
+    plt.show()
