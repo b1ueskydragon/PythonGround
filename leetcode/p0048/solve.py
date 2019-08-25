7b4065bb9d2d566780c5af701a474c859d8449ce
ina <dragoalie@gmail.com>
2019-08-25 23:32:08 +0900

    rename to size

--- leetcode/p0048/solve.py
+++ leetcode/p0048/solve.py
@@ -3,15 +3,15 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        edge = len(matrix) - 1
+        size = len(matrix) - 1
         for i in range(len(matrix) // 2):
-            for j in range(i, edge - i):
-                tmp1 = matrix[j][edge - i]
-                tmp2 = matrix[edge - i][edge - j]
-                tmp3 = matrix[edge - j][i]
+            for j in range(i, size - i):
+                tmp1 = matrix[j][size - i]
+                tmp2 = matrix[size - i][size - j]
+                tmp3 = matrix[size - j][i]
                 tmp4 = matrix[i][j]
 
-                matrix[j][edge - i] = tmp4
-                matrix[edge - i][edge - j] = tmp1
-                matrix[edge - j][i] = tmp2
+                matrix[j][size - i] = tmp4
+                matrix[size - i][size - j] = tmp1
+                matrix[size - j][i] = tmp2
                 matrix[i][j] = tmp3
