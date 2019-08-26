de92e59ee84d218437e901b06ede3b95f47fc51a
ina <dragoalie@gmail.com>
2019-08-26 15:14:06 +0900

    hi quiz

---
+++ leetcode/p0119/solve.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getRow(self, rowIndex):
+        """
+        :param rowIndex: 0= < rowIndex <=33
+        :return: elements in column
+        """
+        if rowIndex == 0:
+            return [1]
+        elif rowIndex == 1:
+            return [1, 1]
+
+        res = []
+        return res
---
+++ leetcode/p0119/test_getRow.py
@@ -0,0 +1,30 @@
+import unittest
+
+from leetcode.p0119.solve import Solution as A
+
+
+class RotateTest(unittest.TestCase):
+    def test_getRow_0(self):
+        a = A()
+        expected = [1]
+        self.assertEqual(expected, a.getRow(0))
+
+    def test_getRow_1(self):
+        a = A()
+        expected = [1, 1]
+        self.assertEqual(expected, a.getRow(1))
+
+    def test_getRow_2(self):
+        a = A()
+        expected = [1, 2, 1]
+        self.assertEqual(expected, a.getRow(2))
+
+    def test_getRow_3(self):
+        a = A()
+        expected = [1, 3, 3, 1]
+        self.assertEqual(expected, a.getRow(3))
+
+    def test_getRow_4(self):
+        a = A()
+        expected = [1, 4, 6, 4, 1]
+        self.assertEqual(expected, a.getRow(4))
