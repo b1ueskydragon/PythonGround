bd978ab0d9a1fbcbf519fba8c4fc33244805555b
ina <dragoalie@gmail.com>
2019-08-27 15:13:18 +0900

    wip - explicit prev res, fix to loop

--- leetcode/p0119/solve.py
+++ leetcode/p0119/solve.py
@@ -4,21 +4,20 @@ class Solution:
         :param rowIndex: 0= < rowIndex <=33
         :return: elements in column
         """
-        res = self.pascal(rowIndex, [1] * (rowIndex + 1))
-        i, j = 0, rowIndex
-        while i <= j:
-            if i == j:
-                res[i] *= 2
-            else:
-                res[-1 - i] = res[i]
+        # pre_res = [1] * (rowIndex + 1)
+        pre_res = [1, 6, 15, 20, 15, 6, 1]
+        if rowIndex < 2:
+            return pre_res
+
+        res = pre_res + [1]
+        i, j = 1, rowIndex - 1
+        while i < j:
+            res[i] = pre_res[i - 1] + pre_res[i]
+            res[j] = res[i]
             i += 1
             j -= 1
 
-        return res
+        if rowIndex % 2 == 0:  # has a pivot
+            res[i] = pre_res[i] * 2
 
-    def pascal(self, k, res):
-        if k < 2:
-            return res
-        for i in range((k + 1) // 2):
-            res[i + 1] = res[i] + res[i + 1]
-        return self.pascal(k - 1, res)
+        return res
