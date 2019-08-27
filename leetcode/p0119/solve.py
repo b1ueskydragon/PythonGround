26323fa8c73de189980ad4972d42b507cd02e745
ina <dragoalie@gmail.com>
2019-08-27 15:34:31 +0900

    solve with gen curr in loop and gen prev in recursion

--- leetcode/p0119/solve.py
+++ leetcode/p0119/solve.py
@@ -4,20 +4,23 @@ class Solution:
         :param rowIndex: 0= < rowIndex <=33
         :return: elements in column
         """
-        # pre_res = [1] * (rowIndex + 1)
-        pre_res = [1, 6, 15, 20, 15, 6, 1]
         if rowIndex < 2:
+            return self.pascal(rowIndex, [1] * (rowIndex + 1))
+        return self.pascal(rowIndex, self.getRow(rowIndex - 1))
+
+    def pascal(self, row_index, pre_res):
+        if row_index < 2:
             return pre_res
 
         res = pre_res + [1]
-        i, j = 1, rowIndex - 1
+        i, j = 1, row_index - 1
         while i < j:
             res[i] = pre_res[i - 1] + pre_res[i]
             res[j] = res[i]
             i += 1
             j -= 1
 
-        if rowIndex % 2 == 0:  # has a pivot
+        if row_index % 2 == 0:  # has a pivot
             res[i] = pre_res[i] * 2
 
         return res
