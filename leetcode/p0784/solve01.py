e3761886e441ed85e8691e63d47bbb48cb9df395
ina <dragoalie@gmail.com>
2019-09-08 18:48:44 +0900

    Fix comment

--- leetcode/p0784/solve01.py
+++ leetcode/p0784/solve01.py
@@ -8,7 +8,7 @@ class Solution:
         res.append(node)
         for i in range(depth, len(S)):
             curr = S[i]
-            # go deeper from current position
             if curr.isalpha():
                 new_node = S[i].upper() if curr.islower() else curr.lower()
+                # i + 1; go deeper from current position
                 self._search(S, i + 1, node[:i] + new_node + node[i + 1:], res)
