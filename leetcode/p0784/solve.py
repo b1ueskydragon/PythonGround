e1ae446ef994a6ba48b4c07485060f5cffbdb7c7
ina <dragoalie@gmail.com>
2019-09-08 13:48:42 +0900

    solve with simple dfs

--- leetcode/p0784/solve.py
+++ leetcode/p0784/solve.py
@@ -1,11 +1,17 @@
 class Solution:
-
     def letterCasePermutation(self, S):
-        return self._dfs(S, 0, [])
-
-    def _dfs(self, ts, i, res):
-        for i in range(len(ts)):
-            curr: str = ts[i]
-            # TODO is alpha, is lower or upper
-            self._dfs(ts, i + 1, res)
+        res = []
+        self._dfs(S, 0, '', res)
         return res
+
+    def _dfs(self, S, depth, node, res):
+        if depth == len(S):
+            res.append(node)
+            return
+
+        curr = S[depth]
+        if curr.isalpha():
+            self._dfs(S, depth + 1, node + curr.upper(), res)
+            self._dfs(S, depth + 1, node + curr.lower(), res)
+        else:  # skip convert, just go deeper
+            self._dfs(S, depth + 1, node + curr, res)
