6f12e573d4ce013f915b20f3202d4eeed69a4158
ina <dragoalie@gmail.com>
2019-09-09 16:54:59 +0900

    Minor refactor

--- leetcode/p0784/solve01.py
+++ leetcode/p0784/solve01.py
@@ -9,6 +9,6 @@ class Solution:
         for i in range(depth, len(S)):
             curr = S[i]
             if curr.isalpha():
-                new_node = S[i].upper() if curr.islower() else curr.lower()
+                new_node = (curr.upper(), curr.lower())[curr.isupper()]
                 # i + 1; go deeper from current position
                 self._search(S, i + 1, node[:i] + new_node + node[i + 1:], res)
