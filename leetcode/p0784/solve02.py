653dbd2e9bc719401aed630e7fc6308f3b96a56c
ina <dragoalie@gmail.com>
2019-09-11 01:16:55 +0900

    complete direct procut, join each of producted elems

--- leetcode/p0784/solve02.py
+++ leetcode/p0784/solve02.py
@@ -4,11 +4,5 @@ class Solution:
         cartesian product := {(a, b) | a ∈ A, b ∈ B}
         """
         import itertools
-        t_cartesian = [[s.lower(), s.upper()] if s.isalpha() else s for s in S]
-        print(t_cartesian)
-        print(list(itertools.product(*t_cartesian)))
-
-
-if __name__ == '__main__':
-    x = Solution()
-    x.letterCasePermutation("ab")
+        pairs = [[s.lower(), s.upper()] if s.isalpha() else s for s in S]
+        return list(map(lambda l: ''.join(l), itertools.product(*pairs)))
