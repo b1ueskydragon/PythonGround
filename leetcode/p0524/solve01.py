3a418cbce9c63ff74331806f9b03d831e642666b
ina <dragoalie@gmail.com>
2019-09-13 17:47:12 +0900

    Simplify

--- leetcode/p0524/solve01.py
+++ leetcode/p0524/solve01.py
@@ -1,21 +1,17 @@
 class Solution:
     def findLongestWord(self, s: str, d: [str]) -> str:
-        bit_word = {}
+        bit_word, res = {}, ""
         for _, word in enumerate(d):
-            i = 0
-            curr_bit = 0
+            i, bit = 0, 0
             for c in s:
-                curr_bit <<= 1
-                if i < len(word):
-                    if word[i] == c:
-                        curr_bit += 1
-                        i += 1
+                bit <<= 1
+                if i < len(word) and c == word[i]:
+                    bit += 1
+                    i += 1
             if len(word) != i:
                 continue
-            if curr_bit:
-                bit_word[curr_bit] = min(bit_word[curr_bit], word) if curr_bit in bit_word else word
-        # print(bit_word)
-        res = ""
+            bit_word[bit] = word
+
         for k, v in bit_word.items():
             res = min(res, v) if len(res) == len(v) else max(res, v, key=len)
         return res
