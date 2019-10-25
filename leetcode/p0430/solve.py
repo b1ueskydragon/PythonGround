aaaabe5d63ea05a46909f8455af38d5a1225ffca
ina <dragoalie@gmail.com>
2019-10-25 18:22:29 +0900

    wip - flatten is ok, set prev

--- leetcode/p0430/solve.py
+++ leetcode/p0430/solve.py
@@ -7,12 +7,22 @@ class Node:
 
 
 class Solution:
+    # TODO set prev
     def flatten(self, head: 'Node') -> 'Node':
         if not head:
             return head
+        branch = None
         if head.next:
             if head.child:
                 branch = head.child  # concat when reached the end
                 head.child = None
             self.flatten(head.next)  # go to the next anyway
+            self.flatten(branch)  # if has child, should be go deeper and do same thing
+            tmp_head = head
+            while head.next:
+                head = head.next
+            else:
+                if branch:
+                    head.next = branch
+            head = tmp_head  # revert
         return head
