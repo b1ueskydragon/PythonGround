a9849baf91f2df0aa8047d8fb96383c9d429c771
ina <dragoalie@gmail.com>
2019-10-27 13:21:36 +0900

    solve another - revert pos with get remainder

--- w3resource/pick3.py
+++ w3resource/pick3.py
@@ -3,18 +3,29 @@ alphas = [chr(i) for i in range(97, 123)]
 nums = [10 * i for i in range(1, 10)]
 
 
-def pick3th(xs):
+def pick3th(xs, res):
     pos = 2
     while xs and pos < len(xs):
-        print(xs.pop(pos))
+        res.append(xs.pop(pos))
         pos += 2
         if pos >= len(xs):
             pos -= len(xs)  # revert
     while xs:
-        print(xs.pop(0))
+        res.append(xs.pop(0))
+
+    return res
 
 
-pick3th(nums)
-print(nums)
-pick3th(alphas)
-print(alphas)
+def pick3th_another(xs, res):
+    i = 0
+    size = len(xs)
+    while size > 0:
+        i = (i + 2) % size  # revert
+        res.append(xs.pop(i))
+        size -= 1
+
+    return res
+
+
+print(pick3th(nums.copy(), []))
+print(pick3th_another(nums.copy(), []))
