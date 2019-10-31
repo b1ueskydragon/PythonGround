7249765c8c6641c92d73dfc5b8142ec2510220e4
ina <dragoalie@gmail.com>
2019-10-31 09:49:45 +0900

    Refactor, Add explanations

--- math/hi_sparse_vector.py
+++ math/hi_sparse_vector.py
@@ -24,16 +24,64 @@ def gen_basic():
     return B  # not a dense matrix.
 
 
+def print_vectors(X):
+    """ just print out """
+
+    """
+    (0, 1)	1.0
+    (0, 2)	2.0
+    (1, 2)	3.0
+    (2, 0)	4.0
+    (2, 1)	5.0
+    (0, 1)	1.0
+    (0, 2)	2.0
+    """
+    print(X)
+
+    """
+    right: row num m (count from zero).
+    left:  indices where the values are.
+    
+    (0, 0)	4.0
+    (0, 1)	5.0
+    """
+    m = 2
+    print(X[m])
+
+
+def print_inner_structures(X):
+    """ just print out """
+
+    """ 
+    a compressed list of non-zero elements. 
+    [1. 2. 3. 4. 5.] 
+    """
+    print(X.data)
+
+    """ 
+    an indices of column that where `X.data` is. count in row direction. 
+    [1 2 2 0 1]
+    """
+    print(X.indices)
+
+    """ 
+    an indices of boundary.
+    [0 2 3 5]
+    
+    boundary       index of X.indices   value of X.indices
+         r0   0    -                    -
+    0 <= r1 < 2    0, 1                 1, 2 
+    2 <= r2 < 3    2                    2
+    3 <= r3 < 5    3, 4                 0, 1
+    """
+    print(X.indptr)
+
+
 if __name__ == '__main__':
     X = gen_basic()
 
-    """ a compressed list of non-zero elements. [1. 2. 3. 4. 5.] """
-    print(X.data)
+    print_vectors(X)
 
-    """ an indices of column that where `X.data` is. count in row direction. [1 2 2 0 1] """
-    print(X.indices)
-
-    # [0 2 3 5]
-    print(X.indptr)
+    print_inner_structures(X)
 
     sys.exit(0)
