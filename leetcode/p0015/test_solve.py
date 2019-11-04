fadd95fd9568d0a7184f6c03eeab81929ceb7192
ina <dragoalie@gmail.com>
2019-11-04 14:31:25 +0900

    Add tests

--- leetcode/p0015/test_solve.py
+++ leetcode/p0015/test_solve.py
@@ -4,23 +4,26 @@ from leetcode.p0015.solve import Solution as A
 
 
 class ThreeSumTest(unittest.TestCase):
-    def test_twoSum(self):
-        a = A()
-        nums = [1, 2, 0, 4, 3, -1, -2, 0]
-        expected = [[-1, 1], [-2, 2], [0, 0]]
-        self.assertEqual(expected, a.twoSum(nums))
-
     def test_threeSum(self):
         a = A()
         nums = [-1, 0, 1, 2, -1, -4]
         expected = [[-1, -1, 2], [-1, 0, 1]]
         self.assertEqual(expected, a.threeSum(nums))
+        self.assertEqual(expected, a.threeSum_improve(nums))
 
     def test_threeSum01(self):
         a = A()
         nums = [1, -6, 4, 2, -1, 0, 2, 0, -2, 0]
         expected = [[-1, 0, 1], [-6, 2, 4], [0, 0, 0], [-2, 0, 2]]
         self.assertEqual(expected, a.threeSum(nums))
+        self.assertEqual(expected, a.threeSum_improve(nums))
+
+    def test_threeSum_less_than_3(self):
+        a = A()
+        nums = [-1, 1]
+        expected = []
+        self.assertEqual(expected, a.threeSum(nums))
+        self.assertEqual(expected, a.threeSum_improve(nums))
 
 
 if __name__ == '__main__':
