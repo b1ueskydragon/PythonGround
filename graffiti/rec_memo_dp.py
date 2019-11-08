ff1db90afbcc88636a0765fff361a47c8032b4bf
ina <dragoalie@gmail.com>
2019-11-09 00:33:01 +0800

    Add dp solution to reuse, Add memos

--- graffiti/rec_memo_dp.py
+++ graffiti/rec_memo_dp.py
@@ -5,6 +5,22 @@
 
 
 def fib_rec(n):
+    """
+    top-down approach.
+    n, n-1, n-2 ...
+
+             n
+           /   \
+       n-1    n-2
+       / \     / \
+     n-2 n-3 n-4 n-3
+           ...
+
+    calling same number and this means calc same thing again.
+
+    :param n:
+    :return:
+    """
     if n <= 1:
         return 1
     return fib_rec(n - 2) + fib_rec(n - 1)
@@ -24,12 +40,20 @@ def fib_memo(n):
 
 
 def fib_dp(n):
+    """
+    Bottom-up approach (write recursion iteratively).
+    0, 1, ... , n
+
+    :param n:
+    :return:
+    """
     fibs = {0: 1, 1: 1}
-    # TODO
+    for i in range(2, n):
+        fibs[i] = fibs[i - 1] + fibs[i - 2]
     return fibs
 
 
 if __name__ == '__main__':
     n = 10
     print([fib_rec(i) for i in range(n)])
-    print([fib_tail_rec(1, i) for i in range(n)])
+    print(list(fib_dp(n).values()))
