2c0cf56f2248808722b5ecdf2c4d578732574e83
ina <dragoalie@gmail.com>
2019-11-16 01:04:24 +0900

    another solution with re-use prev num

---
+++ leetcode/p0013/solve01.py
@@ -0,0 +1,20 @@
+class Solution:
+    def __init__(self):
+        self.table = {"I": 1,
+                      "V": 5,
+                      "X": 10,
+                      "L": 50,
+                      "C": 100,
+                      "D": 500,
+                      "M": 1000}
+
+    def romanToInt(self, s: str) -> int:
+        res = 0
+        prev_num = max(self.table.values()) + 1  # anything bigger than table values
+        for c in s:
+            num = self.table[c]
+            if prev_num < num:
+                res = res - (prev_num * 2)
+            res += num
+            prev_num = num
+        return res
--- leetcode/p0013/test_romanToInt.py
+++ leetcode/p0013/test_romanToInt.py
@@ -1,29 +1,34 @@
 import unittest
 
 from leetcode.p0013.solve import Solution as A
+from leetcode.p0013.solve01 import Solution as B
 
 
 class RomanToIntTest(unittest.TestCase):
     def test_addition_only(self):
-        a = A()
+        a, b = A(), B()
         s = "LVIII"
         expected = 58
         self.assertEqual(expected, a.romanToInt(s))
+        self.assertEqual(expected, b.romanToInt(s))
 
     def test_only_one_subtract(self):
-        a = A()
+        a, b = A(), B()
         s = "IV"
         expected = 4  # -1 + 5
         self.assertEqual(expected, a.romanToInt(s))
+        self.assertEqual(expected, b.romanToInt(s))
 
     def test_complex_but_only_one_subtract(self):
-        a = A()
+        a, b = A(), B()
         s = "MCMXCIV"
         expected = 1994
         self.assertEqual(expected, a.romanToInt(s))
+        self.assertEqual(expected, b.romanToInt(s))
 
     def test_one_more_subtracts(self):
-        a = A()
+        a, b = A(), B()
         s = "MMXLMDCIII"
         expected = 3543  # 1000 + 1000 - 10 - 50 + 1000 + 500 + 100 + 1  + 1 + 1
         self.assertEqual(expected, a.romanToInt(s))
+        self.assertEqual(expected, b.romanToInt(s))
