a7271711fd944e3135ac268b6150c84a19bed5b0
ina <dragoalie@gmail.com>
2019-11-17 18:21:17 +0900

    Append first and pop again if not valid, else go deeper

--- retrospective/flight_itinerary.py
+++ retrospective/flight_itinerary.py
@@ -6,3 +6,14 @@ def flight_itinerary(remains, journey):
     """
     if not remains:  # base case
         return journey
+
+    last_stop = journey[-1]
+
+    for i, (departure, arrival) in enumerate(remains):
+        journey.append(arrival)
+        if departure == last_stop:  # going deeper if only has valid itinerary
+            return flight_itinerary(remains[:i] + remains[i + 1:], journey)
+        else:
+            journey.pop()  # cannot use
+
+    return []  # if remains, the journey is incompleted.
