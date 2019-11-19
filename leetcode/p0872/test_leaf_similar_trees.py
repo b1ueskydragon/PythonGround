60043772eef74b2995acca7b6fe4b6279e42c131
ina <dragoalie@gmail.com>
2019-11-20 00:20:20 +0900

    Add test cases

--- leetcode/p0872/test_leaf_similar_trees.py
+++ leetcode/p0872/test_leaf_similar_trees.py
@@ -4,31 +4,22 @@ from leetcode.p0872.solve import Solution as A
 from leetcode.p0872.solve import TreeNode as Node
 
 
-class IntToRomanTest(unittest.TestCase):
-    """
-    case 1
-    [3,5,1,6,2,9,10,null,null,7,4]
-    [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
-
-    case 2
-    [1,null,2,null,3,null,6, 5, 4]
-    [1,2,null,3,6,null,4,null, 5]
-
-
-    case 3
-    [1,null,2,null,3,null,4]
-    [1,2,null,3,null,4]
-
-    case 4 --- TODO
-    [44,79,25,null,null,112,7,74,49,2,122]
-    [38,86,120,49,54,2,122,null,null,74,79]
-    """
-
-    def test_same_direction_diff_depth(self):
+class LeafSimilarTest(unittest.TestCase):
+    def test_same_order_diff_depth(self):
         a = A()
-        root1 = Node(3)
-        root1 = Node(5)
 
+        # [3,5,1,6,2,9,8,null,null,7,4]
+        root1 = Node(3)
+        root1.left = Node(5)
+        root1.right = Node(1)
+        root1.left.left = Node(6)
+        root1.left.right = Node(2)
+        root1.right.left = Node(9)
+        root1.right.right = Node(8)
+        root1.left.right.left = Node(7)
+        root1.left.right.right = Node(4)
+
+        # [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
         root2 = Node(3)
         root2.left = Node(5)
         root2.right = Node(1)
@@ -40,3 +31,68 @@ class IntToRomanTest(unittest.TestCase):
         root2.right.right.right = Node(8)
 
         self.assertTrue(a.leafSimilar(root1, root2))
+
+    def test_diff_order_diff_depth(self):
+        a = A()
+
+        # [1,null,2,null,3,null,6,5,4]
+        root1 = Node(1)
+        root1.right = Node(2)
+        root1.right.right = Node(3)
+        root1.right.right.right = Node(6)
+        root1.right.right.right.left = Node(5)
+        root1.right.right.right.right = Node(4)
+
+        # [1,2,null,3,6,null,4,null,5]
+        root2 = Node(1)
+        root2.left = Node(2)
+        root2.left.left = Node(3)
+        root2.left.right = Node(6)
+        root2.left.left.right = Node(4)
+        root2.left.right.right = Node(5)
+
+        self.assertFalse(a.leafSimilar(root1, root2))
+
+    def test_linear_tree_diff_direction_same_depth(self):
+        a = A()
+
+        # [1,null,2,null,3,null,4]
+        root1 = Node(1)
+        root1.right = Node(2)
+        root1.right.right = Node(3)
+        root1.right.right.right = Node(4)
+
+        # [1,2,null,3,null,4]
+        root2 = Node(1)
+        root2.left = Node(2)
+        root2.left.left = Node(3)
+        root2.left.left.left = Node(4)
+        self.assertTrue(a.leafSimilar(root1, root2))
+
+    def test_diff_order_same_depth(self):
+        # TODO pass the test
+        a = A()
+
+        # [44,79,25,null,null,112,7,74,49,2,122]
+        root1 = Node(44)
+        root1.left = Node(79)
+        root1.right = Node(25)
+        root1.right.left = Node(112)
+        root1.right.right = Node(7)
+        root1.right.left.left = Node(74)
+        root1.right.left.right = Node(49)
+        root1.right.right.left = Node(2)
+        root1.right.right.right = Node(122)
+
+        # [38,86,120,49,54,2,122,null,null,74,79]
+        root2 = Node(38)
+        root2.left = Node(86)
+        root2.right = Node(120)
+        root2.left.left = Node(49)
+        root2.left.right = Node(54)
+        root2.right.left = Node(2)
+        root2.right.right = Node(122)
+        root2.left.right.left = Node(74)
+        root2.left.right.right = Node(79)
+
+        self.assertFalse(a.leafSimilar(root1, root2))
