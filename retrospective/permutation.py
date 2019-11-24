b2103e911d536fcb5a9913a44b7ab62b68102000
ina <dragoalie@gmail.com>
2019-11-24 20:46:57 +0900

    Make 2 to k to input manual

--- retrospective/permutation.py
+++ retrospective/permutation.py
@@ -1,11 +1,15 @@
-def permutation(xs):
-  res = []
-  for i, x in enumerate(xs):
-    _xs = xs[:i] + xs[i + 1:]
-    for _x in _xs: # TODO
-      res.append(x + _x)
-    
-  return res
-  
+def permutation(xs, k):
+    if k == 0:
+        return [[]]
+    res = []
+    for i, x in enumerate(xs):
+        _xs = permutation(xs[:i] + xs[i + 1:], k - 1)
+        for _x in _xs:
+            res.append([x] + _x)
+
+    return res
+
+
 xs = ['a', 'b', 'c', 'd']
-print(permutation(xs))
+k = 2
+print(permutation(xs, 2))
