5071973ec63b5d073a3dc9cb715a96711812bece
ina <dragoalie@gmail.com>
2019-11-25 19:32:32 +0900

    Fix outputs only

--- retrospective/permutation.py
+++ retrospective/permutation.py
@@ -11,5 +11,9 @@ def permutation(xs, k):
 
 
 xs = ['a', 'b', 'c', 'd']
-k = 2
-print(permutation(xs, 2))
+res_k2 = list(map(''.join, permutation(xs, k=2)))
+res_k3 = list(map(''.join, permutation(xs, k=3)))
+# TODO calc length  n! / (n-k)!
+# TODO test code
+print(len(res_k2), res_k2)
+print(len(res_k3), res_k3)
