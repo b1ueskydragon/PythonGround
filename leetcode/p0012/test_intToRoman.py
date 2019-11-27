35969a08a38ab66261e951fcc280c6c5adacce3b
ina <dragoalie@gmail.com>
2019-11-28 08:49:46 +0900

    solve - hard code to find pattern and use long dict

--- leetcode/p0012/test_intToRoman.py
+++ leetcode/p0012/test_intToRoman.py
@@ -4,26 +4,32 @@ from leetcode.p0012.solve import Solution as A
 
 
 class IntToRomanTest(unittest.TestCase):
-    def test_addition_only(self):
+    def test_01(self):
         a = A()
         num = 58
         expected = "LVIII"
         self.assertEqual(expected, a.intToRoman(num))
 
-    def test_only_one_subtract(self):
+    def test_02(self):
         a = A()
         num = 4
         expected = "IV"
         self.assertEqual(expected, a.intToRoman(num))
 
-    def test_complex_but_only_one_subtract(self):
+    def test_03(self):
         a = A()
         num = 1994
         expected = "MCMXCIV"
         self.assertEqual(expected, a.intToRoman(num))
 
-    def test_one_more_subtracts(self):
+    def test_04(self):
         a = A()
         num = 3543
-        expected = "MMXLMDCIII"
+        expected = "MMMDXLIII"
+        self.assertEqual(expected, a.intToRoman(num))
+
+    def test_05(self):
+        a = A()
+        num = 2222
+        expected = "MMCCXXII"
         self.assertEqual(expected, a.intToRoman(num))
