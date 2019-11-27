3c1bb82ec25f41caae76a230135b4a3fa64ae7cf
ina <dragoalie@gmail.com>
2019-11-28 08:52:35 +0900

    Refactor hard code to one pattern

--- leetcode/p0012/solve.py
+++ leetcode/p0012/solve.py
@@ -4,33 +4,21 @@ class Solution:
             , 4: "IV", 9: "IX", 40: "XL", 90: "XC", 400: "CD", 900: "CM"}
         self.res = ""
 
-    def _convert(self, x):
+    def _convert(self, x, digit):
         if x in self.dict:
             return self.dict[x]
         else:
-            if 0 < x < 10:
-                if x < 5:
-                    return "I" * (x // 1)
-                else:
-                    return "V" + "I" * (abs(5 - x) // 1)
-            elif 10 < x < 100:
-                if x < 50:
-                    return "X" * (x // 10)
-                else:
-                    return "L" + "X" * (abs(50 - x) // 10)
-            elif 100 < x < 1000:
-                if x < 500:
-                    return "C" * (x // 100)
-                else:
-                    return "D" + "C" * (abs(500 - x) // 100)
+            five = digit * 5
+            if x < five:
+                return self.dict[digit] * (x // digit)
             else:  # max is 3999
-                return "M" * (x // 1000)
+                return self.dict[five] + self.dict[digit] * ((x - five) // digit)
 
     def intToRoman(self, num: int) -> str:
         digit = 10 ** (len(str(num)) - 1)
         while num > 0:
             x = (num // digit) * digit
-            self.res += self._convert(x)
+            self.res += self._convert(x, digit)
             num %= digit
             digit //= 10
         return self.res
