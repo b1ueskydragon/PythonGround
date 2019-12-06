a196a57c6c9c6d34658e76cb6876e1a51006d9a7
ina <dragoalie@gmail.com>
2019-12-07 08:16:23 +0900

    Simplify and make params clear

--- dailyOne/p379/solve.py
+++ dailyOne/p379/solve.py
@@ -9,13 +9,11 @@ def subsequence(string: str):
     [[], [x], [y], [xy]] + [[] + [z], [x] + [z], [y] + [z], [xy] + [z]]
                                       = [[], [x], [y], [xy], [z], [xz], [yz], [xyz]
     """
+
     def _subsequence(s: str, ps: [[str]]) -> [[str]]:
         if not s:
             return ps
         h = s[0]
-        new_ps = []
-        for l in ps:
-            new_ps.append(l + [h])
-        return _subsequence(s[1:], ps + new_ps)
+        return _subsequence(s=s[1:], ps=ps + [l + [h] for l in ps])
 
     return list(map(''.join, _subsequence(string, [[]])))
