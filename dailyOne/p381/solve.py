4054e8e38bbc941962fa3095286d3c9b889439fa
ina <dragoalie@gmail.com>
2019-12-08 13:01:15 +0900

    add quiz

---
+++ dailyOne/p381/solve.py
@@ -0,0 +1,12 @@
+"""
+Implement a function that converts a hex string to base64.
+Base64 is meant to encode bytes (8 bit).
+"""
+
+import base64
+
+hex_string = 'deadbeef'
+expected = '3q2+7w=='
+
+def convert(hex_string):
+    return
