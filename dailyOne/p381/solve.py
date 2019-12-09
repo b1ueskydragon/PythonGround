bfcf39cc5778884a70043cec4866ffbcea6b92c9
ina <dragoalie@gmail.com>
2019-12-09 11:15:08 +0900

    fundamental usgae

--- dailyOne/p381/solve.py
+++ dailyOne/p381/solve.py
@@ -1,12 +1,23 @@
 """
 Implement a function that converts a hex string to base64.
 Base64 is meant to encode bytes (8 bit).
+
+see also: https://en.wikipedia.org/wiki/Base64
 """
 
 import base64
 
-hex_string = 'deadbeef'
-expected = '3q2+7w=='
+# fundamental usage
+hex_string = b'\xde\xad\xbe\xef'
+byte_array = b'3q2+7w=='
 
-def convert(hex_string):
-    return
+print(base64.b64encode(hex_string))
+print(base64.b64decode(byte_array))
+
+hex_int = int('deadbeef', 16)
+print(hex_int)
+print(bin(hex_int))
+# 1101\1110\1010\1101\1011\1110\1110\1111
+#   d    e    a    d    b    e    e    f
+# 110111\101010\110110\111110\111011\11 00 00
+#   3      q       2      +     7     w  =  =   # padding
