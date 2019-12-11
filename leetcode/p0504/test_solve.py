27c4f216d265c73c8fb8750f49d1802edf0d4d7d
ina <dragoalie@gmail.com>
2019-12-11 18:37:19 +0900

    Add a case that adding all digits

--- leetcode/p0504/test_solve.py
+++ leetcode/p0504/test_solve.py
@@ -15,3 +15,7 @@ class ConvertToBase7Test(unittest.TestCase):
     def test_zero(self):
         a = A()
         self.assertEqual("0", a.convertToBase7(0))
+
+    def test_stack_all_digits(self):
+        a = A()
+        self.assertEqual("150666342", a.convertToBase7(9999999))
