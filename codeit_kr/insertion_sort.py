d457b0c9d8a1cce8cb7702702aec3b80fcfae813
ina <dragoalie@gmail.com>
2019-12-19 01:07:13 +0900

    Rm redundant comments

--- codeit_kr/insertion_sort.py
+++ codeit_kr/insertion_sort.py
@@ -1,25 +1,16 @@
-# 挿入ソート
 def insertion_sort(list):
     for index in range(1, len(list)):
-
-        value = list[index]  # 比較対象の探索
-
+        value = list[index]
         i = index - 1
-        # compare value to each item to the left on it
-        #  keep taking i further left, until i == beginning of the list (0)
-
         while i >= 0:
             if value < list[i]:
-                list[i + 1] = list[i]  # shift number in slot i right to i+1
-                list[i] = value  # shift value left into slot i
-
-               # list[i], list[i + 1] = list[i + 1], list[i]  # にしても同じ結果ー。
-
-                i -= 1  # need decremented i
-            else:  # when don't need shift value anymore (already-right-placed)
+                list[i + 1] = list[i]
+                list[i] = value
+                i -= 1
+            else:
                 break
-
     return list
+
 
 some_list = [11, 3, 6, 4, 12, 1, 2]
 print(insertion_sort(some_list))
--- codeit_kr/insertion_sort__.py
+++ codeit_kr/insertion_sort__.py
@@ -2,20 +2,14 @@ def insertion_sort(list):
     for i in range(len(list)):
         key = list[i]
 
-        # i - 1 からスタート、左方向に一つずつ探索
-        # 左の端([0])まで探索し終えたか、
-        # key の入る場所が見つかったら終了
+        j = i - 1
 
-        j = i - 1  # 必ず i よりは左で (そして0よりは同じか大きい)
-
-        while j >= 0 and list[j] > key:  # 交換し続ける条件
+        while j >= 0 and list[j] > key:
             list[j + 1] = list[j]
-            j -= 1  # さらに左へ移動
+            j -= 1
 
-        # while の外
-        # key が入る場所に挿入
-        # 左の端に到着して、j が -1 なら [0] に key を挿入
         list[j + 1] = key
+
 
 some_list = [11, 3, 6, 4, 4, 12, 1, 2, 3]
 insertion_sort(some_list)
