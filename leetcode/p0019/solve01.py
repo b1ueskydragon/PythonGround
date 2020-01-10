c06af3a205ddf47dc9e1adee43279ab1e2221ad4
ina <dragoalie@gmail.com>
2020-01-10 13:54:39 +0900

    pointer or not

--- leetcode/p0019/solve01.py
+++ leetcode/p0019/solve01.py
@@ -11,16 +11,21 @@ class Solution:
     """
 
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        j = 0
-        i = j - n
-        res = ListNode(head.val)
-        point = res
-        while head.next:
-            j += 1
-            i += 1
-            head = head.next
-            if head:
-                point.next = head
-                print(i, j)
-                point = point.next
-        return res
+        # j = 0
+        # i = j - n
+        # res = ListNode(head.val)
+        # point = res
+        # while head.next:
+        #     j += 1
+        #     i += 1
+        #     head = head.next
+        #     if head:
+        #         point.next = head
+        #         print(i, j)
+        #         point = point.next
+        # return res
+        p1 = head
+        p2 = head
+        while n:
+            p2 = p2.next
+            n -= 1
