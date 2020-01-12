cd07e888a0a0485127bba2fc161615018106d346
ina <dragoalie@gmail.com>
2020-01-12 17:17:13 +0900

    Insertion sort with a write back

--- graffiti/insertion_sort.py
+++ graffiti/insertion_sort.py
@@ -1,3 +1,10 @@
 def insertion_sort(nums):
-    for i, num in enumerate(nums, start=1):
-        j = i - 1
+    """ in-place """
+    for i in range(1, len(nums)):
+        fixed_ahead = nums[i]
+        j = i - 1  # followed
+        while j > -1 and nums[j] > fixed_ahead:
+            nums[j + 1] = nums[j]
+            nums[j] = fixed_ahead
+            j -= 1  # write back
+    return nums
--- graffiti/insertion_sort_test.py
+++ graffiti/insertion_sort_test.py
@@ -1,15 +1,25 @@
 import unittest
 
+from graffiti.insertion_sort import insertion_sort
+
 
 class InsertionSortTest(unittest.TestCase):
     def test_odd_len_nums(self):
         nums = [11, 3, 6, 4, 12, 1, 2]
-        self.assertEqual(False, True)
+        self.assertEqual(list(sorted(nums)), insertion_sort(nums))
 
     def test_even_len_nums(self):
         nums = [11, 3, 6, 4, 5, 12, 1, 2]
-        self.assertEqual(False, True)
+        self.assertEqual(list(sorted(nums)), insertion_sort(nums))
 
     def test_has_duplicated_elements(self):
         nums = [11, 3, 6, 4, 4, 12, 1, 2, 3]
-        self.assertEqual(False, True)
+        self.assertEqual(list(sorted(nums)), insertion_sort(nums))
+
+    def test_empty(self):
+        nums = []
+        self.assertEqual(list(sorted(nums)), insertion_sort(nums))
+
+    def test_single_element(self):
+        nums = [-1]
+        self.assertEqual(list(sorted(nums)), insertion_sort(nums))
