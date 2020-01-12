df170973a8acb1189ce749a96127e00e7cd2f9d9
ina <dragoalie@gmail.com>
2020-01-12 18:45:30 +0900

    Improve to set j = i

--- graffiti/insertion_sort.py
+++ graffiti/insertion_sort.py
@@ -2,9 +2,9 @@ def insertion_sort(nums):
     """ in-place """
     for i in range(1, len(nums)):
         fixed_ahead = nums[i]
-        j = i - 1  # followed
-        while j > -1 and nums[j] > fixed_ahead:
-            nums[j + 1] = nums[j]
-            nums[j] = fixed_ahead
+        j = i  # followed
+        while j > 0 and nums[j - 1] > fixed_ahead:
+            nums[j] = nums[j - 1]
             j -= 1  # write back
+        nums[j] = fixed_ahead
     return nums
