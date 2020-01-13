521a12eef5bd7a7b52d669562985c01f97527c0f
ina <dragoalie@gmail.com>
2020-01-13 22:28:31 +0900

    Insertion sort desc uses reverse writing and sentinel

--- graffiti/insertion_sort.py
+++ graffiti/insertion_sort.py
@@ -8,3 +8,19 @@ def insertion_sort(nums):
             j -= 1  # write back
         nums[j] = fixed_ahead
     return nums
+
+
+def insertion_sort_desc(nums):
+    """ in-place """
+    n = len(nums)
+    min_int = -2147483648
+    nums.append(min_int)  # sentinel to skip checking n > j
+
+    for i in reversed(range(n - 1)):
+        fixed_ahead = nums[i]
+        j = i + 1
+        while nums[j] > fixed_ahead:
+            nums[j - 1] = nums[j]
+            j += 1
+        nums[j - 1] = fixed_ahead
+    return nums[:-1]
--- graffiti/insertion_sort_test.py
+++ graffiti/insertion_sort_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from graffiti.insertion_sort import insertion_sort
+from graffiti.insertion_sort import insertion_sort, insertion_sort_desc
 
 
 class InsertionSortTest(unittest.TestCase):
@@ -23,3 +23,11 @@ class InsertionSortTest(unittest.TestCase):
     def test_single_element(self):
         nums = [-1]
         self.assertEqual(list(sorted(nums)), insertion_sort(nums))
+
+    def test_insertion_sort_desc(self):
+        nums = [5, 4, 1, 2, 3]
+        self.assertEqual(list(sorted(nums, reverse=True)), insertion_sort_desc(nums))
+
+    def test_has_duplicated_elements_desc(self):
+        nums = [11, 3, 6, 4, 4, 12, 1, 2, 3]
+        self.assertEqual(list(sorted(nums, reverse=True)), insertion_sort_desc(nums))
