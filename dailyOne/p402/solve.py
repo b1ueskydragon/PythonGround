1a0a9e815b4abbda82c99ad0456540e05074d6a8
ina <dragoalie@gmail.com>
2020-01-19 00:07:17 +0900

    wip note

--- dailyOne/p402/solve.py
+++ dailyOne/p402/solve.py
@@ -1,5 +1,7 @@
 def strobogrammatic_numbers(n):
     """
+    DP solution
+
     :param n: digits
     :return: strobogrammatic numbers with N digits
     """
@@ -15,9 +17,31 @@ def strobogrammatic_numbers(n):
             c = 8 * 10 ** (n - 1) + 8
             d = 9 * 10 ** (n - 1) + 6
             for s in [a, b, c, d]:
+
+                # TODO: fix to the loop
+                # e. g.) when n = 7, add the f(3).map(x => x * 100) to each seeds
+                """
+                100 0 001, # n = 1
+                100 1 001,
+                100 8 001,
+                
+                10 101 01, # n = 3
+                10 111 01,
+                10 181 01,
+                1060901,
+                1061901,
+                1068901,
+                1080801,
+                1081801,
+                1088801,
+                1090601,
+                1091601,
+                1098601,
+                """
                 res.append(s)
                 res.append(s + 10 ** (n // 2))
                 res.append(s + 8 * 10 ** (n // 2))
+
                 if n == 3:
                     continue  # skip a duplicate
                 for x in prev:
