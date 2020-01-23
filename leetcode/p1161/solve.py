dce30f7b3fe8753fddb0fc51bf6786ac2f61d5f3
ina <dragoalie@gmail.com>
2020-01-24 00:00:21 +0900

    wip - use a queue of pairs

--- leetcode/p1161/solve.py
+++ leetcode/p1161/solve.py
@@ -1,7 +1,3 @@
-import math
-from collections import deque
-
-
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -11,34 +7,5 @@ class TreeNode:
 
 class Solution:
     def maxLevelSum(self, root: TreeNode) -> int:
-        level = 1
-        q = deque([root])
-        mapping = []
-        while q:
-            parent = q.popleft()
-            x = 0
-            if parent:
-                x = parent.val
-                q.append(parent.left)
-                q.append(parent.right)
-            mapping.append((level, x))
-            level += 1
-        # print(mapping)
-        r = deque(mapping)
-        max_sum, tmp = (0, -2147483648), 0
-        table = {1, 2, 4, 8}
-        start, end = 1, 2
-        while r:
-            while start < end:
-                if not r:
-                    break
-                l, v = r.popleft()
-                tmp += v
-                start += 1
-            else:
-                if max_sum[1] < tmp:
-                    max_sum = (int(math.log2(end)), tmp)
-                tmp = 0
-                start = end
-                end <<= 1
-        return max_sum[0]
+        level = [(root, 1)]
+        max_val = (root.val, 1)  # or insert a sentinel
