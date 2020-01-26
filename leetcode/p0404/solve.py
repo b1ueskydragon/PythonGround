001b7c72ca5980f6513201dbf963ad3a88d2beee
ina <dragoalie@gmail.com>
2020-01-26 18:25:09 +0900

    solved - bfs, get only leaves have not children

--- leetcode/p0404/solve.py
+++ leetcode/p0404/solve.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -5,18 +8,17 @@ class TreeNode:
         self.right = None
 
 
-from collections import deque
-
-
 class Solution:
     def sumOfLeftLeaves(self, root: TreeNode) -> int:
-        q = deque([root])
         left_acc = 0
-        while q:
-            node = q.popleft()
-            if node.left:
-                left_acc += node.left.val
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)
+        if root:
+            q = deque([root])
+            while q:
+                node = q.popleft()
+                if node.left:
+                    if not node.left.left and not node.left.right:
+                        left_acc += node.left.val
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
         return left_acc
--- leetcode/p0404/test_sum_of_left_leaves.py
+++ leetcode/p0404/test_sum_of_left_leaves.py
@@ -4,8 +4,8 @@ from leetcode.p0404.solve import TreeNode, Solution as A
 
 
 class SumOfLeftLeavesTest(unittest.TestCase):
-    def test_large(self):
-        # [3, 9, 20, null, null, 15, 7]
+    def test_sample(self):
+        # [3,9,20,null,null,15,7]
         a = A()
         root = TreeNode(3)
         root.left = TreeNode(9)
@@ -15,9 +15,27 @@ class SumOfLeftLeavesTest(unittest.TestCase):
 
         self.assertEqual(24, a.sumOfLeftLeaves(root))
 
-    # TODO :
-    #  pass the case
-    #  [1,2,3,null,4,5,6,null,null,7,8,9,10,11,null,12]
+    def test_empty(self):
+        # []
+        a = A()
+        self.assertEqual(0, a.sumOfLeftLeaves(None))
+
+    def test_get_only_leaves_have_not_children(self):
+        #  [1,2,3,null,4,5,6,null,null,7,8,9,10,11,null,12]
+        a = A()
+        root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.right = TreeNode(3)
+        root.left.right = TreeNode(4)
+        root.right.left = TreeNode(5)
+        root.right.right = TreeNode(6)
+        root.right.left.left = TreeNode(7)
+        root.right.left.right = TreeNode(8)
+        root.right.right.left = TreeNode(9)  # acc
+        root.right.right.right = TreeNode(10)
+        root.right.left.left.left = TreeNode(11)  # acc
+        root.right.left.right.left = TreeNode(12)  # acc
+        self.assertEqual(32, a.sumOfLeftLeaves(root))
 
 
 if __name__ == '__main__':
