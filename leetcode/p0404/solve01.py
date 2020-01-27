070511d9cbda776dfbc4e78467c8c60c00de1ab4
ina <dragoalie@gmail.com>
2020-01-27 11:21:05 +0900

    solve - DFS, if not left but right, no return but proceed

--- leetcode/p0404/solve01.py
+++ leetcode/p0404/solve01.py
@@ -8,9 +8,8 @@ class TreeNode:
 class Solution:
     def sumOfLeftLeaves(self, root: TreeNode, is_left: bool = False) -> int:
         if not root:
-            return 0
-        if is_left and not root.left:
-            if root.right:
+            if not root:
                 return 0
+        if is_left and not root.left and not root.right:
             return root.val
         return self.sumOfLeftLeaves(root.left, True) + self.sumOfLeftLeaves(root.right, False)
