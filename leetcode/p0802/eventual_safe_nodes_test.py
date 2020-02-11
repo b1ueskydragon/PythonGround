d59f413985532e39a1c707bafd3c568375b48e38
ina <dragoalie@gmail.com>
2020-02-12 02:08:22 +0900

    Fix and add tests

--- leetcode/p0802/eventual_safe_nodes_test.py
+++ leetcode/p0802/eventual_safe_nodes_test.py
@@ -4,8 +4,14 @@ from leetcode.p0802.solve import Solution as A
 
 
 class EventualSafeStatesTest(unittest.TestCase):
-    def test_basic_balanced_leaf(self):
+    def test_sample(self):
         a = A()
         graph = [[1, 2], [2, 3], [5], [0], [5], [], []]
+        expected = [2, 4, 5, 6]
+        self.assertEqual(expected, a.eventualSafeNodes(graph))
+
+    def test_sample_node_4_goes_back(self):
+        a = A()
+        graph = [[1, 2], [2, 3], [5], [0], [2], [], []]
         expected = [2, 4, 5, 6]
         self.assertEqual(expected, a.eventualSafeNodes(graph))
