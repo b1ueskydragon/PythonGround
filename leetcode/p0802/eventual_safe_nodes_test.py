443092004f8f0e4a6c1297d87bf4fcc77e842543
ina <dragoalie@gmail.com>
2020-02-14 00:00:57 +0900

    Add a case that has chances of both circle and terminal

--- leetcode/p0802/eventual_safe_nodes_test.py
+++ leetcode/p0802/eventual_safe_nodes_test.py
@@ -27,3 +27,10 @@ class EventualSafeStatesTest(unittest.TestCase):
         graph = [[0]]
         expected = []
         self.assertEqual(expected, a.eventualSafeNodes(graph))
+
+    def test_just_one_goal(self):
+        a = A()
+        graph = [[1, 2, 3, 4], [1, 2], [3, 4], [0, 4], []]
+        # i=0 has a chance to be a circle
+        expected = [4]
+        self.assertEqual(expected, a.eventualSafeNodes(graph))
