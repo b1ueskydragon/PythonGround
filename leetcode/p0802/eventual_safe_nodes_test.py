8154ee24cfe33cb391a8f263c3cc330865de5f30
ina <dragoalie@gmail.com>
2020-02-13 22:25:20 +0900

    Add edge test cases

--- leetcode/p0802/eventual_safe_nodes_test.py
+++ leetcode/p0802/eventual_safe_nodes_test.py
@@ -15,3 +15,15 @@ class EventualSafeStatesTest(unittest.TestCase):
         graph = [[1, 2], [2, 3], [5], [0], [2], [], []]
         expected = [2, 4, 5, 6]
         self.assertEqual(expected, a.eventualSafeNodes(graph))
+
+    def test_simple_circle(self):
+        a = A()
+        graph = [[1], [0]]
+        expected = []
+        self.assertEqual(expected, a.eventualSafeNodes(graph))
+
+    def test_single_element_return_to_itself(self):
+        a = A()
+        graph = [[0]]
+        expected = []
+        self.assertEqual(expected, a.eventualSafeNodes(graph))
