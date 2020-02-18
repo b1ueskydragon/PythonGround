b7633dd6825f7ac7f87d8c91221c4f000429fa66
ina <dragoalie@gmail.com>
2020-02-19 00:03:21 +0900

    improve - for-comprehension, add overall visited set to skip more

--- leetcode/p0802/solve.py
+++ leetcode/p0802/solve.py
@@ -1,25 +1,25 @@
 class Solution:
     def __init__(self):
-        self.res = []
         self.terminals = set()
-        # self.visited = set()  # overall
+        self.visitedRoots = set()  # overall
 
     def eventualSafeNodes(self, graph: [[int]]) -> [int]:
         self.terminals = {node for node, end_points in enumerate(graph) if not end_points}
-        for node, _ in enumerate(graph):
-            if self.has_terminal_node(graph, node, set()):
-                self.res.append(node)
-        return self.res
+        return [node for node, _ in enumerate(graph) if self.is_terminal_node(graph, node, set())]
 
-    def has_terminal_node(self, graph, start, visited):
-        if start in self.terminals:  # since this a terminal node
+    def is_terminal_node(self, graph, start, visited):
+        if start in self.terminals:
             return True
+        if start in self.visitedRoots:
+            return False
         if start in visited:
             return False
         visited.add(start)  # during the specific start point
         for step in graph[start]:
-            if self.has_terminal_node(graph, step, visited):
+            if self.is_terminal_node(graph, step, visited):
                 self.terminals.add(step)
             else:
+                self.visitedRoots.add(start)
+                visited.add(step)
                 return False
         return True
