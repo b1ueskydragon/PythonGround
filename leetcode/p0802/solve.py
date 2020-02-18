6e7d2aaee282aadeb94cc6a4776d3e8492e82002
ina <dragoalie@gmail.com>
2020-02-18 23:04:58 +0900

    solved with remember terminals first but very slow

--- leetcode/p0802/solve.py
+++ leetcode/p0802/solve.py
@@ -1,22 +1,25 @@
 class Solution:
     def __init__(self):
         self.res = []
+        self.terminals = set()
         # self.visited = set()  # overall
 
     def eventualSafeNodes(self, graph: [[int]]) -> [int]:
+        self.terminals = {node for node, end_points in enumerate(graph) if not end_points}
         for node, _ in enumerate(graph):
             if self.has_terminal_node(graph, node, set()):
                 self.res.append(node)
         return self.res
 
     def has_terminal_node(self, graph, start, visited):
-        end_points = graph[start]
-        if not end_points:  # a terminal node
+        if start in self.terminals:  # since this a terminal node
             return True
         if start in visited:
             return False
-        visited.add(start)  # at a specific start point
-        for step in end_points:
-            if not self.has_terminal_node(graph, step, visited):
+        visited.add(start)  # during the specific start point
+        for step in graph[start]:
+            if self.has_terminal_node(graph, step, visited):
+                self.terminals.add(step)
+            else:
                 return False
         return True
