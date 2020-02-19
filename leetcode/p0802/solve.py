36bff53cea1221fe15c807acaac2a4e5f7930d6e
ina <dragoalie@gmail.com>
2020-02-19 09:44:02 +0900

    Simplify

--- leetcode/p0802/solve.py
+++ leetcode/p0802/solve.py
@@ -1,25 +1,22 @@
 class Solution:
     def __init__(self):
-        self.terminals = set()
-        self.visitedRoots = set()  # overall
+        self.res = set()
+        self.visited = set()
 
     def eventualSafeNodes(self, graph: [[int]]) -> [int]:
-        self.terminals = {node for node, end_points in enumerate(graph) if not end_points}
-        return [node for node, _ in enumerate(graph) if self.is_terminal_node(graph, node, set())]
+        for node, end_points in enumerate(graph):
+            if node not in self.visited:
+                self.is_terminal_node(node, graph)
+        return sorted(self.res)
 
-    def is_terminal_node(self, graph, start, visited):
-        if start in self.terminals:
+    def is_terminal_node(self, start, graph):
+        if start in self.res:
             return True
-        if start in self.visitedRoots:
+        if start in self.visited:
             return False
-        if start in visited:
-            return False
-        visited.add(start)  # during the specific start point
+        self.visited.add(start)
         for step in graph[start]:
-            if self.is_terminal_node(graph, step, visited):
-                self.terminals.add(step)
-            else:
-                self.visitedRoots.add(start)
-                visited.add(step)
+            if not self.is_terminal_node(step, graph):
                 return False
+        self.res.add(start)
         return True
