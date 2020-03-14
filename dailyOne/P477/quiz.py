3574f98af89a5d85f688ef60e2aa2d99fa1595d3
ina <dragoalie@gmail.com>
2020-03-14 10:10:41 +0900

    comments

--- dailyOne/P477/quiz.py
+++ dailyOne/P477/quiz.py
@@ -3,7 +3,7 @@ What does the below code snippet print out?
 """
 functions = []
 for i in range(10):
-    functions.append(lambda: i)
+    functions.append(lambda: i)  # i is mutated, and the closures all refer to the same i.
 
 for f in functions:
     print(f())
--- dailyOne/P477/solve.py
+++ dailyOne/P477/solve.py
@@ -3,7 +3,7 @@ How can we fix the anonymous functions to behave as we'd expect?
 """
 functions = []
 for i in range(10):
-    functions.append(lambda i: i)
+    functions.append(lambda i: i)  # create local i
 
 for i, f in enumerate(functions):
-    print(f(i))
+    print(f(i))  # pass i from outside
