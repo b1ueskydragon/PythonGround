321c96919145f2f9727d69247eb5c78f34b4e132
ina <dragoalie@gmail.com>
2020-03-20 10:22:56 +0900

    solved - improve pop solution, pop i during O(N) retrieval

---
+++ leetcode/p0026/solve02.py
@@ -0,0 +1,14 @@
+"""
+Simple pop solution. pop specific index.
+"""
+
+
+class Solution:
+    def removeDuplicates(self, nums: [int]) -> int:
+        i = 0
+        while i < len(nums) - 1:
+            if nums[i] == nums[i + 1]:
+                nums.pop(i)
+            else:
+                i += 1
+        return len(nums)
