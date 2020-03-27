421c0634a95423ea2331b2b03e5b6ac3eac66625
ina <dragoalie@gmail.com>
2020-03-27 12:26:07 +0900

    :p

--- dailyOne/p489/length_of_longest_subarray_test.py
+++ dailyOne/p489/length_of_longest_subarray_test.py
@@ -1,25 +1,25 @@
 from unittest import TestCase
 
-from dailyOne.p489.solve import length_of_longest_subarray
+from dailyOne.p489.solve import length_of_longest_subarray as a
 
 
 class LengthOfLongestSubArrayTest(TestCase):
     def test_numbers(self):
-        xs = [5, 1, 3, 5, 2, 3, 4, 1]
-        self.assertEqual(5, length_of_longest_subarray(xs))  # "[5, 2, 3, 4, 1]"
+        xs = [5, 1, 3, 5, 2, 3, 4, 1]  # [5, 2, 3, 4, 1]
+        self.assertEqual(5, a(xs))
 
     def test_substring(self):
-        xs = ['a', 'b', 'c', 'a', 'b', 'c', 'b', 'b']
-        self.assertEqual(3, length_of_longest_subarray(xs))  # "abc"
+        xs = ['a', 'b', 'c', 'a', 'b', 'c', 'b', 'b']  # "abc"
+        self.assertEqual(3, a(xs))
 
     def test_substring_single_element(self):
-        xs = ['b'] * 5
-        self.assertEqual(1, length_of_longest_subarray(xs))  # "b"
+        xs = ['b'] * 5  # "b"
+        self.assertEqual(1, a(xs))
 
     def test_substring_not_a_subsequence(self):
-        xs = ['p', 'w', 'w', 'k', 'e', 'w']
-        self.assertEqual(3, length_of_longest_subarray(xs))  # "wke"
+        xs = ['p', 'w', 'w', 'k', 'e', 'w']  # "wke"
+        self.assertEqual(3, a(xs))
 
     def test_should_not_skip_too_much(self):
-        xs = ['p', 'w', 'w', 'k', 'e', 'w', 'p']
-        self.assertEqual(4, length_of_longest_subarray(xs))  # "kewp"
+        xs = ['p', 'w', 'w', 'k', 'e', 'w', 'p']  # "kewp"
+        self.assertEqual(4, a(xs))
