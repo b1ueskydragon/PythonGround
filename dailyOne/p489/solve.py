f588721b6b199c48a46398816764ef4dde8c5717
ina <dragoalie@gmail.com>
2020-03-27 12:27:00 +0900

    improve - spread range with mutable set to check is duplicated

--- dailyOne/p489/solve.py
+++ dailyOne/p489/solve.py
@@ -1,26 +1,19 @@
 """
-Given an array of elements, return the length of the longest subarray where all its elements are distinct.
-For example, given the array [5, 1, 3, 5, 2, 3, 4, 1],
-return 5 as the longest subarray of distinct elements is [5, 2, 3, 4, 1].
+Sliding Window. O(N) time.
 """
 
 
 def length_of_longest_subarray(xs):
-    if not xs:
-        return 0
-
-    def is_all_unique(i, j, ary):
-        a = ary[i:j + 1]  # additional space
-        return len(a) == len(set(a))
-
-    prev, curr = 1, 1
-    l, r = 0, 1  # r is faster
-    while r < len(xs):
-        if is_all_unique(l, r, xs):
-            curr = r - l + 1
+    visited = set()  # additional space
+    res = 0
+    i, j = 0, 0
+    while j < len(xs):
+        # try to move j for spread range.
+        if xs[j] not in visited:
+            visited.add(xs[j])
+            j += 1
+            res = max(res, j - i)
         else:
-            prev = max(curr, prev)
-            curr = 1
-            l += 1
-        r += 1
-    return max(curr, prev)
+            visited.remove(xs[i])  # xs[j] already visited the point where xs[i] is.
+            i += 1  # the new start point of the range.
+    return res
