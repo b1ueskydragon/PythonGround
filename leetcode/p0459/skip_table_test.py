27c626d830a97eb359a77fba4be33a8596a10ca9
ina <dragoalie@gmail.com>
2020-04-18 01:43:59 +0900

    kmp table - fix the movement of each cursors and simplify

--- leetcode/p0459/skip_table_test.py
+++ leetcode/p0459/skip_table_test.py
@@ -7,3 +7,13 @@ class SkipTableTest(unittest.TestCase):
         s = Solution()
         pattern = 'ababc'
         self.assertEqual([0, 0, 1, 2, 0], s.skip_table(pattern))
+
+    def test02(self):
+        s = Solution()
+        pattern = 'aabaaac'
+        self.assertEqual([0, 1, 0, 1, 2, 2, 0], s.skip_table(pattern))
+
+    def test03(self):
+        s = Solution()
+        pattern = 'aabaaab'
+        self.assertEqual([0, 1, 0, 1, 2, 2, 3], s.skip_table(pattern))
--- leetcode/p0459/solve01.py
+++ leetcode/p0459/solve01.py
@@ -2,7 +2,7 @@ class Solution:
     @staticmethod
     def skip_table(sub_str):
         table = [0] * len(sub_str)
-        # if the pattern matched until n th char, then we can make a k skip.
+        # if the pattern matched until n th char ( i, j ∈ n ), then we can make a k skip.
         # the table should be [k1, ... , kn]
         i, j = 0, 1  # j is faster than i. both of cursors will retrieval inside of sub_str.
         while j < len(sub_str):
@@ -11,8 +11,11 @@ class Solution:
                 i += 1
                 j += 1
             else:
-                table[j] = 0
-                j += 1
+                if i == 0:
+                    table[j] = 0
+                    j += 1
+                else:  # if i and j not matched rewind i.
+                    i -= 1  # j should wait and will find a skip count on the next loop.
         return table
 
     def repeatedSubstringPattern(self, s: str) -> bool:
