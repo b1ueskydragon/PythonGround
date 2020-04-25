434e936d02ce29137c8131fd20b6c2726cc68f17
ina <dragoalie@gmail.com>
2020-04-25 13:58:28 +0900

    speed imporved - threeSum, without sort and applied twoSum

--- leetcode/p0015/solve.py
+++ leetcode/p0015/solve.py
@@ -1,45 +1,26 @@
 class Solution:
-    def threeSum_improve(self, nums):
-        """
-        sort -> binary search
-        """
-        n = len(nums)
-        if n < 3:  # no more redundant calculation.
-            return []
-        res = set()
-        nums.sort()
-
-        for i, x in enumerate(nums[:-2]):
-            """
-            we can skip a turn that has same value as previous one
-            since sorted first.
-            """
-            if i != 0 and x == nums[i - 1]:
-                continue
-            l, r = i + 1, n - 1
-            while l < r:
-                if nums[l] + nums[r] == -x:
-                    res.add((x, nums[l], nums[r]))
-                    l += 1
-                    r -= 1
-                elif nums[l] + nums[r] > -x:
-                    r -= 1
-                else:
-                    l += 1
-
-        return list(map(list, res))
-
     def threeSum(self, nums: [int]) -> [[int]]:
-        """
-        slow
-        """
-        res = set()
-        n = len(nums)
-        nums.sort()
+        res = []
+        count = {}
+        for i in nums:
+            if i in count:
+                count[i] += 1
+            else:
+                count[i] = 1
+        for x in count:
+            if count[x] > 1 and -2 * x in count:
+                if x == 0 and count[x] < 3:
+                    continue
+                res.append((x, x, -2 * x))
+
+        nums = list(count.keys())  # all unique
         for i, x in enumerate(nums):
-            for j in range(i + 1, n):
+            ref = set()
+            for j in range(i + 1, len(nums)):
                 y = nums[j]
-                z = -(x + y)
-                if z in set(nums[j + 1:]):
-                    res.add((x, y, z))
-        return list(map(list, res))
+                z = -x - y
+                if z in ref:
+                    res.append((x, y, z))
+                else:
+                    ref.add(y)
+        return res
