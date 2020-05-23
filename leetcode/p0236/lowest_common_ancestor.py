3eff698064505d656c9c8a19c5e8667bd5207c03
ina <dragoalie@gmail.com>
2020-05-23 09:41:14 +0900

    improved - LCA binary tree; stack one side first, then track with other side

--- leetcode/p0236/lowest_common_ancestor.py
+++ leetcode/p0236/lowest_common_ancestor.py
@@ -24,21 +24,10 @@ class Solution:
                 path.append(curr.right)
                 pair[curr.right] = curr
 
-        pp, qp = pair[p], pair[q]
-        ans_p, ans_q = [p], [q]
-        while pp:
-            ans_p.append(pp)
-            pp = pair[pp]
-        while qp:
-            ans_q.append(qp)
-            qp = pair[qp]
-
-        while len(ans_p) > len(ans_q):
-            ans_p.pop(0)
-        while len(ans_q) > len(ans_p):
-            ans_q.pop(0)
-
-        for i, lcp in enumerate(ans_p):
-            if ans_q[i] == lcp:
-                return lcp
-        return None
+        ans = set()
+        while q:  # start with q or p either is ok.
+            ans.add(q)
+            q = pair[q]
+        while p not in ans:  # the first appear will be an lcp.
+            p = pair[p]
+        return p
