88dc8db60c7bcbb92abbf4349289ce6996a153d9
ina <dragoalie@gmail.com>
2020-05-27 19:25:02 +0900

    solve - find LCA with dfs; return root or p or q

---
+++ leetcode/p0236/lowest_common_ancestor01.py
@@ -0,0 +1,21 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    # All of the nodes' values will be unique.
+    # p and q are different and both values will exist in the binary tree.
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        if left:
+            return left
+        if right:
+            return right
