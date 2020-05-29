b8501bedb56f7e6c7bfd0951b2414c82134b62be
ina <dragoalie@gmail.com>
2020-05-30 08:47:31 +0900

    refactor/improve - turbulence array; using up and down cache as a graph

--- leetcode/p0978/longest_turbulent_subarray.py
+++ leetcode/p0978/longest_turbulent_subarray.py
@@ -7,23 +7,18 @@ class Solution:
                 change[i] = 1
             elif a < prev:
                 change[i] = -1
-            else:
-                change[i] = 0
             prev = a
 
         prev = -2
         max_len = 1
         incl_curr = 0
-        i = 0
-        while i < len(A):
-            if prev != change[i] != 0:
+        for x in change:
+            if prev != x != 0:
                 incl_curr += 1
-            elif change[i] == 0:
-                incl_curr = 1
+            elif x == 0:
+                incl_curr = 1  # reset
             else:
-                incl_curr = 2
+                incl_curr = 2  # reset
             max_len = max(max_len, incl_curr)
-            prev = change[i]
-            # print(i, incl_curr)
-            i += 1
+            prev = x
         return max_len
