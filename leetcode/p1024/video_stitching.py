08a13db31630b2c6ac3dee9cc93889c19812bfc9
ina <dragoalie@gmail.com>
2020-06-05 13:40:35 +0900

    refactor inlines - jump to the farthest without sorting

--- leetcode/p1024/video_stitching.py
+++ leetcode/p1024/video_stitching.py
@@ -1,16 +1,17 @@
 class Solution:
     def videoStitching(self, clips: [[int]], T: int) -> int:
-        end_points = [0] * 101  # farthest from each start point. constraint T<=100
+        end_points = [0] * 101  # can have extra video after the event ends. constraint T<=100
         for start, end in clips:
             end_points[start] = max(end, end_points[start])
         end_before = reachable = count = 0
-        for curr in range(T + 1):
-            if curr > reachable:  # has a blank(fragmentation)
+        for t in range(T + 1):
+            if t > reachable:  # has a blank(fragmentation)
                 return -1
-            reachable = max(reachable, end_points[curr])
-            if curr >= end_before:
-                count += 1
-                end_before = reachable
-                if end_before >= T:  # reachable already exceed T at this point
-                    return count
+            reachable = max(reachable, end_points[t])
+            if t < end_before:
+                continue
+            count += 1
+            end_before = reachable
+            if end_before >= T:  # reachable already exceed T at this point
+                return count
         return -1
