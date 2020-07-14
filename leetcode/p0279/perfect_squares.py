014884f1260fe38caaf45912711db8056bef1c48
ina <dragoalie@gmail.com>
2020-07-14 22:31:15 +0900

    solved maybe TLE - bottom-up DP; a bit improvement

--- leetcode/p0279/perfect_squares.py
+++ leetcode/p0279/perfect_squares.py
@@ -2,18 +2,12 @@ class Solution:
     def numSquares(self, n: int) -> int:
         dp = [float('inf')] * (n + 1)
         dp[0] = 0
-        for i in range(1, n + 1):
-            ii = i * i
-            if ii >= n + 1:
-                break
-            dp[ii] = 1
-        if dp[-1] == 1:
-            return 1
         for k in range(1, n + 1):
+            kk = k * k
+            if kk < n + 1:
+                dp[kk] = 1
             for i in range(1, k + 1):
-                ii = i * i  # for the minimal count, at least one number should be a square num (count = 1).
-                if ii >= k:
-                    break
-                # f(5) := f(4) + f(1)  vs f(3) + f(2)
+                ii = i * i
+                if ii >= k: break
                 dp[k] = min(dp[k], dp[ii] + dp[k - (ii)])
         return dp[-1]
