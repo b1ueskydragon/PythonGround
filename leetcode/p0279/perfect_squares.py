eb0f38e44b60fcd6dbc0b47e1e597d1ac8fe7df8
ina <dragoalie@gmail.com>
2020-07-14 22:26:58 +0900

    solved maybe TLE - bottom-up DP; a bit improvement

--- leetcode/p0279/perfect_squares.py
+++ leetcode/p0279/perfect_squares.py
@@ -3,15 +3,17 @@ class Solution:
         dp = [float('inf')] * (n + 1)
         dp[0] = 0
         for i in range(1, n + 1):
-            if i * i >= n + 1:
+            ii = i * i
+            if ii >= n + 1:
                 break
-            dp[i * i] = 1
-
+            dp[ii] = 1
         if dp[-1] == 1:
             return 1
-
         for k in range(1, n + 1):
-            for i in range(1, (k // 2) + 1):
+            for i in range(1, k + 1):
+                ii = i * i  # for the minimal count, at least one number should be a square num (count = 1).
+                if ii >= k:
+                    break
                 # f(5) := f(4) + f(1)  vs f(3) + f(2)
-                dp[k] = min(dp[k], dp[i] + dp[k - i])
+                dp[k] = min(dp[k], dp[ii] + dp[k - (ii)])
         return dp[-1]
