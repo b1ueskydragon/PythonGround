46edc07c985a1768c3d896a91876e96ccc448534
ina <dragoalie@gmail.com>
2020-07-17 22:50:46 +0900

    resolve - reduce count of the array index reference

--- leetcode/p0279/perfect_squares.py
+++ leetcode/p0279/perfect_squares.py
@@ -3,11 +3,8 @@ class Solution:
         dp = [float('inf')] * (n + 1)
         dp[0] = 0
         for k in range(1, n + 1):
-            kk = k * k
-            if kk < n + 1:
-                dp[kk] = 1
-            for i in range(1, k + 1):
-                ii = i * i
-                if ii >= k: break
-                dp[k] = min(dp[k], dp[ii] + dp[k - (ii)])
+            i = 1
+            while i * i <= k:
+                dp[k] = min(dp[k], dp[k - i * i] + 1)
+                i += 1
         return dp[-1]
