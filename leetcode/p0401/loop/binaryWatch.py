4e100c4318597bb68c317ced7f2d09ff01167d97
ina <dragoalie@gmail.com>
2020-09-06 13:53:50 +0900

    add paratheness for bit-manipulation and simplify

--- leetcode/p0401/loop/binaryWatch.py
+++ leetcode/p0401/loop/binaryWatch.py
@@ -4,11 +4,10 @@ from typing import List
 # The order of output does not matter.
 # There is a  limit, h < 12, m < 60.
 class Solution:
-    def __init__(self):
+    def readBinaryWatch(self, num: int) -> List[str]:
         def _ones(n, cnt=0):
             while n > 0:
-                if n & 1 == 1:
-                    cnt += 1
+                cnt += (n & 1)
                 n >>= 1
             return cnt
 
@@ -21,11 +20,7 @@ class Solution:
                 table[digit].append(i)
             return table
 
-        self.h = _gen_table(12)
-        self.m = _gen_table(60)
-
-    def readBinaryWatch(self, num: int) -> List[str]:
-        h, m = self.h, self.m
+        h, m = _gen_table(12), _gen_table(60)
         res = []
         for i in range(num + 1):
             if i not in h or num - i not in m:
